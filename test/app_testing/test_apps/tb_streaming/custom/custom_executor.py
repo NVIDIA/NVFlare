f5d71a018968588c9d5811cf35641450abd25db2
YuanTingHsieh <yuantinghsieh@gmail.com>
2021-12-03 15:40:15 -0800

    Update streaming widget to use just function but not component

--- test/app_testing/test_apps/tb_streaming/custom/custom_executor.py
+++ test/app_testing/test_apps/tb_streaming/custom/custom_executor.py
@@ -7,18 +7,17 @@ from nvflare.apis.fl_constant import FLContextKey
 from nvflare.apis.fl_context import FLContext
 from nvflare.apis.shareable import Shareable
 from nvflare.apis.signal import Signal
+from nvflare.app_common.widgets.streaming import send_analytic_dxo, write_scalar, write_text
 
 
 class CustomExecutor(Executor):
-    def __init__(self, task_name: str = "poc", streamer_id: str = "analytic_sender"):
+    def __init__(self, task_name: str = "poc"):
         super().__init__()
         if not isinstance(task_name, str):
             raise TypeError("task name should be a string.")
-        if not isinstance(streamer_id, str):
-            raise TypeError("streamer_id should be a string.")
+
         self.task_name = task_name
         self.logger = logging.getLogger("POCExecutor")
-        self.streamer_id = streamer_id
 
     def execute(
         self,
@@ -32,13 +31,13 @@ class CustomExecutor(Executor):
             r = peer_ctx.get_prop("current_round")
 
             number = random.random()
-            engine = fl_ctx.get_engine()
 
             # send analytics
-            analytic_sender = engine.get_component(self.streamer_id)
-            analytic_sender.write_scalar(fl_ctx, "random_number", number, global_step=r)
-            analytic_sender.write_text(fl_ctx, "debug_msg", "Hello world", global_step=r)
-            time.sleep(1.0)
+            dxo = write_scalar("random_number", number, global_step=r)
+            send_analytic_dxo(comp=self, dxo=dxo, fl_ctx=fl_ctx)
+            dxo = write_text("debug_msg", "Hello world", global_step=r)
+            send_analytic_dxo(comp=self, dxo=dxo, fl_ctx=fl_ctx)
+            time.sleep(2.0)
 
             return shareable
         else:
