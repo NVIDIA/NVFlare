ae4bdd6043120cae758deef6a401ee0545004461
Nicolas Pannetier <nicolaspannetier@flywheel.io>
2022-01-04 15:09:07 -0800

    get client IP more robustly' (#81)

--- nvflare/private/fed/client/communicator.py
+++ nvflare/private/fed/client/communicator.py
@@ -107,13 +107,33 @@ class Communicator:
 
         return state_message
 
+    def get_client_ip(self):
+        """Return localhost IP.
+
+        More robust than ``socket.gethostbyname(socket.gethostname())``. See
+        https://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib/28950776#28950776
+        for more details.
+
+        :return: The host IP.
+        """
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            s.connect(('10.255.255.255', 1))  # doesn't even have to be reachable
+            ip = s.getsockname()[0]
+        except Exception:
+            ip = '127.0.0.1'
+        finally:
+            s.close()
+        return ip
+
     def client_registration(self, client_name, servers, project_name):
         """
         Client's meta data used to authenticate and communicate.
 
         :return: a ClientLogin message.
         """
-        local_ip = socket.gethostbyname(socket.gethostname())
+        local_ip = self.get_client_ip()
+
         login_message = fed_msg.ClientLogin(client_name=client_name, client_ip=local_ip)
         # login_message = fed_msg.ClientLogin(
         #     client_id=None, token=None, client_ip=local_ip)
