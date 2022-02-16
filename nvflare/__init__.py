4242f072f4241e21beda5021e0304e7d54b54744
Isaac Yang <isaacy@nvidia.com>
2022-02-16 08:55:25 -0800

    Add versioneer so nvflare.__version__ has meaningful value

--- nvflare/__init__.py
+++ nvflare/__init__.py
@@ -12,4 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "2.0.7"
+from . import _version
+__version__ = _version.get_versions()['version']
