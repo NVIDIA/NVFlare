267cd8577d1ef4b367544361f3d316d33e4e54de
Yuhong Wen <yh_wen@hotmail.com>
2022-03-07 15:25:38 -0500

    Added storage_state_persistor.py

---
+++ nvflare/app_common/state_persistors/storage_state_persistor.py
@@ -0,0 +1,67 @@
+# Copyright (c) 2021-2022, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import datetime
+import pickle
+import os
+
+from nvflare.apis.fl_snapshot import FLSnapshot
+from nvflare.apis.state_persistor import StatePersistor
+from nvflare.apis.storage import StorageSpec
+
+
+class StorageStatePersistor(StatePersistor):
+    def __init__(self, storage: StorageSpec, location: str):
+        self.storage = storage
+
+        self.location = location
+
+        if not os.path.isabs(location):
+            raise ValueError("snapshot location must be absolute path.")
+
+    def save(self, snapshot: FLSnapshot) -> str:
+        """Call to save the snapshot of the FL state to storage.
+        Args:
+            snapshot: FLSnapshot object
+        Returns: storage location
+        """
+        snapshot_dict = snapshot.get_snapshot()
+        # snapshot_uri_timestamp = "snapshot-" + datetime.datetime.now().strftime("%Y-%m-%d,%H:%M:%S")
+        self.storage.create_object(
+            uri=self.location, data=pickle.dumps(snapshot_dict), meta={}, overwrite_existing=True
+        )
+
+        return self.location
+
+    def retrieve(self) -> FLSnapshot:
+        """Call to load the persisted FL components snapshot from the persisted location.
+        Args:
+        Returns:
+        """
+        retrieved_snapshot = None
+        try:
+            retrieved_dict = pickle.loads(self.storage.get_data(self.location))
+            retrieved_snapshot = FLSnapshot()
+            for component_id, component_dict in retrieved_dict.items():
+                retrieved_snapshot.save_component_snapshot(component_id, component_dict)
+        finally:
+            return retrieved_snapshot
+
+    def delete(self, location: str):
+        """To delete the FL component snapshot.
+        Args:
+            location: persist location
+        Returns:
+        """
+        self.storage.delete_object(location)
