433b3607afa2b8e7434ee686af8bb71aed7218bf
Yuhong Wen <yh_wen@hotmail.com>
2022-03-29 20:23:12 -0400

    Removed theStudyManagerSpec.

--- nvflare/private/fed/server/server_engine.py
+++ nvflare/private/fed/server/server_engine.py
@@ -35,7 +35,6 @@ from nvflare.apis.fl_context import FLContext
 from nvflare.apis.fl_snapshot import FLSnapshot
 from nvflare.apis.shareable import Shareable, make_reply
 from nvflare.apis.utils.common_utils import get_open_ports
-from nvflare.apis.study_manager_spec import StudyManagerSpec
 from nvflare.apis.utils.fl_context_utils import get_serializable_data
 from nvflare.apis.workspace import Workspace
 from nvflare.fuel.hci.zip_utils import zip_directory_to_bytes
