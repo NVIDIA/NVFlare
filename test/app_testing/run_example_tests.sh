b05353ba77db06268290a6f3bfe4172a5358134b
YuanTingHsieh <yuantinghsieh@gmail.com>
2022-04-01 05:24:25 -0700

    Add install torchvision

--- test/app_testing/run_example_tests.sh
+++ test/app_testing/run_example_tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-pip install tensorflow torch
+pip install tensorflow torch torchvision
 
 TF_FORCE_GPU_ALLOW_GROWTH=true python test_runner.py --poc ../../nvflare/poc --n_clients 2 --yaml test_examples.yml --app_path ../../examples --cleanup
