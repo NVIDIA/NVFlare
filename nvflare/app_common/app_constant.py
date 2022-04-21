00a94007adbe498a5468f07a09b2ea5e8ec213ee
YuanTingHsieh <yuantinghsieh@gmail.com>
2022-04-21 16:58:01 -0700

    Remove unused app constant

--- nvflare/app_common/app_constant.py
+++ nvflare/app_common/app_constant.py
@@ -24,36 +24,12 @@ class ExecutorTasks:
 
 class AppConstants(object):
 
-    CONFIG_PATH = "config_path"
-    MODEL_NETWORK = "model_network"
-    MULTI_GPU = "multi_gpu"
-    TRAIN_CONTEXT = "train_context"
-    DEVICE = "device"
-    MODEL_NAME = "model_name"
-    MODEL_URL = "model_url"
     START_ROUND = "start_round"
     CURRENT_ROUND = "current_round"
     CONTRIBUTION_ROUND = "contribution_round"
-    CONTRIBUTION_CLIENT = "contribution_client"
     NUM_ROUNDS = "num_rounds"
-    WAIT_AFTER_MIN_CLIENTS = "wait_after_min_clients"
 
-    NUM_TOTAL_STEPS = "num_total_steps"  # TOTAL_STEPS
-    NUM_EPOCHS_CURRENT_ROUND = "num_epochs_current_round"  # CURRENT_EPOCHS
-    NUM_TOTAL_EPOCHS = "num_total_epochs"  # LOCAL_EPOCHS
-    LOCAL_EPOCHS = "local_epochs"
-
-    IS_FIRST_ROUND = "is_first_round"
-    MY_RANK = "my_rank"
-    INITIAL_LEARNING_RATE = "initial_learning_rate"
-    CURRENT_LEARNING_RATE = "current_learning_rate"
-    NUMBER_OF_GPUS = "number_of_gpus"
-    META_COOKIE = "cookie"
-    META_DATA = "meta_data"
     GLOBAL_MODEL = "global_model"
-
-    IS_BEST = "is_best"
-    FAILURE = "failure"
 
     LOG_DIR = "model_log_dir"
     CKPT_PRELOAD_PATH = "ckpt_preload_path"
@@ -63,27 +39,14 @@ class AppConstants(object):
     PHASE = "_phase_"
     PHASE_INIT = "_init_"
     PHASE_TRAIN = "train"
-    PHASE_MODEL_VALIDATION = "model_validation"
     PHASE_FINISHED = "_finished_"
 
-    STATUS_WAIT = "_wait_"
-    STATUS_DONE = "_done_"
-    STATUS_TRAINING = "_training_"
-    STATUS_IDLE = "_idle_"
-
-    MODEL_LOAD_PATH = "_model_load_path"
-    MODEL_SAVE_PATH = "_model_save_path"
-    DEFAULT_MODEL_DIR = "models"
-
-    ROUND = "_round_"
     MODEL_WEIGHTS = "_model_weights_"
     AGGREGATION_RESULT = "_aggregation_result"
-    AGGREGATION_TRIGGERED = "_aggregation_triggered"
     AGGREGATION_ACCEPTED = "_aggregation_accepted"
     TRAIN_SHAREABLE = "_train_shareable_"
     TRAINING_RESULT = "_training_result_"
 
-    SUBMIT_MODEL_FAILURE_REASON = "_submit_model_failure_reason"
     CROSS_VAL_DIR = "cross_site_val"
     CROSS_VAL_MODEL_DIR_NAME = "model_shareables"
     CROSS_VAL_RESULTS_DIR_NAME = "result_shareables"
@@ -98,16 +61,10 @@ class AppConstants(object):
     TASK_SUBMIT_MODEL = "submit_model"
     TASK_VALIDATION = "validate"
 
-    CROSS_VAL_SERVER_MODEL = "_cross_val_server_model_"
-    CROSS_VAL_CLIENT_MODEL = "_cross_val_client_model_"
-    PARTICIPATING_CLIENTS = "_particpating_clients_"
+    PARTICIPATING_CLIENTS = "_participating_clients_"
 
     MODEL_OWNER = "_model_owner_"
 
-    DEFAULT_FORMATTER_ID = "formatter"
-    DEFAULT_MODEL_LOCATOR_ID = "model_locator"
-
-    TASK_END_RUN = "_end_run_"
     TASK_TRAIN = "train"
 
     DEFAULT_AGGREGATOR_ID = "aggregator"
@@ -133,7 +90,6 @@ class DefaultCheckpointFileName(object):
 class ModelName(object):
 
     BEST_MODEL = "best_model"
-    FINAL_MODEL = "final_model"
 
 
 class ModelFormat(object):
