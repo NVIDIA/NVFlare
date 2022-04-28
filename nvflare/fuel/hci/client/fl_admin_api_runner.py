76b9e1c9f308869806677ecabd98d4b502e0e76e
Kevin Lu <kevlu@nvidia.com>
2022-04-28 15:09:13 -0400

    update FLAdminAPI runner

--- nvflare/fuel/hci/client/fl_admin_api_runner.py
+++ nvflare/fuel/hci/client/fl_admin_api_runner.py
@@ -17,7 +17,6 @@ import time
 
 from nvflare.fuel.common.excepts import ConfigError
 from nvflare.fuel.hci.client.fl_admin_api import FLAdminAPI
-from nvflare.fuel.hci.client.fl_admin_api_constants import FLDetailKey
 from nvflare.fuel.hci.client.fl_admin_api_spec import TargetType
 from nvflare.private.fed.app.fl_conf import FLAdminClientStarterConfigurator
 
@@ -34,22 +33,6 @@ def api_command_wrapper(api_command_result):
         raise RuntimeError("command was not successful!")
 
     return api_command_result
-
-
-def wait_until_clients_greater_than_cb(reply, min_clients=2):
-    """Callback in wait_until_server_status for waiting until a minimum number of clients are registered on server.
-
-    Args:
-        reply: reply from checking the server status
-        min_clients: minimum number of clients (default: 2)
-
-    Returns: True if the minimum number of clients has been registered, otherwise False.
-
-    """
-    if reply["details"][FLDetailKey.REGISTERED_CLIENTS] >= min_clients:
-        return True
-    else:
-        return False
 
 
 class FLAdminAPIRunner:
@@ -141,11 +124,7 @@ class FLAdminAPIRunner:
 
     def run(
         self,
-        app,
-        restart_all_first=False,
-        min_clients=2,
-        shutdown_on_error=False,
-        shutdown_at_end=False,
+        job_folder_name,
     ):
         """An example script to upload, deploy, and start a specified app.
 
@@ -153,39 +132,19 @@ class FLAdminAPIRunner:
         to follow along as the commands run.
 
         Args:
-            run_number: run number to use
-            app: app to upload, deploy, and start
-            min_clients: minimum number of clients to have registered on the server after restart all before proceeding (default: 2)
-            timeout: number of seconds to wait in the wait_until_client_status command before the app is aborted in this example (default: 2000)
-            restart_all_first: whether to restart all before the run in order to have clean environment
-            shutdown_on_error: whether to shut down all if there is an error on app startup
-            shutdown_at_end: whether to shut down all at the end of script
+            job_folder_name: name of job folder to submit, either relative to the upload_dir specified in the fed_admin.json config, or absolute path
 
         """
         try:
-            if restart_all_first:
-                print(f'api.restart("{TargetType.ALL}")')
-                api_command_wrapper(self.api.restart(TargetType.ALL))
-                self.attempt_login_until_success()
-                print("api.wait_until_server_status(callback=wait_until_clients_greater_than_cb)")
-                api_command_wrapper(
-                    self.api.wait_until_server_status(
-                        callback=wait_until_clients_greater_than_cb, min_clients=min_clients
-                    )
-                )
             print("api.check_status(TargetType.SERVER)")
             api_command_wrapper(self.api.check_status(TargetType.SERVER))
-            print(f'api.submit_job("{app}")')
-            api_command_wrapper(self.api.submit_job(app))
+            print(f'api.submit_job("{job_folder_name}")')
+            api_command_wrapper(self.api.submit_job(job_folder_name))
             time.sleep(1)
             print("api.check_status(TargetType.SERVER)")
-            reply = api_command_wrapper(self.api.check_status(TargetType.SERVER))
-            if shutdown_on_error and reply["details"][FLDetailKey.SERVER_ENGINE_STATUS] == "stopped":
-                print("Server startup failed! Shutdown all...")
-                api_command_wrapper(self.api.shutdown(TargetType.ALL))
-                return
-            # The following wait_until can be put into a loop that has other behavior other than waiting.
-            # This code is just a demonstration of running an app expected to stop.
+            api_command_wrapper(self.api.check_status(TargetType.SERVER))
+            # The following wait_until can be put into a loop that has other behavior other than waiting until clients
+            # are in a status of stopped. For this code, the app is expected to stop, or this may not end.
             print("api.wait_until_client_status()")
             wait_result = api_command_wrapper(self.api.wait_until_client_status())
             print(wait_result)
@@ -195,33 +154,5 @@ class FLAdminAPIRunner:
             time.sleep(10)  # wait for clients to stop in case they take longer than server to stop
             print("api.check_status(TargetType.CLIENT)")
             api_command_wrapper(self.api.check_status(TargetType.CLIENT))
-            if shutdown_at_end:
-                print("api.shutdown(TargetType.ALL)")
-                api_command_wrapper(self.api.shutdown(TargetType.ALL))
         except RuntimeError as e:
             print(f"There was an exception: {e}")
-            if shutdown_on_error:
-                print("Attempting shutdown all...")
-                try:
-                    # Note that this will only shut down the active server and clients. Cold servers and the overseer
-                    # will need to be shutdown separately or with the "shutdown_system" command
-                    api_command_wrapper(self.api.shutdown(TargetType.ALL))
-                except RuntimeError as e:
-                    print(f"There was an exception while attempting shutdown all: {e}")
-
-    def attempt_login_until_success(self):
-        logged_in = False
-        while not logged_in:
-            reply = None
-            try:
-                if self.poc:
-                    reply = self.api.login_with_poc("admin", "admin")
-                else:
-                    print("api.login()")
-                    reply = api_command_wrapper(self.api.login(self.username))
-            except RuntimeError:
-                time.sleep(10)
-                pass
-            if reply and reply["status"] == "SUCCESS":
-                logged_in = True
-        return True
