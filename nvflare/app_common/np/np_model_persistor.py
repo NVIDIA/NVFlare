528677c9e3731b4caca4c5c82bb286df1e929ae9
Yuan-Ting Hsieh (謝沅廷) <yuantinghsieh@gmail.com>
2022-05-12 09:38:24 -0700

    Fix np codes (#514)

--- nvflare/app_common/np/np_model_persistor.py
+++ nvflare/app_common/np/np_model_persistor.py
@@ -20,9 +20,19 @@ from nvflare.apis.fl_constant import FLContextKey
 from nvflare.apis.fl_context import FLContext
 from nvflare.app_common.abstract.model import ModelLearnable, ModelLearnableKey, make_model_learnable
 from nvflare.app_common.abstract.model_persistor import ModelPersistor
-from nvflare.app_common.app_constant import AppConstants
 
 from .constants import NPConstants
+
+
+def _get_run_dir(fl_ctx: FLContext):
+    engine = fl_ctx.get_engine()
+    if engine is None:
+        raise RuntimeError("engine is missing in fl_ctx.")
+    run_number = fl_ctx.get_prop(FLContextKey.CURRENT_RUN)
+    if run_number is None:
+        raise RuntimeError("run_number is missing in fl_ctx.")
+    run_dir = engine.get_workspace().get_run_dir(run_number)
+    return run_dir
 
 
 class NPModelPersistor(ModelPersistor):
@@ -36,42 +46,30 @@ class NPModelPersistor(ModelPersistor):
         self.default_data = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]], dtype=np.float32)
 
     def load_model(self, fl_ctx: FLContext) -> ModelLearnable:
-        # Get start round from FLContext. If start_round > 0, we will try loading model from disk.
-        start_round = fl_ctx.get_prop(AppConstants.START_ROUND, 0)
-        engine = fl_ctx.get_engine()
-        run_number = fl_ctx.get_prop(FLContextKey.CURRENT_RUN)
-        run_dir = engine.get_workspace().get_run_dir(run_number)
+        run_dir = _get_run_dir(fl_ctx)
         model_path = os.path.join(run_dir, self.model_dir, self.model_name)
-
-        # Create a new numpy model
-        if start_round > 0:
-            try:
-                data = np.load(model_path)
-            except Exception as e:
-                self.log_exception(
-                    fl_ctx, f"Unable to load model from {model_path}. Using default data instead.", fire_event=False
-                )
-                data = self.default_data.copy()
-        else:
+        try:
+            # try loading previous model
+            data = np.load(model_path)
+        except Exception as e:
+            self.log_exception(
+                fl_ctx,
+                f"Unable to load model from {model_path}: {e}. Using default data instead.",
+                fire_event=False,
+            )
             data = self.default_data.copy()
 
-        # Generate model dictionary and create model_learnable.
-        weights = {NPConstants.NUMPY_KEY: data}
-        model_learnable = make_model_learnable(weights, {})
+        model_learnable = make_model_learnable(weights={NPConstants.NUMPY_KEY: data}, meta_props={})
 
-        self.logger.info(f"Loaded initial model: {model_learnable[ModelLearnableKey.WEIGHTS]}")
+        self.log_info(fl_ctx, f"Loaded initial model: {model_learnable[ModelLearnableKey.WEIGHTS]}")
         return model_learnable
 
-    def save_model(self, model: ModelLearnable, fl_ctx: FLContext):
-        engine = fl_ctx.get_engine()
-        run_number = fl_ctx.get_prop(FLContextKey.CURRENT_RUN)
-        run_dir = engine.get_workspace().get_run_dir(run_number)
-        model_path = os.path.join(run_dir, self.model_dir)
-        if not os.path.exists(model_path):
-            os.makedirs(model_path)
+    def save_model(self, model_learnable: ModelLearnable, fl_ctx: FLContext):
+        run_dir = _get_run_dir(fl_ctx)
+        model_root_dir = os.path.join(run_dir, self.model_dir)
+        if not os.path.exists(model_root_dir):
+            os.makedirs(model_root_dir)
 
-        model_save_path = os.path.join(model_path, self.model_name)
-        if model_save_path:
-            with open(model_save_path, "wb") as f:
-                np.save(f, model[ModelLearnableKey.WEIGHTS][NPConstants.NUMPY_KEY])
-            self.log_info(fl_ctx, f"Saved numpy model to: {model_save_path}")
+        model_path = os.path.join(model_root_dir, self.model_name)
+        np.save(model_path, model_learnable[ModelLearnableKey.WEIGHTS][NPConstants.NUMPY_KEY])
+        self.log_info(fl_ctx, f"Saved numpy model to: {model_path}")
