a2afc2987498fea4aebb03aa334d47143b9f04ab
Ziyue Xu <ziyue.xu@gmail.com>
2022-05-18 13:31:08 -0400

    Modify trainers

--- examples/brats18/pt/learners/supervised_monai_brats_learner.py
+++ examples/brats18/pt/learners/supervised_monai_brats_learner.py
@@ -15,29 +15,28 @@
 import json
 import os
 
+import numpy as np
 import torch
 import torch.optim as optim
-from monai.data import CacheDataset, DataLoader, Dataset, load_decathlon_datalist
+from monai.data import CacheDataset, DataLoader, Dataset, load_decathlon_datalist, decollate_batch
 from monai.inferers import SlidingWindowInferer
 from monai.losses import DiceLoss
 from monai.metrics import DiceMetric
-from monai.networks.nets.unet import UNet
+from monai.networks.nets.segresnet import SegResNet
 from monai.transforms import (
     Activations,
     AsDiscrete,
-    AsDiscreted,
     Compose,
+    ConvertToMultiChannelBasedOnBratsClassesd,
     DivisiblePadd,
     EnsureChannelFirstd,
-    EnsureType,
-    EnsureTyped,
     LoadImaged,
     NormalizeIntensityd,
     Orientationd,
-    RandCropByPosNegLabeld,
     RandFlipd,
     RandScaleIntensityd,
     RandShiftIntensityd,
+    RandSpatialCropd,
     Spacingd,
 )
 from pt.learners.supervised_learner import SupervisedLearner
@@ -46,16 +45,18 @@ from pt.utils.custom_client_datalist_json_path import custom_client_datalist_jso
 from nvflare.apis.fl_context import FLContext
 from nvflare.app_common.app_constant import AppConstants
 from nvflare.app_common.pt.pt_fedproxloss import PTFedProxLoss
+from nvflare.apis.fl_constant import FLContextKey, ReturnCode
+from nvflare.apis.shareable import Shareable, make_reply
+from nvflare.apis.signal import Signal
 
-
-class SupervisedMonaiProstateLearner(SupervisedLearner):
+class SupervisedMonaiBratsLearner(SupervisedLearner):
     def __init__(
         self,
         train_config_filename,
         aggregation_epochs: int = 1,
         train_task_name: str = AppConstants.TASK_TRAIN,
     ):
-        """MONAI Learner for prostate segmentation task.
+        """MONAI Learner for BraTS18 segmentation task.
         It inherits from SupervisedLearner.
 
         Args:
@@ -97,8 +98,8 @@ class SupervisedMonaiProstateLearner(SupervisedLearner):
         cache_rate = self.config_info["cache_dataset"]
         dataset_base_dir = self.config_info["dataset_base_dir"]
         datalist_json_path = self.config_info["datalist_json_path"]
-        self.roi_size = self.config_info.get("roi_size", (224, 224, 32))
-        self.infer_roi_size = self.config_info.get("infer_roi_size", (224, 224, 32))
+        self.roi_size = self.config_info.get("roi_size", (224, 224, 144))
+        self.infer_roi_size = self.config_info.get("infer_roi_size", (240, 240, 160))
 
         # Get datalist json
         datalist_json_path = custom_client_datalist_json_path(datalist_json_path, self.client_id)
@@ -123,16 +124,23 @@ class SupervisedMonaiProstateLearner(SupervisedLearner):
 
         # Set the training-related context
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        self.model = UNet(
-            dimensions=3,
-            in_channels=1,
-            out_channels=1,
-            channels=(16, 32, 64, 128, 256),
-            strides=(2, 2, 2, 2),
-            num_res_units=2,
+        self.model = SegResNet(
+            blocks_down=[1, 2, 2, 4],
+            blocks_up=[1, 1, 1],
+            init_filters=16,
+            in_channels=4,
+            out_channels=3,
+            dropout_prob=0.2,
         ).to(self.device)
-        self.optimizer = optim.SGD(self.model.parameters(), lr=self.lr, momentum=0.9)
-        self.criterion = DiceLoss(sigmoid=True)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr, weight_decay=1e-5)
+        self.lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(self.optimizer, T_max=100)
+        self.criterion = DiceLoss(
+            smooth_nr=0,
+            smooth_dr=1e-5,
+            squared_pred=True,
+            to_onehot_y=False,
+            sigmoid=True,
+        )
 
         if self.fedproxloss_mu > 0:
             self.log_info(fl_ctx, f"using FedProx loss with mu {self.fedproxloss_mu}")
@@ -140,48 +148,41 @@ class SupervisedMonaiProstateLearner(SupervisedLearner):
 
         self.transform_train = Compose(
             [
+                # load Nifti image
                 LoadImaged(keys=["image", "label"]),
-                EnsureChannelFirstd(keys=["image", "label"]),
+                EnsureChannelFirstd(keys="image"),
+                ConvertToMultiChannelBasedOnBratsClassesd(keys="label"),
                 Spacingd(
                     keys=["image", "label"],
-                    pixdim=(0.3, 0.3, 1.0),
+                    pixdim=(1.0, 1.0, 1.0),
                     mode=("bilinear", "nearest"),
                 ),
-                DivisiblePadd(keys=["image", "label"], k=32),
                 Orientationd(keys=["image", "label"], axcodes="RAS"),
-                RandCropByPosNegLabeld(
-                    keys=["image", "label"],
-                    label_key="label",
-                    spatial_size=self.roi_size,
-                    pos=1,
-                    neg=1,
-                    num_samples=4,
-                ),
+                RandSpatialCropd(keys=["image", "label"], roi_size=self.roi_size, random_size=False),
                 RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=0),
                 RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=1),
                 RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=2),
                 NormalizeIntensityd(keys="image", nonzero=True, channel_wise=True),
                 RandScaleIntensityd(keys="image", factors=0.1, prob=1.0),
                 RandShiftIntensityd(keys="image", offsets=0.1, prob=1.0),
-                EnsureTyped(keys=["image", "label"]),
             ]
         )
         self.transform_valid = Compose(
             [
                 LoadImaged(keys=["image", "label"]),
-                EnsureChannelFirstd(keys=["image", "label"]),
+                EnsureChannelFirstd(keys="image"),
+                ConvertToMultiChannelBasedOnBratsClassesd(keys="label"),
                 Spacingd(
                     keys=["image", "label"],
-                    pixdim=(0.3, 0.3, 1.0),
+                    pixdim=(1.0, 1.0, 1.0),
                     mode=("bilinear", "nearest"),
                 ),
                 DivisiblePadd(keys=["image", "label"], k=32),
                 Orientationd(keys=["image", "label"], axcodes="RAS"),
                 NormalizeIntensityd(keys="image", nonzero=True, channel_wise=True),
-                EnsureTyped(keys=["image", "label"]),
             ]
         )
-        self.transform_post = Compose([EnsureType(), Activations(sigmoid=True), AsDiscrete(threshold=0.5)])
+        self.transform_post = Compose([Activations(sigmoid=True), AsDiscrete(threshold=0.5)])
 
         # Set dataset
         if cache_rate > 0.0:
@@ -221,5 +222,47 @@ class SupervisedMonaiProstateLearner(SupervisedLearner):
         )
 
         # Set inferer and evaluation metric
-        self.inferer = SlidingWindowInferer(roi_size=self.infer_roi_size, sw_batch_size=4, overlap=0.25)
-        self.valid_metric = DiceMetric(include_background=False, reduction="mean", get_not_nans=False)
+        self.inferer = SlidingWindowInferer(roi_size=self.infer_roi_size, sw_batch_size=1, overlap=0.5)
+        self.valid_metric = DiceMetric(include_background=True, reduction="mean")
+
+    # Brats has 3 classes, so the metric computation needs some change
+    def local_valid(
+        self,
+        model,
+        valid_loader,
+        abort_signal: Signal,
+        tb_id=None,
+        record_epoch=None,
+    ):
+        """Typical validation logic
+        Load data pairs from train_loader: image / label
+        Compute outputs with self.model
+        Perform post transform (binarization, etc.)
+        Compute evaluation metric with self.valid_metric
+        Add score to tensorboard record with specified id
+        """
+        model.eval()
+        with torch.no_grad():
+            metric = 0
+            ct = 0
+            for i, batch_data in enumerate(valid_loader):
+                if abort_signal.triggered:
+                    return make_reply(ReturnCode.TASK_ABORTED)
+                val_images = batch_data["image"].to(self.device)
+                val_labels = batch_data["label"].to(self.device)
+                # Inference
+                val_outputs = self.inferer(val_images, model)
+                val_outputs = self.transform_post(val_outputs)
+                # Compute metric
+                metric_score = self.valid_metric(y_pred=val_outputs, y=val_labels)
+                for sub_region in range(3):
+                    metric_score_single = metric_score[0][sub_region].item()
+                    if not np.isnan(metric_score_single):
+                        metric += metric_score_single
+                        ct += 1
+            # compute mean dice over whole validation set
+            metric /= ct
+            # tensorboard record id, add to record if provided
+            if tb_id:
+                self.writer.add_scalar(tb_id, metric, record_epoch)
+        return metric
