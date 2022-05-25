d6fd4e53e003b65f715ce705934f4ac5a7184554
YanxuanLiu <yanxuanl@nvidia.com>
2022-05-25 17:50:50 +0800

    add arg for shell to control tests to execute

--- ci/run_premerge_cpu.sh
+++ ci/run_premerge_cpu.sh
@@ -15,27 +15,63 @@
 # limitations under the License.
 #
 
-set -ex
+# Argument(s):
+#   BUILD_TYPE:   all/specific_test_name, tests to execute
 
-function pip_uninstall_deps() {
+set -ex
+BUILD_TYPE=all
+
+if [[ $# -eq 1 ]]; then
+    BUILD_TYPE=$1
+
+elif [[ $# -gt 1 ]]; then
+    echo "ERROR: too many parameters are provided"
+    exit 1
+fi
+
+pip_uninstall_deps() {
     echo "pip uninstalling requirements: $1"
     pip uninstall -r $1 -y
 }
 
-function pip_install_deps() {
+pip_install_deps() {
     echo "pip installing requirements: $1"
     pip install -r $1
     export PYTHONPATH=$PWD
 }
 
-## Unit Tests
-pip_install_deps requirements-dev.txt
-./runtest.sh
-pip_uninstall_deps requirements-dev.txt
+unit_test() {
+    echo "Run unit test..."
+    pip_install_deps requirements-dev.txt
+    ./runtest.sh
+    pip_uninstall_deps requirements-dev.txt
+}
 
+wheel_build() {
+    echo "Run wheel build..."
+    pip_install_deps requirements-dev.txt
+    pip install build twine torch torchvision
+    python3 -m build --wheel
+    pip_uninstall_deps requirements-dev.txt
+}
 
-## Wheel Build
-pip_install_deps requirements-dev.txt
-pip install build twine torch torchvision
-python3 -m build --wheel
-pip_uninstall_deps requirements-dev.txt
+case $BUILD_TYPE in
+
+    all)
+        echo "Run all tests..."
+        unit_test
+        wheel_build
+        ;;
+
+    unit_test)
+        unit_test
+        ;;
+
+    wheel_build )
+        wheel_build
+        ;;
+
+    *)
+        echo "ERROR: unknown parameter: $BUILD_TYPE"
+        ;;
+esac
