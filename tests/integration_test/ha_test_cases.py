747cc5f8721a22d76029921e91ad2f5e0b2f096f
YuanTingHsieh <yuantinghsieh@gmail.com>
2022-05-25 16:34:10 -0700

    Fix unit test and integration test errors

--- tests/integration_test/ha_test_cases.py
+++ tests/integration_test/ha_test_cases.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# TODO:: remove this file
 
 # HA test cases (referenced from NVFlare 2.1 Test Plan- High Availability)
 
