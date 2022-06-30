5205af226ef80f89d0b2de9dbf8e72725aa066c8
YuanTingHsieh <yuantinghsieh@gmail.com>
2022-06-30 00:58:54 -0700

    Use tensorflow gpu package

--- tests/integration_test/run_integration_tests.sh
+++ tests/integration_test/run_integration_tests.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-pip install tensorflow==2.8.0 torch torchvision
+pip install tensorflow-gpu==2.8.0 torch torchvision
 
 TF_FORCE_GPU_ALLOW_GROWTH=true pytest --junitxml=./integration_test.xml -v system_test.py overseer_test.py -s
