0b71aea4176fa3d20481ceab1aca6b48f264ff37
Ziyue Xu <ziyue.xu@gmail.com>
2022-08-23 19:30:08 -0400

    Revert "code refactor"
    This reverts commit 299142c8930afdabb46527b833b33b211c6bcfc4.

--- examples/xgboost/train_config_gen.sh
+++ examples/xgboost/train_config_gen.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-mkdir train_configs
 for split_mode in uniform exponential
 do
   python3 utils/prepare_train_config.py --site_num 5 --split_method ${split_mode} --out_path "train_configs/config_train_5_${split_mode}.json"
