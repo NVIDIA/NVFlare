1e49a51f4cdcc5a8b31f2f499a3dc78712dd87c1
Yan Cheng <yanc@nvidia.com>
2022-08-30 17:29:46 -0400

    fix typo

--- nvflare/app_common/filters/convert_weights.py
+++ nvflare/app_common/filters/convert_weights.py
@@ -86,8 +86,6 @@ class ConvertWeights(DXOFilter):
         if not base_weights:
             return None
 
-        scope_props = fl_ctx.get_prop(key=FLContextKey.SCOPE_PROPERTIES)
-
         processed_algo = dxo.get_meta_prop(MetaKey.PROCESSED_ALGORITHM, None)
         if processed_algo:
             self.log_info(fl_ctx,
