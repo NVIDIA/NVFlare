f7ddd07f1da588499a1ff7c8218bd63048e0c4b7
Ziyue Xu <ziyue.xu@gmail.com>
2022-08-31 22:58:03 -0400

    add one-shot training scheme

--- nvflare/private/fed/client/client_runner.py
+++ nvflare/private/fed/client/client_runner.py
@@ -58,7 +58,7 @@ class ClientRunner(FLComponent):
         config: ClientRunnerConfig,
         job_id,
         engine: ClientEngineExecutorSpec,
-        task_fetch_interval: int = 5,  # fetch task every 5 secs
+        task_fetch_interval: int = 0.1,  # fetch task every 5 secs
     ):
         """To init the ClientRunner.
 
