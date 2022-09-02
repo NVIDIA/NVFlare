a63f36e7e940615869e952fe4755c0770c51188b
YuanTingHsieh <yuantinghsieh@gmail.com>
2022-09-02 14:03:57 -0700

    Add hello-xgboost example

--- nvflare/app_opt/xgboost/constants.py
+++ nvflare/app_opt/xgboost/constants.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021, NVIDIA CORPORATION.
+# Copyright (c) 2021-2022, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,3 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
+class XGBShareableHeader:
+    WORLD_SIZE = "_world_size"
+    RANK_MAP = "_rank_map"
+    XGB_FL_SERVER_PORT = "_server_port"
+    XGB_FL_SERVER_SECURE = "_secure_server"
