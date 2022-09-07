dd4032df34f640d2d7e919f9279d73a1dd5605d0
Holger Roth <6304754+holgerroth@users.noreply.github.com>
2022-09-07 15:45:02 -0400

    add monai integration (#828)
    * add monai integration
    
    * add dependency min versions
    
    * switch back to monai rc

---
+++ integration/monai/setup.py
@@ -0,0 +1,59 @@
+# Copyright (c) 2021-2022, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import datetime
+import os
+from setuptools import find_packages, setup
+
+# read the contents of your README file
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+release = os.environ.get("MONAI_NVFL_RELEASE")
+if release == "1":
+    package_name = "monai-nvflare"
+    version = "0.1.0"
+else:
+    package_name = "monai-nvflare-nightly"
+    today = datetime.date.today().timetuple()
+    year = today[0] % 1000
+    month = today[1]
+    day = today[2]
+    version = f"0.1.0.{year:02d}{month:02d}{day:02d}"
+
+setup(
+    name=package_name,
+    version=version,
+    description="MONAI NVIDIA FLARE integration",
+    url="https://github.com/NVIDIA/NVFlare",
+    package_dir={"monai_nvflare": "monai_nvflare"},
+    packages=find_packages(
+        where=".",
+        include=[
+            "*",
+        ],
+        exclude=["tests", "tests.*"],
+    ),
+    license_files=("LICENSE",),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.7,<3.9",
+    install_requires=["monai==1.0.0rc1", "nvflare"],
+)
