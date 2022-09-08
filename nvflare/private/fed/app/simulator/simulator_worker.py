6dddcbba2ed903a8eb72c5a02007fd3f52328297
Yuhong Wen <yuhongw@nvidia.com>
2022-09-08 17:08:11 -0400

    Fixed some log issues and a timing issue for CTR-C kill the simulator. (#866)

--- nvflare/private/fed/app/simulator/simulator_worker.py
+++ nvflare/private/fed/app/simulator/simulator_worker.py
@@ -162,7 +162,7 @@ class ClientTaskWorker(FLComponent):
         except EOFError:
             self.logger.info("Client run completed.")
         except BaseException as error:
-            self.logger.error(error)
+            self.logger.error(f"ClientTaskWorker run error. {error}")
         finally:
             if admin_agent:
                 admin_agent.shutdown()
@@ -181,6 +181,7 @@ def main():
     parser.add_argument("--client", type=str, help="Client name", required=True)
     parser.add_argument("--port", type=str, help="Listen port", required=True)
     parser.add_argument("--gpu", "-g", type=str, help="gpu index number")
+    parser.add_argument("--parent_pid", type=int, help="parent process pid", required=True)
     args = parser.parse_args()
 
     log_config_file_path = os.path.join(args.workspace, "startup", "log.config")
@@ -202,7 +203,7 @@ def main():
     conn = _create_connection(args.port)
 
     # start parent process checking thread
-    parent_pid = os.getppid()
+    parent_pid = args.parent_pid
     stop_event = threading.Event()
     thread = threading.Thread(target=check_parent_alive, args=(parent_pid, stop_event))
     thread.start()
