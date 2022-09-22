945a1d199e3271be54465ee1e13aa975a7ff89f6
Holger Roth <6304754+holgerroth@users.noreply.github.com>
2022-09-21 20:05:22 -0400

    update dependency versions (#907)

--- integration/monai/setup.py
+++ integration/monai/setup.py
@@ -23,14 +23,14 @@ with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
 release = os.environ.get("MONAI_NVFL_RELEASE")
 if release == "1":
     package_name = "monai-nvflare"
-    version = "0.1.0"
+    version = "0.2.0"
 else:
     package_name = "monai-nvflare-nightly"
     today = datetime.date.today().timetuple()
     year = today[0] % 1000
     month = today[1]
     day = today[2]
-    version = f"0.1.0.{year:02d}{month:02d}{day:02d}"
+    version = f"0.2.0.{year:02d}{month:02d}{day:02d}"
 
 setup(
     name=package_name,
@@ -55,5 +55,5 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires=">=3.7,<3.9",
-    install_requires=["monai==1.0.0rc1", "nvflare"],
+    install_requires=["monai==1.0.0", "nvflare>=2.2.0"],
 )
