3bf0afa69f99a528e723fb99face80dec22b574f
Ziyue Xu <ziyue.xu@gmail.com>
2022-09-24 17:08:01 -0400

    convert to simulator with latest performance updates

--- examples/xgboost/job_config_gen.sh
+++ examples/xgboost/job_config_gen.sh
@@ -7,8 +7,8 @@ python3 utils/prepare_job_config.py --site_num 5 --training_mode bagging --split
 python3 utils/prepare_job_config.py --site_num 5 --training_mode cyclic --split_method exponential --lr_mode uniform --nthread 16 --tree_method $TREE_METHOD
 python3 utils/prepare_job_config.py --site_num 5 --training_mode cyclic --split_method uniform --lr_mode uniform --nthread 16 --tree_method $TREE_METHOD
 
-python3 utils/prepare_job_config.py --site_num 20 --training_mode bagging --split_method square --lr_mode scaled --nthread 4 --tree_method $TREE_METHOD
-python3 utils/prepare_job_config.py --site_num 20 --training_mode bagging --split_method square --lr_mode uniform --nthread 4 --tree_method $TREE_METHOD
-python3 utils/prepare_job_config.py --site_num 20 --training_mode bagging --split_method uniform --lr_mode uniform --nthread 4 --tree_method $TREE_METHOD
-python3 utils/prepare_job_config.py --site_num 20 --training_mode cyclic --split_method square --lr_mode uniform --nthread 4 --tree_method $TREE_METHOD
-python3 utils/prepare_job_config.py --site_num 20 --training_mode cyclic --split_method uniform --lr_mode uniform --nthread 4 --tree_method $TREE_METHOD
+python3 utils/prepare_job_config.py --site_num 20 --training_mode bagging --split_method square --lr_mode scaled --nthread 16 --tree_method $TREE_METHOD
+python3 utils/prepare_job_config.py --site_num 20 --training_mode bagging --split_method square --lr_mode uniform --nthread 16 --tree_method $TREE_METHOD
+python3 utils/prepare_job_config.py --site_num 20 --training_mode bagging --split_method uniform --lr_mode uniform --nthread 16 --tree_method $TREE_METHOD
+python3 utils/prepare_job_config.py --site_num 20 --training_mode cyclic --split_method square --lr_mode uniform --nthread 16 --tree_method $TREE_METHOD
+python3 utils/prepare_job_config.py --site_num 20 --training_mode cyclic --split_method uniform --lr_mode uniform --nthread 16 --tree_method $TREE_METHOD
--- examples/xgboost/utils/plot_tensorboard_events.py
+++ examples/xgboost/utils/plot_tensorboard_events.py
@@ -19,33 +19,33 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import tensorflow as tf
 
-# poc workspace
+# simulator workspace
 client_results_root = "./workspaces/xgboost_workspace_"
 client_num_list = [5, 20]
-client_pre = "site-"
+client_pre = "app_site-"
 centralized_path = "./workspaces/centralized_1_1/events.*"
 
 # bagging and cyclic need different handle
 experiments_bagging = {
     5: {
-        "higgs_5_bagging_uniform_split_uniform_lr": {"tag": "AUC"},
-        "higgs_5_bagging_exponential_split_uniform_lr": {"tag": "AUC"},
-        "higgs_base": {"tag": "AUC"},
+        "5_bagging_uniform_split_uniform_lr": {"tag": "AUC"},
+        "5_bagging_exponential_split_uniform_lr": {"tag": "AUC"},
+        "5_bagging_exponential_split_scaled_lr": {"tag": "AUC"}
     },
     20: {
-        "higgs_20_bagging_uniform_split_uniform_lr": {"tag": "AUC"},
-        "higgs_20_bagging_square_split_uniform_lr": {"tag": "AUC"},
-        "higgs_20_bagging_square_split_scaled_lr": {"tag": "AUC"},
+        "20_bagging_uniform_split_uniform_lr": {"tag": "AUC"},
+        "20_bagging_square_split_uniform_lr": {"tag": "AUC"},
+        "20_bagging_square_split_scaled_lr": {"tag": "AUC"},
     },
 }
 experiments_cyclic = {
     5: {
-        "higgs_5_cyclic_uniform_split_uniform_lr": {"tag": "AUC"},
-        "higgs_5_cyclic_exponential_split_uniform_lr": {"tag": "AUC"},
+        "5_cyclic_uniform_split_uniform_lr": {"tag": "AUC"},
+        "5_cyclic_exponential_split_uniform_lr": {"tag": "AUC"},
     },
     20: {
-        "higgs_20_cyclic_uniform_split_uniform_lr": {"tag": "AUC"},
-        "higgs_20_cyclic_square_split_uniform_lr": {"tag": "AUC"},
+        "20_cyclic_uniform_split_uniform_lr": {"tag": "AUC"},
+        "20_cyclic_square_split_uniform_lr": {"tag": "AUC"},
     },
 }
 
@@ -60,20 +60,6 @@ def smooth(scalars, weight):  # Weight between 0 and 1
         smoothed.append(smoothed_val)  # Save it
         last = smoothed_val  # Anchor the last smoothed value
     return smoothed
-
-
-def find_job_id(workdir, fl_app_name="xgboost"):
-    """Find the first matching experiment"""
-    target_path = os.path.join(workdir, "*", "fl_app.txt")
-    fl_app_files = glob.glob(target_path, recursive=True)
-    assert len(fl_app_files) > 0, f"No `fl_app.txt` files found in workdir={workdir}."
-    for fl_app_file in fl_app_files:
-        with open(fl_app_file, "r") as f:
-            _fl_app_name = f.read()
-        if fl_app_name == _fl_app_name:  # alpha will be matched based on value in config file
-            job_id = os.path.basename(os.path.dirname(fl_app_file))
-            return job_id
-    raise ValueError(f"No job id found for fl_app_name={fl_app_name} in workdir={workdir}")
 
 
 def read_eventfile(filepath, tags=["AUC"]):
@@ -119,16 +105,12 @@ def main():
         assert len(eventfile) == 1, "No unique event file found!" + eventfile
         eventfile = eventfile[0]
         print("adding", eventfile)
-        add_eventdata(data, "higgs_centralized", eventfile, tag="AUC")
-        # Pick first client for bagging experiments
+        add_eventdata(data, "centralized", eventfile, tag="AUC")
+        # pick first client for bagging experiments
         site = 1
         for config, exp in experiments_bagging[client_num].items():
-            job_id = find_job_id(
-                workdir=os.path.join(client_results_root + str(client_num), client_pre + str(site)), fl_app_name=config
-            )
-            print(f"Found run {job_id} for {config}")
             record_path = os.path.join(
-                client_results_root + str(client_num), client_pre + str(site), job_id, "*", "events.*"
+                client_results_root + config, "simulate_job", client_pre + str(site), "events.*"
             )
             eventfile = glob.glob(record_path, recursive=True)
             assert len(eventfile) == 1, "No unique event file found!"
@@ -139,16 +121,11 @@ def main():
         # Combine all clients' records for cyclic experiments
         for site in range(1, client_num + 1):
             for config, exp in experiments_cyclic[client_num].items():
-                job_id = find_job_id(
-                    workdir=os.path.join(client_results_root + str(client_num), client_pre + str(site)),
-                    fl_app_name=config,
-                )
-                print(f"Found run {job_id} for {config}")
                 record_path = os.path.join(
-                    client_results_root + str(client_num), client_pre + str(site), job_id, "*", "events.*"
+                    client_results_root + config, "simulate_job", client_pre + str(site), "events.*"
                 )
                 eventfile = glob.glob(record_path, recursive=True)
-                assert len(eventfile) == 1, "No unique event file found!"
+                assert len(eventfile) == 1, f"No unique event file found under {record_path}!"
                 eventfile = eventfile[0]
                 print("adding", eventfile)
                 add_eventdata(data, config, eventfile, tag=exp["tag"])
