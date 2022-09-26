2d73636d4bb887521e044582154d0be30fe4b869
Isaac Yang <isaacy@nvidia.com>
2022-09-26 12:27:39 -0700

    Switch to 2.2.1.devYYMMDD format for nightly build

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ if versions["error"]:
     year = today[0] % 1000
     month = today[1]
     day = today[2]
-    version = f"0.0.{year:02d}{month:02d}{day:02d}"
+    version = f"2.2.1.dev{year:02d}{month:02d}{day:02d}"
 else:
     version = versions["version"]
 
