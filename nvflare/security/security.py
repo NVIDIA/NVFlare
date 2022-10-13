5bc65f2c1548cda332ec306a5c44e58e5e4dbfb6
YuanTingHsieh <yuantinghsieh@gmail.com>
2022-10-13 12:27:56 -0700

    Undo changes to nvflare

--- nvflare/security/security.py
+++ nvflare/security/security.py
@@ -53,19 +53,18 @@ COMMAND_CATEGORIES = {
 
 
 class FLAuthorizer(Authorizer):
-    def __init__(self, site_org: str, policy_config: dict):
+    def __init__(self, for_org: str, policy_config: dict):
         """System-wide authorization class.
 
         Examine if a user has certain rights on a specific site
         based on authorization.json file.
 
         """
-        if not isinstance(policy_config, dict):
-            raise ValueError(f"policy_config must be a dict but got {type(policy_config)}")
-        Authorizer.__init__(self, site_org, COMMAND_CATEGORIES)
+        assert isinstance(policy_config, dict), "policy_config must be a dict but got {}".format(type(policy_config))
+        Authorizer.__init__(self, for_org, COMMAND_CATEGORIES)
         err = self.load_policy(policy_config)
         if err:
-            raise ValueError(f"invalid policy config: {err}")
+            raise SyntaxError("invalid policy config: {}".format(err))
 
 
 class EmptyAuthorizer(Authorizer):
