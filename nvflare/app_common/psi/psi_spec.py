e79328a9fbe3409eee85758ff6e74523dc4fa148
chesterxgchen <chesterxgchen@yahoo.com>
2022-12-21 19:13:02 -0800

    update

--- nvflare/app_common/psi/psi_spec.py
+++ nvflare/app_common/psi/psi_spec.py
@@ -24,14 +24,21 @@ from nvflare.app_common.utils.component_utils import check_component_type
 class PSI(FLComponent, ABC):
     """
     PSI interface is intended for end-user interface to
-    get intersect without knowing the details of PSI algorithms, which will be delegated to the PSIHandler.
+    get intersection of items of clients specified without knowing
+    1) the details of PSI algorithms
+    2) real client's own items to other FL clients and FL Servers
     """
-
     def __init__(self, psi_writer_id: str):
+        """
+        Args:
+            psi_writer_id: psi write that will save result to provide the store if provide. For example, FilePsiWriter
+            will implement PsiPersistor interface and save to the local disk
+        """
         super().__init__()
         self.psi_writer_id = psi_writer_id
         self.psi_writer: Optional[PsiPersistor] = None
         self.fl_ctx = None
+        self.intersection: Optional[List[str]] = None
 
     def initialize(self, fl_ctx: FLContext):
         self.fl_ctx = fl_ctx
@@ -42,7 +49,23 @@ class PSI(FLComponent, ABC):
 
     @abstractmethod
     def load_items(self) -> List[str]:
+        """
+            This method needs to be implemented to provide the list of items to PSI algorithm in order to
+            calculate intersection.
+
+        Returns: List of Items to be used for intersection calculation
+
+        """
         pass
 
-    def save(self, intersections: List[str]):
-        self.psi_writer.save(items=intersections, overwrite_existing=True, fl_ctx=self.fl_ctx)
+    def get_intersection(self) -> Optional[List[str]]:
+        """
+            This method will return the calculated intersection once PSI job is completed and successful
+        Returns: Intersection result or None
+        """
+        return self.intersection
+
+    def save(self, intersection: List[str]):
+        self.intersection = intersection
+        if self.psi_writer:
+            self.psi_writer.save(items=intersection, overwrite_existing=True, fl_ctx=self.fl_ctx)
