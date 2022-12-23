c5ea077d018abc573c193794385db366d3df3837
chesterxgchen <chesterxgchen@yahoo.com>
2022-12-23 00:01:14 -0800

    1. Change controller. 2. add multicasts_and_wait
    Working in progress

--- nvflare/app_common/workflows/broadcast_operator.py
+++ nvflare/app_common/workflows/broadcast_operator.py
@@ -31,26 +31,58 @@ class BroadcastAndWait(FLComponent, ABC):
         self.fl_ctx = fl_ctx
         self.controller = controller
         self.task = None
-        self.results: Dict[str, DXO] = {}
+
+        # [<input_id, [client_id, DXO]]
+        self.results: Dict[str, Dict[str, DXO]] = {}
 
     def broadcast_and_wait(
-        self,
-        task_name: str,
-        task_props: dict,
-        inputs: Shareable,
-        targets: Union[List[Client], List[str], None] = None,
-        min_responses: int = 0,
-        abort_signal: Signal = None,
+            self,
+            task_name: str,
+            task_props: dict,
+            task_input: Shareable,
+            targets: Union[List[Client], List[str], None] = None,
+            min_responses: int = 0,
+            abort_signal: Signal = None,
     ) -> Dict[str, DXO]:
+        if task_props:
+            task_props["input_id"] = task_name
+        else:
+            task_props = {"input_id": task_name}
 
-        task = Task(name=task_name, data=inputs, result_received_cb=self.results_cb, props=task_props)
-        self.task = task
+        task = Task(name=task_name, data=task_input, result_received_cb=self.results_cb, props=task_props)
         self.controller.broadcast_and_wait(task, self.fl_ctx, targets, min_responses, 0, abort_signal)
+
+        return self.results[task_name]
+
+    def multi_broadcasts_and_wait(
+            self,
+            task_name: str,
+            task_inputs: Dict[str, Shareable],
+            targets: Union[List[Client], List[str], None] = None,
+            min_responses: int = 0,
+            abort_signal: Signal = None,
+    ) -> Dict[str, Dict[str, DXO]]:
+
+        tasks = self.get_tasks(task_name, task_inputs)
+        for task in tasks:
+            self.controller.broadcast(task, self.fl_ctx, targets, min_responses, 0)
+        for task in tasks:
+            self.controller.wait_for_task(task, abort_signal)
+
         return self.results
+
+    def get_tasks(self, task_name, task_inputs):
+        tasks = []
+        for _input in task_inputs:
+            task_props = {"input_id": _input}
+            task = Task(name=task_name, data=task_inputs[_input], result_received_cb=self.results_cb, props=task_props)
+            tasks.append(task)
+        return tasks
 
     def results_cb(self, client_task: ClientTask, fl_ctx: FLContext):
         client_name = client_task.client.name
         task_name = client_task.task.name
+        input_id = client_task.props["input_id"]
 
         self.log_info(fl_ctx, f"Processing {task_name}, {self.task} result from client {client_name}")
         result = client_task.result
@@ -59,7 +91,10 @@ class BroadcastAndWait(FLComponent, ABC):
         if rc == ReturnCode.OK:
             self.log_info(fl_ctx, f"Received result from client:{client_name} for task {task_name} ")
             dxo = from_shareable(result)
-            self.results.update({client_name: dxo})
+            if input_id in self.results:
+                self.results[input_id].update({client_name: dxo})
+            else:
+                self.results[input_id] = {client_name: dxo}
         else:
             if rc in self.controller.abort_job_in_error.keys():
                 self.controller.handle_client_errors(rc, client_task, fl_ctx)
