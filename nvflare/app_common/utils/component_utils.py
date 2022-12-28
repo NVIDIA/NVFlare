0b86cafb687b7e3b47ef419e2a5745d902a59628
chesterxgchen <chesterxgchen@yahoo.com>
2022-12-27 21:00:29 -0800

    1) Add common_executor.py to help handle many boiler plate codes * events start_run/end_run * check initialization error * handle abort signal * change DXO to sharable
    2) add client_executor.py to focused on the core exec function (client_exec())

--- nvflare/app_common/utils/component_utils.py
+++ nvflare/app_common/utils/component_utils.py
@@ -11,27 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from abc import ABC, abstractmethod
-
-from nvflare.apis.fl_component import FLComponent
-from nvflare.apis.fl_context import FLContext
 
 
-class InitFinalComponent(FLComponent, ABC):
-    @abstractmethod
-    def initialize(self, fl_ctx: FLContext):
-        pass
-
-    @abstractmethod
-    def finalize(self):
-        pass
-
-
-class InitFinalArgsComponent(InitFinalComponent, ABC):
-    @abstractmethod
-    def initialize(self, fl_ctx: FLContext, **kwargs):
-        pass
-
-    @abstractmethod
-    def finalize(self):
-        pass
+def check_component_type(comp, t):
+    if not isinstance(comp, t):
+        raise TypeError(f"{type(comp).__name__} must implement `{t}` type. Got: {type(comp)}")
