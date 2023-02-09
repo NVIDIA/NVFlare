fba5992d7a5c78f6a3b095bc0a87b6d616ce7a75
Ziyue Xu <71786575+ZiyueXu77@users.noreply.github.com>
2023-02-09 18:40:09 -0500

    Add non-linear SVM example (#1176)
    * Initial functional FL SVM draft
    
    * code refactor
    
    * Refactor for abstraction, data access, and readme
    
    * Refactor for abstraction, data access, and readme
    
    * Refactor SVM executor to cover all sklearn apps
    
    * Further refactor on executor
    
    * Finalized and sync changes across all three sklearn examples
    
    * extend the data split function
    
    * Apply the same refactor as KMeans example
    
    * Add env preparation to readme
    
    * reorganize folder
    
    * add system_panic for num_rounds>2
    
    * add system_panic for num_rounds>2
    
    ---------
    
    Co-authored-by: Yuan-Ting Hsieh (謝沅廷) <yuantingh@nvidia.com>

---
+++ examples/advanced/sklearn-svm/run_experiment_simulator.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+task_name="sklearn_svm"
+n=3
+for study in uniform
+do
+    nvflare simulator job_configs/${task_name}_${n}_${study} -w ${PWD}/workspaces/${task_name}_${n}_${study} -n ${n} -t ${n}
+done
