8369f155f7c111e869d93303315cf7fa4f0c3052
Zhihong Zhang <zhihongz@nvidia.com>
2023-02-15 11:21:39 -0500

    fix

--- nvflare/fuel/f3/drivers/socket_conn.py
+++ nvflare/fuel/f3/drivers/socket_conn.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import logging
+import socket
 from socketserver import BaseRequestHandler
 from typing import Any
 
