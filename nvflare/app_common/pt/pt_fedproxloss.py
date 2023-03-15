18baa2d2a289701416b4b50a7be464744096838e
Yuan-Ting Hsieh (謝沅廷) <yuantingh@nvidia.com>
2023-03-15 10:28:04 -0700

    Remove things in __init__.py in app_opt (#1508)

--- nvflare/app_common/pt/pt_fedproxloss.py
+++ nvflare/app_common/pt/pt_fedproxloss.py
@@ -15,8 +15,10 @@
 import warnings
 
 warnings.warn(
-    f"This module: {__file__} is deprecated. Please use nvflare.app_opt.pt.", category=FutureWarning, stacklevel=2
+    f"This module: {__file__} is deprecated. Please use nvflare.app_opt.pt.fedproxloss",
+    category=FutureWarning,
+    stacklevel=2,
 )
 
 # flake8: noqa: F401
-from nvflare.app_opt.pt import PTFedProxLoss
+from nvflare.app_opt.pt.fedproxloss import PTFedProxLoss
