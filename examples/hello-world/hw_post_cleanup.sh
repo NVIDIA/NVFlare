a691bd2a9af5f74ae9a26bea7a00723f8b464bfa
chesterxgchen <chesterxgchen@yahoo.com>
2023-03-24 11:37:59 -0700

    remove clean up directories

--- examples/hello-world/hw_post_cleanup.sh
+++ examples/hello-world/hw_post_cleanup.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/env bash
 
 nvflare poc --stop
-sleep 3
-
-if [ -f "/tmp/nvflare/jobs-storage" ]; then
-  rm -r "/tmp/nvflare/jobs-storage"
-fi
-
-if [ -f "/tmp/nvflare/snapshot-storage" ]; then
-  rm -r "/tmp/nvflare/snapshot-storage"
-fi
