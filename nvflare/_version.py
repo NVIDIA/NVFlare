d08a8149a31f21107673eb81f5dee5a39070d47a
YuanTingHsieh <yuantingh@nvidia.com>
2023-04-03 17:17:47 -0700

    Fix issues

--- nvflare/_version.py
+++ nvflare/_version.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
+# This file helps to compute a version number in source trees obtained from
 # git-archive tarball (such as those provided by githubs download-from-tag
 # feature). Distribution tarballs (built by setup.py sdist) and build
 # directories (produced by setup.py build) will contain a much shorter file
