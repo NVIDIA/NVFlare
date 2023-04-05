62f431532c50c7cd577e4459256697425109c6a3
Chester Chen <512707+chesterxgchen@users.noreply.github.com>
2023-04-05 11:03:15 -0700

    notebooks tweaks [skip ci] (#1651)
    * upgrade notebooks
    
    * update notebooks
    
    * update notebooks
    
    * update notebooks
    
    * update notebooks
    
    ---------
    
    Co-authored-by: chesterc <n9Z0GoPp5u1Y>

--- examples/advanced/federated-statistics/image_stats/utils/prepare_data.py
+++ examples/advanced/federated-statistics/image_stats/utils/prepare_data.py
@@ -52,6 +52,23 @@ def save_data_list(data, data_list_file, data_root, key="data"):
 
     print(f"Saved {len(data_list)} entries at {data_list_file}")
 
+def prepare_data(input_dir: str,
+                 input_ext:str = ".png",
+                 output_dir :str = "/tmp/nvflare/image_stats/data",
+                 sub_dirs: str = "COVID,Lung_Opacity,Normal,Viral Pneumonia"):
+    sub_dir_list = [sd for sd in sub_dirs.split(",")]
+
+    data_lists = create_datasets(
+        root=input_dir, subdirs=sub_dir_list, extension=input_ext, shuffle=True, seed=SEED
+    )
+    print(f"Created {len(data_lists)} data lists for {sub_dir_list}.")
+
+    site_id = 1
+    for subdir, data_list in zip(sub_dir_list, data_lists):
+        save_data_list(
+            data_list, os.path.join(output_dir, f"site-{site_id}_{subdir}.json"), data_root=input_dir
+        )
+        site_id += 1
 
 def main():
     parser = argparse.ArgumentParser()
@@ -69,19 +86,9 @@ def main():
     args = parser.parse_args()
 
     assert "," in args.subdirs, "Expecting a comma separated list of subdirs names"
-    subdirs = [sd for sd in args.subdirs.split(",")]
 
-    data_lists = create_datasets(
-        root=args.input_dir, subdirs=subdirs, extension=args.input_ext, shuffle=True, seed=SEED
-    )
-    print(f"Created {len(data_lists)} data lists for {subdirs}.")
+    prepare_data(args.input_dir, args.input_ext, args.output_dir, args.subdirs)
 
-    site_id = 1
-    for subdir, data_list in zip(subdirs, data_lists):
-        save_data_list(
-            data_list, os.path.join(args.output_dir, f"site-{site_id}_{subdir}.json"), data_root=args.input_dir
-        )
-        site_id += 1
 
 
 if __name__ == "__main__":
