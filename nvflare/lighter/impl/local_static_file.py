b2a46a616dbe33377c4424f5f9a5b1c939a63f86
Chester Chen <512707+chesterxgchen@users.noreply.github.com>
2023-05-23 16:08:47 -0700

    Upgrade POC: changing POC mode to Local Prod Mode, but keep POC command name.  (#1755)
    * working in progress
    
    * All commands worked except --docker.
    
    Need to re-write the docker.sh
    
    * All commands worked except --docker.
    
    Need to re-write the docker.sh
    
    * Add docker_builder
    
    * code complete needs more tests.
    
    * temp notebook
    
    * temp notebook
    
    * finish docker poc support
    
    * finish docker poc support
    
    * cleanup some loose end
    
    * update
    
    * update
    
    * cleanup
    
    * cleanup
    
    * change "-e" syntax to allow it used in both --prepare and --prepare command
    
    * 1) remove global_package global variable
    2) add unit tests
    3) replace ValueError with CLIException, so it won't print out the help messages
    
    * style format
    
    * update license year
    
    * rename "package" to "service"

---
+++ nvflare/lighter/impl/local_static_file.py
@@ -0,0 +1,71 @@
+# Copyright (c) 2023, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from nvflare.lighter.impl.static_file import StaticFileBuilder
+
+
+class LocalStaticFileBuilder(StaticFileBuilder):
+    def __init__(
+        self,
+        enable_byoc=False,
+        config_folder="",
+        scheme="grpc",
+        app_validator="",
+        download_job_url="",
+        docker_image="",
+        snapshot_persistor="",
+        overseer_agent="",
+        components="",
+        username="",
+    ):
+        """Build all static files from template.
+
+        Uses the information from project.yml through project to go through the participants and write the contents of
+        each file with the template, and replacing with the appropriate values from project.yml.
+
+        Usually, two main categories of files are created in all FL participants, static and dynamic. Static files
+        have similar contents among different participants, with small differences.  For example, the differences in
+        sub_start.sh are client name and python module.  Those are basically static files.  This builder uses template
+        file and string replacement to generate those static files for each participant.
+
+        Args:
+            enable_byoc: for each participant, true to enable loading of code in the custom folder of applications
+            config_folder: usually "config"
+            app_validator: optional path to an app validator to verify that uploaded app has the expected structure
+            docker_image: when docker_image is set to a docker image name, docker.sh will be generated on server/client/admin
+        """
+        super().__init__(
+            enable_byoc,
+            config_folder,
+            scheme,
+            app_validator,
+            download_job_url,
+            docker_image,
+            snapshot_persistor,
+            overseer_agent,
+            components,
+        )
+        self.username = username
+
+    def get_server_name(self, server):
+        return "localhost"
+
+    def get_overseer_name(self, overseer):
+        return "localhost"
+
+    def prepare_admin_config(self, admin, ctx):
+        config = super().prepare_admin_config(admin, ctx)
+        config["admin"]["username"] = self.username
+        config["admin"]["cred_type"] = "local_cert"
+        return config
