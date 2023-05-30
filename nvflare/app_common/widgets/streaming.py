5a2280b6d16b09e765be479a93d2e095fa907e47
Kevin <kevlu@nvidia.com>
2023-05-30 19:39:41 -0400

    fix ci

--- nvflare/app_common/widgets/streaming.py
+++ nvflare/app_common/widgets/streaming.py
@@ -15,6 +15,7 @@
 from abc import ABC, abstractmethod
 from threading import Lock
 from typing import List, Optional
+
 from typing_extensions import deprecated
 
 from nvflare.apis.analytix import AnalyticsData, AnalyticsDataType
@@ -111,7 +112,9 @@ class AnalyticsSender(Widget):
         with self.engine.new_context() as fl_ctx:
             send_analytic_dxo(self, dxo=dxo, fl_ctx=fl_ctx, event_type=self.event_type)
 
-    @deprecated("This method is deprecated, please use :py:class:`TBWriter <nvflare.app_opt.tracking.tb.tb_writer.TBWriter>` instead.")
+    @deprecated(
+        "This method is deprecated, please use :py:class:`TBWriter <nvflare.app_opt.tracking.tb.tb_writer.TBWriter>` instead."
+    )
     def add_scalar(self, tag: str, scalar: float, global_step: Optional[int] = None, **kwargs):
         """Legacy method to send a scalar.
 
@@ -126,7 +129,9 @@ class AnalyticsSender(Widget):
         """
         self.add(tag=tag, value=scalar, data_type=AnalyticsDataType.SCALAR, global_step=global_step, **kwargs)
 
-    @deprecated("This method is deprecated, please use :py:class:`TBWriter <nvflare.app_opt.tracking.tb.tb_writer.TBWriter>` instead.")
+    @deprecated(
+        "This method is deprecated, please use :py:class:`TBWriter <nvflare.app_opt.tracking.tb.tb_writer.TBWriter>` instead."
+    )
     def add_scalars(self, tag: str, scalars: dict, global_step: Optional[int] = None, **kwargs):
         """Legacy method to send scalars.
 
@@ -141,7 +146,9 @@ class AnalyticsSender(Widget):
         """
         self.add(tag=tag, value=scalars, data_type=AnalyticsDataType.SCALARS, global_step=global_step, **kwargs)
 
-    @deprecated("This method is deprecated, please use :py:class:`TBWriter <nvflare.app_opt.tracking.tb.tb_writer.TBWriter>` instead.")
+    @deprecated(
+        "This method is deprecated, please use :py:class:`TBWriter <nvflare.app_opt.tracking.tb.tb_writer.TBWriter>` instead."
+    )
     def flush(self):
         """Legacy method to flush out the message.
 
