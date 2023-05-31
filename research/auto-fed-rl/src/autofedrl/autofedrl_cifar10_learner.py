67659436c37824c3ead0a492993bad817162478c
Yan Cheng <yanc@nvidia.com>
2023-05-31 12:43:00 -0400

    revert learner executor

--- research/auto-fed-rl/src/autofedrl/autofedrl_cifar10_learner.py
+++ research/auto-fed-rl/src/autofedrl/autofedrl_cifar10_learner.py
@@ -19,17 +19,17 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
-
-from typing import Union
-
 from pt.learners.cifar10_learner import CIFAR10Learner
 from pt.utils.cifar10_data_utils import CIFAR10_ROOT
 from pt.utils.cifar10_dataset import CIFAR10_Idx
 from torchvision import datasets
 
-from nvflare.apis.dxo import DXO, DataKind, MetaKey
+from nvflare.apis.dxo import DXO, DataKind, MetaKey, from_shareable
 from nvflare.apis.fl_constant import ReturnCode
-from nvflare.app_common.app_constant import ValidateType
+from nvflare.apis.fl_context import FLContext
+from nvflare.apis.shareable import ReservedHeaderKey, Shareable, make_reply
+from nvflare.apis.signal import Signal
+from nvflare.app_common.app_constant import AppConstants, ValidateType
 from nvflare.app_opt.pt.decomposers import TensorDecomposer
 from nvflare.fuel.utils import fobs
 
@@ -91,13 +91,13 @@ class CIFAR10AutoFedRLearner(CIFAR10Learner):  # TODO: also support CIFAR10Scaff
         # Use FOBS serializing/deserializing PyTorch tensors
         fobs.register(TensorDecomposer)
 
-    def initialize(self):
+    def initialize(self, parts: dict, fl_ctx: FLContext):
         # Initialize super class
-        CIFAR10Learner.initialize(self)
+        CIFAR10Learner.initialize(self, parts=parts, fl_ctx=fl_ctx)
         # Enabling the Nesterov momentum can stabilize the training.
         self.optimizer = optim.SGD(self.model.parameters(), lr=self.lr, momentum=0.9, weight_decay=0.0, nesterov=True)
 
-    def _create_datasets(self):
+    def _create_datasets(self, fl_ctx: FLContext):
         """To be called only after Cifar10DataSplitter downloaded the data and computed splits"""
 
         if self.train_dataset is None or self.train_loader is None:
@@ -105,18 +105,18 @@ class CIFAR10AutoFedRLearner(CIFAR10Learner):  # TODO: also support CIFAR10Scaff
             if not self.central:
                 # Set datalist, here the path and filename are hard-coded, can also be fed as an argument
                 site_idx_file_name = os.path.join(self.train_idx_root, self.client_id + ".npy")
-                self.info(f"IndexList Path: {site_idx_file_name}")
+                self.log_info(fl_ctx, f"IndexList Path: {site_idx_file_name}")
                 if os.path.exists(site_idx_file_name):
-                    self.info("Loading subset index")
+                    self.log_info(fl_ctx, "Loading subset index")
                     site_idx = np.load(site_idx_file_name).tolist()  # TODO: get from fl_ctx/shareable?
                 else:
-                    self.stop_run(f"No subset index found! File {site_idx_file_name} does not exist!")
+                    self.system_panic(f"No subset index found! File {site_idx_file_name} does not exist!", fl_ctx)
                     return
-                self.info(f"Client subset size: {len(site_idx)}")
+                self.log_info(fl_ctx, f"Client subset size: {len(site_idx)}")
             else:
                 site_idx = None  # use whole training dataset if self.central=True
 
-            self.debug(f"site_idx: {site_idx}")
+            self.log_debug(fl_ctx, msg)(fl_ctx, f"site_idx: {site_idx}")
 
             # Train set
             n_img_for_search = self.batch_size * 10
@@ -141,7 +141,8 @@ class CIFAR10AutoFedRLearner(CIFAR10Learner):  # TODO: also support CIFAR10Scaff
             self.val_loader_search = torch.utils.data.DataLoader(
                 self.val_dataset_for_search, batch_size=self.batch_size, shuffle=False
             )
-            self.info(
+            self.log_info(
+                fl_ctx,
                 f"Split ({n_img_for_search}) images from {len(site_idx)} training images for Hyerparamters Search",
             )
 
@@ -156,17 +157,17 @@ class CIFAR10AutoFedRLearner(CIFAR10Learner):  # TODO: also support CIFAR10Scaff
                 self.valid_dataset, batch_size=self.batch_size, shuffle=False
             )
 
-    def local_train(self, train_loader, model_global, val_freq: int = 0):
+    def local_train(self, fl_ctx, train_loader, model_global, abort_signal: Signal, val_freq: int = 0):
         for epoch in range(self.aggregation_epochs):
-            if self.is_aborted():
+            if abort_signal.triggered:
                 return
             self.model.train()
             epoch_len = len(train_loader)
             self.epoch_global = self.epoch_of_start_time + epoch
-            self.info(f"Local epoch {self.client_id}: {epoch + 1}/{self.aggregation_epochs} (lr={self.lr})")
+            self.log_info(fl_ctx, f"Local epoch {self.client_id}: {epoch + 1}/{self.aggregation_epochs} (lr={self.lr})")
             avg_loss = 0.0
             for i, (inputs, labels) in enumerate(train_loader):
-                if self.is_aborted():
+                if abort_signal.triggered:
                     return
                 inputs, labels = inputs.to(self.device), labels.to(self.device)
                 # Forward
@@ -194,25 +195,28 @@ class CIFAR10AutoFedRLearner(CIFAR10Learner):  # TODO: also support CIFAR10Scaff
                 avg_loss += loss.item()
             self.writer.add_scalar("train_loss", avg_loss / len(train_loader), current_step)
             if val_freq > 0 and epoch % val_freq == 0:
-                acc = self.local_valid(self.valid_loader, tb_id="val_acc_local_model")
+                acc = self.local_valid(self.valid_loader, abort_signal, tb_id="val_acc_local_model", fl_ctx=fl_ctx)
                 if acc > self.best_acc:
                     self.best_acc = acc
                     self.save_model(is_best=True)
 
-    def train(self, dxo: DXO) -> Union[str, DXO]:
-        self._create_datasets()
+    def train(self, shareable: Shareable, fl_ctx: FLContext, abort_signal: Signal) -> Shareable:
+        self._create_datasets(fl_ctx)
 
         # Check abort signal
-        if self.is_aborted():
-            return ReturnCode.TASK_ABORTED
+        if abort_signal.triggered:
+            return make_reply(ReturnCode.TASK_ABORTED)
 
         # Get round information
-        self.info(f"Current/Total Round: {self.current_round + 1}/{self.total_rounds}")
-        self.info(f"Client identity: {self.site_name}")
+        current_round = shareable.get_header(AppConstants.CURRENT_ROUND)
+        total_rounds = shareable.get_header(AppConstants.NUM_ROUNDS)
+        self.log_info(fl_ctx, f"Current/Total Round: {current_round + 1}/{total_rounds}")
+        self.log_info(fl_ctx, f"Client identity: {fl_ctx.get_identity_name()}")
+        self.current_round = current_round
 
         # Get lr and ne from server
         current_lr, current_ne = None, None
-        hps = fobs.loads(self.get_shareable_header(AutoFedRLConstants.HYPERPARAMTER_COLLECTION))
+        hps = fobs.loads(shareable.get_header(AutoFedRLConstants.HYPERPARAMTER_COLLECTION))
         if hps is not None:
             current_lr = hps.get("lr")
             self.lr = current_lr
@@ -220,12 +224,13 @@ class CIFAR10AutoFedRLearner(CIFAR10Learner):  # TODO: also support CIFAR10Scaff
         if current_lr is not None:
             for param_group in self.optimizer.param_groups:
                 param_group["lr"] = current_lr
-            self.info(f"Received and override current learning rate as: {current_lr}")
+            self.log_info(fl_ctx, f"Received and override current learning rate as: {current_lr}")
         if current_ne is not None:
             self.aggregation_epochs = current_ne
-            self.info(f"Received and override current number of local epochs: {current_ne}")
+            self.log_info(fl_ctx, f"Received and override current number of local epochs: {current_ne}")
 
         # Update local model weights with received weights
+        dxo = from_shareable(shareable)
         global_weights = dxo.data
 
         # Before loading weights, tensors might need to be reshaped to support HE for secure aggregation.
@@ -245,7 +250,7 @@ class CIFAR10AutoFedRLearner(CIFAR10Learner):  # TODO: also support CIFAR10Scaff
 
         # Local steps
         epoch_len = len(self.train_loader)
-        self.info(f"Local steps per epoch: {epoch_len}")
+        self.log_info(fl_ctx, f"Local steps per epoch: {epoch_len}")
 
         # Make a copy of model_global as reference for potential FedProx loss or SCAFFOLD
         model_global = copy.deepcopy(self.model)
@@ -254,20 +259,21 @@ class CIFAR10AutoFedRLearner(CIFAR10Learner):  # TODO: also support CIFAR10Scaff
 
         # Local train
         self.local_train(
+            fl_ctx=fl_ctx,
             train_loader=self.train_loader,
             model_global=model_global,
+            abort_signal=abort_signal,
             val_freq=1 if self.central else 0,
         )
-        if self.is_aborted():
-            return ReturnCode.TASK_ABORTED
-
+        if abort_signal.triggered:
+            return make_reply(ReturnCode.TASK_ABORTED)
         self.epoch_of_start_time += self.aggregation_epochs
 
         # Perform valid after local train
-        acc = self.local_valid(self.valid_loader, tb_id="val_acc_local_model")
-        if self.is_aborted():
-            return ReturnCode.TASK_ABORTED
-        self.info(f"val_acc_local_model: {acc:.4f}")
+        acc = self.local_valid(self.valid_loader, abort_signal, tb_id="val_acc_local_model", fl_ctx=fl_ctx)
+        if abort_signal.triggered:
+            return make_reply(ReturnCode.TASK_ABORTED)
+        self.log_info(fl_ctx, f"val_acc_local_model: {acc:.4f}")
 
         # Save model
         self.save_model(is_best=False)
@@ -283,8 +289,8 @@ class CIFAR10AutoFedRLearner(CIFAR10Learner):  # TODO: also support CIFAR10Scaff
                 continue
             model_diff[name] = np.subtract(local_weights[name].cpu().numpy(), global_weights[name], dtype=np.float32)
             if np.any(np.isnan(model_diff[name])):
-                self.panic(f"{name} weights became NaN...")
-                return ReturnCode.EXECUTION_EXCEPTION
+                self.system_panic(f"{name} weights became NaN...", fl_ctx)
+                return make_reply(ReturnCode.EXECUTION_EXCEPTION)
 
         # Build the shareable
         dxo = DXO(data_kind=DataKind.WEIGHT_DIFF, data=model_diff)
@@ -297,15 +303,15 @@ class CIFAR10AutoFedRLearner(CIFAR10Learner):  # TODO: also support CIFAR10Scaff
         else:
             dxo.set_meta_prop(MetaKey.NUM_STEPS_CURRENT_ROUND, epoch_len)
 
-        self.info("Local epochs finished. Returning shareable")
-        return dxo
+        self.log_info(fl_ctx, "Local epochs finished. Returning shareable")
+        return dxo.to_shareable()
 
-    def local_valid(self, valid_loader, tb_id=None, get_loss=False):
+    def local_valid(self, valid_loader, abort_signal: Signal, tb_id=None, fl_ctx=None, get_loss=False):
         self.model.eval()
         with torch.no_grad():
             correct, total = 0, 0
             for _, (inputs, labels) in enumerate(valid_loader):
-                if self.is_aborted():
+                if abort_signal.triggered:
                     return None
                 inputs, labels = inputs.to(self.device), labels.to(self.device)
                 outputs = self.model(inputs)
@@ -321,22 +327,29 @@ class CIFAR10AutoFedRLearner(CIFAR10Learner):  # TODO: also support CIFAR10Scaff
                     correct += (pred_label == labels.data).sum().item()
             metric = correct / float(total)
             if get_loss:
-                self.info(f"HP Search loss: {metric} of {total} batches on {self.site_name}")
+                self.log_info(fl_ctx, f"HP Search loss: {metric} of {total} batches on {fl_ctx.get_identity_name()}")
             if tb_id:
                 self.writer.add_scalar(tb_id, metric, self.current_round)
         return metric
 
-    def validate(self, dxo: DXO, validate_type: str, model_owner: str) -> Union[str, DXO]:
-        self._create_datasets()
+    def validate(self, shareable: Shareable, fl_ctx: FLContext, abort_signal: Signal) -> Shareable:
+        self._create_datasets(fl_ctx)
 
         # Check abort signal
-        if self.is_aborted():
-            return ReturnCode.TASK_ABORTED
+        if abort_signal.triggered:
+            return make_reply(ReturnCode.TASK_ABORTED)
 
         # Get validation information
-        self.info(f"Client identity: {self.site_name}")
+        self.log_info(fl_ctx, f"Client identity: {fl_ctx.get_identity_name()}")
+        model_owner = shareable.get(ReservedHeaderKey.HEADERS).get(AppConstants.MODEL_OWNER)
+        if model_owner:
+            self.log_info(fl_ctx, f"Evaluating model from {model_owner} on {fl_ctx.get_identity_name()}")
+        else:
+            # Evaluating global model during training
+            model_owner = "global_model"
 
         # Update local model weights with received weights
+        dxo = from_shareable(shareable)
         global_weights = dxo.data
 
         # Before loading weights, tensors might need to be reshaped to support HE for secure aggregation.
@@ -356,47 +369,51 @@ class CIFAR10AutoFedRLearner(CIFAR10Learner):  # TODO: also support CIFAR10Scaff
         if n_loaded == 0:
             raise ValueError(f"No weights loaded for validation! Received weight dict is {global_weights}")
 
+        validate_type = shareable.get_header(AppConstants.VALIDATE_TYPE)
         if validate_type == ValidateType.BEFORE_TRAIN_VALIDATE:
             # Perform valid before local train
-            global_acc = self.local_valid(self.valid_loader, tb_id="val_acc_global_model")
-            if self.is_aborted():
-                return ReturnCode.TASK_ABORTED
-            self.info(f"val_acc_global_model ({model_owner}): {global_acc}")
+            global_acc = self.local_valid(self.valid_loader, abort_signal, tb_id="val_acc_global_model", fl_ctx=fl_ctx)
+            if abort_signal.triggered:
+                return make_reply(ReturnCode.TASK_ABORTED)
+            self.log_info(fl_ctx, f"val_acc_global_model ({model_owner}): {global_acc}")
 
             if global_acc > self.best_global_acc:
                 self.best_global_acc = global_acc
             # Log the best global model_accuracy
             self.writer.add_scalar("best_val_acc_global_model", self.best_global_acc, self.current_round)
 
-            return DXO(data_kind=DataKind.METRICS, data={MetaKey.INITIAL_METRICS: global_acc}, meta={})
+            return DXO(data_kind=DataKind.METRICS, data={MetaKey.INITIAL_METRICS: global_acc}, meta={}).to_shareable()
 
         elif validate_type == ValidateType.MODEL_VALIDATE:
             # Perform valid
-            train_acc = self.local_valid(self.train_loader)
-            if self.is_aborted():
-                return ReturnCode.TASK_ABORTED
-            self.info(f"training acc ({model_owner}): {train_acc}")
+            train_acc = self.local_valid(self.train_loader, abort_signal)
+            if abort_signal.triggered:
+                return make_reply(ReturnCode.TASK_ABORTED)
+            self.log_info(fl_ctx, f"training acc ({model_owner}): {train_acc}")
 
-            val_acc = self.local_valid(self.valid_loader)
-            if self.is_aborted():
-                return ReturnCode.TASK_ABORTED
-            self.info(f"validation acc ({model_owner}): {val_acc}")
+            val_acc = self.local_valid(self.valid_loader, abort_signal)
+            if abort_signal.triggered:
+                return make_reply(ReturnCode.TASK_ABORTED)
+            self.log_info(fl_ctx, f"validation acc ({model_owner}): {val_acc}")
 
-            self.info("Evaluation finished. Returning shareable")
+            self.log_info(fl_ctx, "Evaluation finished. Returning shareable")
 
             val_results = {"train_accuracy": train_acc, "val_accuracy": val_acc}
 
-            return DXO(data_kind=DataKind.METRICS, data=val_results)
+            metric_dxo = DXO(data_kind=DataKind.METRICS, data=val_results)
+            return metric_dxo.to_shareable()
 
         elif validate_type == AutoFedRLConstants.MODEL_VALIDATE_FOR_SEARCH:
-            self.info(f"Evaluating model from {model_owner} on {self.site_name} for HP Search")
+            self.log_info(fl_ctx, f"Evaluating model from {model_owner} on {fl_ctx.get_identity_name()} for HP Search")
 
-            val_loss_hp = self.local_valid(self.val_loader_search, get_loss=True)
-            if self.is_aborted():
-                return ReturnCode.TASK_ABORTED
+            val_loss_hp = self.local_valid(self.val_loader_search, abort_signal, fl_ctx=fl_ctx, get_loss=True)
+            if abort_signal.triggered:
+                return make_reply(ReturnCode.TASK_ABORTED)
 
             val_results = {"val_loss": val_loss_hp}
 
-            return DXO(data_kind=DataKind.METRICS, data=val_results)
+            metric_dxo = DXO(data_kind=DataKind.METRICS, data=val_results)
+            return metric_dxo.to_shareable()
+
         else:
-            return ReturnCode.VALIDATE_TYPE_UNKNOWN
+            return make_reply(ReturnCode.VALIDATE_TYPE_UNKNOWN)
