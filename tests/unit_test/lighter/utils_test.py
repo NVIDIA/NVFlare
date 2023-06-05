f620d599f888b4c7e72312d32ef93fff19396b6a
YuanTingHsieh <yuantingh@nvidia.com>
2023-06-05 15:57:59 -0700

    Fix lighter unit test

--- tests/unit_test/lighter/utils_test.py
+++ tests/unit_test/lighter/utils_test.py
@@ -19,13 +19,12 @@ import tempfile
 
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.x509.oid import NameOID
 
 from nvflare.lighter.impl.cert import serialize_cert
 from nvflare.lighter.utils import sign_folders, verify_folder_signature
-
 
 folders = ["folder1", "folder2"]
 files = ["file1", "file2"]
