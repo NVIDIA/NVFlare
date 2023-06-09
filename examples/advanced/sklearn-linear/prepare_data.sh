fbba85c32bfa64d6d034a1badf79c1e01ff9bfc4
YuanTingHsieh <yuantingh@nvidia.com>
2023-06-09 15:56:15 -0700

    Add UCI website note

--- examples/advanced/sklearn-linear/prepare_data.sh
+++ examples/advanced/sklearn-linear/prepare_data.sh
@@ -6,6 +6,7 @@ DATASET_PATH="${DATASET_DIR}/HIGGS.csv"
 if [ -f "$DATASET_PATH" ]; then
     echo "${DATASET_PATH} exists."
 else
+    # Please note that the UCI's website may experience occasional downtime.
     wget --directory-prefix ${DATASET_DIR} https://archive.ics.uci.edu/static/public/280/higgs.zip
     unzip "${DATASET_DIR}/higgs.zip"
     gzip -d "${DATASET_DIR}/HIGGS.csv.gz"
