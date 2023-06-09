a8dd8805cc746e649d52d6fa06cb09f734b7a451
chesterxgchen <chesterxgchen@yahoo.com>
2023-06-09 16:18:41 -0700

    update docstring

--- nvflare/fuel/utils/config.py
+++ nvflare/fuel/utils/config.py
@@ -41,12 +41,13 @@ class Config(ABC):
     @abstractmethod
     def get_native_conf(self):
         """
-        Return the original underline config object representation if you prefer to use it directly
-        Pyhocon → ConfigTree
-        JSON → Dict
-        OMEGACONF → ConfigDict
+            Return the original underline config object representation if you prefer to use it directly
+            Pyhocon → ConfigTree
+            JSON → Dict
+            OMEGACONF → ConfigDict
 
-        Returns: Any, native config objects
+        Returns: Any,
+            return native config objects
         """
 
         pass
@@ -54,7 +55,9 @@ class Config(ABC):
     @abstractmethod
     def get_format(self):
         """
-        Returns: ConfigFormat, returns the current config objects ConfigFormat
+            returns the current config objects ConfigFormat
+        Returns:
+            return ConfigFormat
         """
 
         pass
@@ -62,7 +65,10 @@ class Config(ABC):
     @abstractmethod
     def get_location(self) -> Optional[str]:
         """
-        Returns: None if the config is not from file else return the file path where this configuration is loaded from
+           return the file path where this configuration is loaded from
+
+        Returns:
+            return None if the config is not from file else return file path
 
         """
         pass
@@ -70,8 +76,10 @@ class Config(ABC):
     @abstractmethod
     def to_dict(self) -> Dict:
         """
-        convert underline config object to dictionary
-        Returns:Dict
+            convert underline config object to dictionary
+
+        Returns:
+            Returns: converted configuration as dict
 
         """
         pass
@@ -83,10 +91,12 @@ class Config(ABC):
             For example, for JsonFormat, the method return json string
             for PyhoconFormat, the method return pyhocon string
             for OmegaconfFormat, the method returns YAML string representation
+
         Args:
             element: dict
 
-        Returns: str string representation of the configuration in given format
+        Returns:
+            string representation of the configuration in given format
 
         """
         pass
@@ -98,37 +108,42 @@ class ConfigLoader(ABC):
         self, file_path: str, default_file_path: Optional[str] = None, overwrite_config: Optional[Dict] = None
     ) -> Config:
         """
-        configuration from default_file_path will be the default config if specified
-        configuration from file_path will be the merge with default config overwrite the same key
-        configuration from overwrite_config if provided will be the merge with config overwrite the same key
+            configuration from default_file_path will be the default config if specified
+            configuration from file_path will be the merge with default config overwrite the same key
+            configuration from overwrite_config if provided will be the merge with config overwrite the same key
+
         Args:
             file_path: file path for configuration to be loaded
             default_file_path: file path for default configuration
             overwrite_config: dict config that will  overwrite the final config if provided
 
-        Returns:nvflare.fuel.utils.Config
+        Returns:
+            return Config
         """
 
         pass
 
     def load_config_from_str(self, config_str: str) -> Config:
         """
-        Load Configuration based on the string representation of the underline configuration
-        for example, Json String for Jsonformat. python conf string or yaml string presentation
+            Load Configuration based on the string representation of the underline configuration
+            for example, Json String for Jsonformat. python conf string or yaml string presentation
+
         Args:
             config_str:
-        Returns:nvflare.fuel.utils.Config
+        Returns:
+            return Config
 
         """
         raise NotImplementedError
 
     def load_config_from_dict(self, config_dict: dict) -> Config:
         """
-        Load Configuration based for given config dict.
+            Load Configuration based for given config dict.
 
         Args:
             config_dict:
-        Returns:nvflare.fuel.utils.Config
+        Returns:
+            Config
 
         """
         raise NotImplementedError
