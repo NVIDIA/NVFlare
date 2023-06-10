950e41ec410112ae3547503ce36121327257ec1b
YuanTingHsieh <yuantingh@nvidia.com>
2023-06-09 17:43:48 -0700

    Fix unzip

--- examples/advanced/sklearn-linear/prepare_data.sh
+++ examples/advanced/sklearn-linear/prepare_data.sh
@@ -8,7 +8,7 @@ if [ -f "$DATASET_PATH" ]; then
 else
     # Please note that the UCI's website may experience occasional downtime.
     wget --directory-prefix ${DATASET_DIR} https://archive.ics.uci.edu/static/public/280/higgs.zip
-    unzip "${DATASET_DIR}/higgs.zip"
+    unzip "${DATASET_DIR}/higgs.zip" -d "${DATASET_DIR}"
     gzip -d "${DATASET_DIR}/HIGGS.csv.gz"
 
     echo "Data downloaded and saved in ${DATASET_PATH}"
