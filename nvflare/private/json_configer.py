48007eeac015200d34d9dffd234a3bd79d987d20
chesterxgchen <chesterxgchen@yahoo.com>
2023-07-05 13:49:02 -0700

    optimize import

--- nvflare/private/json_configer.py
+++ nvflare/private/json_configer.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
-import os
 from typing import List, Union
 
 from nvflare.fuel.common.excepts import ConfigError
