50ea350b89c74448f98ee6108514d044bb6cc34d
YuanTingHsieh <yuantingh@nvidia.com>
2023-07-06 15:29:17 -0700

    Add SubprocessLauncher + PTFilePipeLauncherExecutor + NVFlare Client API example

---
+++ examples/advanced/ml-to-fl/README.md
@@ -0,0 +1,24 @@
+# ML to FL trasition with NVFlare
+
+Converting Machine Learning or Deep Learning to FL is not easy, as it involves:
+
+1. Algorithms formulation, how to formulate a ML/DL to FL algorithm and what information needs to be pass between Client and Server
+
+2. Convert existing standalone, centralized ML/DL code to FL code.
+
+3. Configure the workflow to use the newly changed code.
+
+In this example, we assume #1 algorithm formulation is fixed (FedAvg).
+We are showing #2, that is how to quickly convert the centralized DL to FL.
+We will demonstrate different techniques depending the existing code structure and preferences.
+
+For #3 one can reference to the config we have here and the documentation.
+
+In this directory, we are providing job configurations to showcase how to utilizes 
+`LauncherExecutor`, `Launcher` and several NVFlare interfaces to simplify the
+transition from your ML code to FL with NVFlare.
+
+
+## Examples
+
+- [client_api](./jobs/client_api/) Use `SubprocessLauncher`, `PTFilePipeLauncherExecutor` and NVFlare Client API to do federated learning with CIFAR10 dataset and PyTorch framework
---
+++ examples/advanced/ml-to-fl/jobs/client_api/app/custom/cifar10.py
@@ -0,0 +1,103 @@
+# Copyright (c) 2023, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from net import Net
+
+import nvflare.client as flare
+
+DATASET_PATH = "/tmp/nvflare/data"
+device = "cuda:0"
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+batch_size = 4
+
+trainset = torchvision.datasets.CIFAR10(root=DATASET_PATH, train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root=DATASET_PATH, train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
+
+
+net = Net()
+
+# initializes NVFlare interface
+flare.init(config="config/config_exchange.json")
+input_model, input_meta = flare.receive_model()
+
+# get model from NVFlare
+net.load_state_dict(input_model)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+
+net.to(device)
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data[0].to(device), data[1].to(device)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:  # print every 2000 mini-batches
+            print(f"[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}")
+            running_loss = 0.0
+            break
+
+print("Finished Training")
+
+
+PATH = "./cifar_net.pth"
+torch.save(net.state_dict(), PATH)
+
+
+net = Net()
+net.load_state_dict(input_model)
+net.to(device)
+
+
+correct = 0
+total = 0
+# since we're not training, we don't need to calculate the gradients for our outputs
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data[0].to(device), data[1].to(device)
+        # calculate outputs by running images through the network
+        outputs = net(images)
+        # the class with the highest energy is what we choose as prediction
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f"Accuracy of the network on the 10000 test images: {100 * correct // total} %")
+
+net.load_state_dict(torch.load(PATH))
+flare.submit_metrics({"accuracy": 100 * correct // total})
+flare.submit_model(net.cpu().state_dict())
