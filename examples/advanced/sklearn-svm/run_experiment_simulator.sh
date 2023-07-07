3fbcb6545c80d6634173fdc4f32b62bd0cf3cf30
YuanTingHsieh <yuantingh@nvidia.com>
2023-07-07 16:47:02 -0700

    Remove CUML from requirements.txt and add install instructions

--- examples/advanced/sklearn-svm/run_experiment_simulator.sh
+++ examples/advanced/sklearn-svm/run_experiment_simulator.sh
@@ -6,7 +6,7 @@ for site_num in 3;
 do
     for split_mode in uniform;
     do
-        for backend in sklearn cuml;
+        for backend in sklearn;
         do
             nvflare simulator jobs/${task_name}_${site_num}_${split_mode}_${backend} \
                 -w "${PWD}"/workspaces/${task_name}_${site_num}_${split_mode}_${backend} \
