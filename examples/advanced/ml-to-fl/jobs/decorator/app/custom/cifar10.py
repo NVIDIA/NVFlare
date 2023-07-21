f4bfb3a6a1cfdb861f7af9fef3d601e3c5489e5b
YuanTingHsieh <yuantingh@nvidia.com>
2023-07-21 16:57:52 -0700

    Update example

--- examples/advanced/ml-to-fl/jobs/decorator/app/custom/cifar10.py
+++ examples/advanced/ml-to-fl/jobs/decorator/app/custom/cifar10.py
@@ -19,8 +19,10 @@ import torchvision
 import torchvision.transforms as transforms
 from net import Net
 
+# (1.0) import nvflare client API
 import nvflare.client as flare
 
+# (optional) set a fix place so we don't need to download everytime
 DATASET_PATH = "/tmp/nvflare/data"
 PATH = "./cifar_net.pth"
 
@@ -34,31 +36,37 @@ trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuff
 testset = torchvision.datasets.CIFAR10(root=DATASET_PATH, train=False, download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
 
-flare.init(config="config/config_exchange.json")
 
 net = Net()
 
+# (1.1) initializes NVFlare client API
+flare.init()
 
-@flare.train(job_id="meta.job_id")
-def train(weights=None, total_epochs=2, lr=0.001, device="cuda:0", job_id=None):
-    print(f"JOB is {job_id}")
+
+# (1.2) wraps training logic into a method
+@flare.train
+def train(weights=None, total_epochs=2, lr=0.001, device="cuda:0"):
     if weights is not None:
         net.load_state_dict(weights)
-    net.to(device)
+
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.9)
-    steps = 0
+
+    # (optional) use GPU to speed things up
+    net.to(device)
     for epoch in range(total_epochs):  # loop over the dataset multiple times
+
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
             # get the inputs; data is a list of [inputs, labels]
-            images, labels = data[0].to(device), data[1].to(device)
+            # (optional) use GPU to speed things up
+            inputs, labels = data[0].to(device), data[1].to(device)
 
             # zero the parameter gradients
             optimizer.zero_grad()
 
             # forward + backward + optimize
-            outputs = net(images)
+            outputs = net(inputs)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
@@ -68,37 +76,40 @@ def train(weights=None, total_epochs=2, lr=0.001, device="cuda:0", job_id=None):
             if i % 2000 == 1999:  # print every 2000 mini-batches
                 print(f"[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}")
                 running_loss = 0.0
-                break
-
-            steps += 1
 
     print("Finished Training")
     torch.save(net.state_dict(), PATH)
 
-    return net.cpu().state_dict(), {"NUM_STEPS_CURRENT_ROUND": steps}
+    return net.cpu().state_dict()
 
 
+# (1.3) wraps evaluate logic into a method
 @flare.evaluate
-def evaluate(weights=None, device="cuda:0"):
-    if weights is not None:
-        net.load_state_dict(weights)
+def evaluate(input_weights=None, device="cuda:0"):
+    if input_weights is not None:
+        net.load_state_dict(input_weights)
+    # (optional) use GPU to speed things up
     net.to(device)
+
     correct = 0
     total = 0
     # since we're not training, we don't need to calculate the gradients for our outputs
     with torch.no_grad():
         for data in testloader:
+            # (optional) use GPU to speed things up
             images, labels = data[0].to(device), data[1].to(device)
-
             # calculate outputs by running images through the network
             outputs = net(images)
             # the class with the highest energy is what we choose as prediction
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
+
     print(f"Accuracy of the network on the 10000 test images: {100 * correct // total} %")
     return 100 * correct // total
 
 
+# (1.4) call evaluate method
 evaluate()
+# (1.5) call train method
 train(total_epochs=2, lr=0.001)
