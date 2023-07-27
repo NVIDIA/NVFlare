47e8df337218f3c935ea1e8e66aee57d2113e78b
Yuhong Wen <yuhongw@nvidia.com>
2023-07-27 09:00:01 -0400

    Refacftored.

--- nvflare/apis/impl/client_controller.py
+++ nvflare/apis/impl/client_controller.py
@@ -27,8 +27,6 @@ from nvflare.security.logging import secure_format_exception
 
 class ClientController(FLComponent, ControllerSpec):
     def __init__(self,
-                 # task_data_filters: dict,  # task_name => list of filters
-                 # task_result_filters: dict,  # task_name => list of filters
                  ) -> None:
         super().__init__()
         self.task_data_filters = None
@@ -54,7 +52,6 @@ class ClientController(FLComponent, ControllerSpec):
 
     def broadcast(self, task: Task, fl_ctx: FLContext, targets: Union[List[Client], List[str], None] = None,
                   min_responses: int = 0, wait_time_after_min_received: int = 0):
-        # super().broadcast(task, fl_ctx, targets, min_responses, wait_time_after_min_received)
 
         return self.broadcast_and_wait(task, fl_ctx, targets, min_responses, wait_time_after_min_received)
 
@@ -79,11 +76,10 @@ class ClientController(FLComponent, ControllerSpec):
             filter_list.extend(task_filter_list)
 
         if filter_list:
-            task_data = task.data
             for f in filter_list:
                 filter_name = f.__class__.__name__
                 try:
-                    task_data = f.process(task_data, fl_ctx)
+                    request = f.process(request, fl_ctx)
                 except Exception as e:
                     self.log_exception(
                         fl_ctx, f"Processing error from Task Data Filter {filter_name}: {secure_format_exception(e)}"
@@ -94,7 +90,8 @@ class ClientController(FLComponent, ControllerSpec):
 
         request.set_header(ReservedKey.TASK_NAME, task.name)
         replies = engine.send_aux_request(
-            targets=targets, topic=ReservedTopic.CLIENT_CONTROLLER_TASK, request=request, timeout=task.timeout, fl_ctx=fl_ctx
+            targets=targets, topic=ReservedTopic.CLIENT_CONTROLLER_TASK, request=request,
+            timeout=task.timeout, fl_ctx=fl_ctx
         )
 
         for client in targets:
@@ -133,28 +130,32 @@ class ClientController(FLComponent, ControllerSpec):
 
     def _call_tasK_cb(self, task_cb, client, task, fl_ctx):
         with task.cb_lock:
-            client_task_to_send = ClientTask(task=task, client=client)
+            client_task = ClientTask(task=task, client=client)
             if task.before_task_sent_cb is not None:
                 try:
-                    task_cb(client_task=client_task_to_send, fl_ctx=fl_ctx)
+                    task_cb(client_task=client_task, fl_ctx=fl_ctx)
                 except Exception as e:
                     self.log_exception(
                         fl_ctx,
-                        f"processing error in {task_cb} on task {client_task_to_send.task.name} "
-                        f"({client_task_to_send.id}): {secure_format_exception(e)}"
+                        f"processing error in {task_cb} on task {client_task.task.name} "
+                        f"({client_task.id}): {secure_format_exception(e)}"
                         )
                     # this task cannot proceed anymore
                     task.completion_status = TaskCompletionStatus.ERROR
                     task.exception = e
 
-            self.logger.debug(f"{task_cb} done on client_task_to_send: {client_task_to_send}")
+            self.logger.debug(f"{task_cb} done on client_task: {client_task}")
             self.logger.debug(f"task completion status is {task.completion_status}")
 
     def send(self, task: Task, fl_ctx: FLContext, targets: Union[List[Client], List[str], None] = None,
              send_order: SendOrder = SendOrder.SEQUENTIAL, task_assignment_timeout: int = 0):
-        super().send(task, fl_ctx, targets, send_order, task_assignment_timeout)
+        return self.send_and_wait(task, fl_ctx, targets, send_order, task_assignment_timeout)
 
     def send_and_wait(self, task: Task, fl_ctx: FLContext, targets: Union[List[Client], List[str], None] = None,
                       send_order: SendOrder = SendOrder.SEQUENTIAL, task_assignment_timeout: int = 0,
                       abort_signal: Signal = None):
-        super().send_and_wait(task, fl_ctx, targets, send_order, task_assignment_timeout, abort_signal)
+        replies = {}
+        for target in targets:
+            reply = self.broadcast_and_wait(task, fl_ctx, [target], abort_signal=abort_signal)
+            replies.update(reply)
+        return replies
