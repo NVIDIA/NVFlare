fe76cb1ebd39ed7e4eea939df885f8e6fe3f0885
Yan Cheng <yanc@nvidia.com>
2023-08-03 11:29:34 -0400

    fix import

--- nvflare/fuel/f3/cellnet/net_agent.py
+++ nvflare/fuel/f3/cellnet/net_agent.py
@@ -23,7 +23,7 @@ import time
 from abc import ABC
 from typing import List, Union
 
-from nvflare.fuel.f3.cellnet.cell import Cell, Message
+from nvflare.fuel.f3.cellnet.cell import Message
 from nvflare.fuel.f3.cellnet.connector_manager import ConnectorData
 from nvflare.fuel.f3.cellnet.defs import MessageHeaderKey, ReturnCode
 from nvflare.fuel.f3.cellnet.fqcn import FQCN
