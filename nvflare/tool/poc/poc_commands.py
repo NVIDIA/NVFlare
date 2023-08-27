a29699e3604592838bc601c576abcebdf26c3c90
chesterxgchen <chesterxgchen@yahoo.com>
2023-08-26 20:24:32 -0700

    Update POC tutorials

--- nvflare/tool/poc/poc_commands.py
+++ nvflare/tool/poc/poc_commands.py
@@ -137,15 +137,18 @@ def prepare_examples(cmd_args):
     _prepare_examples(cmd_args.examples, poc_workspace)
 
 
-def _prepare_examples(example_dir: str, workspace: str, config_packages: Optional[Tuple] = None):
-    _, service_config = config_packages if config_packages else setup_service_config(workspace)
+def _prepare_examples(example_dir: str,
+                      workspace: str,
+                      config_packages: Optional[Tuple] = None):
+    project_config, service_config = config_packages if config_packages else setup_service_config(workspace)
+    project_name = project_config.get("name")
     if example_dir is None or example_dir == "":
         raise CLIException("example_dir is required")
     src = os.path.abspath(example_dir)
     if not os.path.isdir(src):
         raise CLIException(f"example_dir '{example_dir}' is not valid directory")
 
-    prod_dir = get_prod_dir(workspace)
+    prod_dir = get_prod_dir(workspace, project_name)
     if not os.path.exists(prod_dir):
         raise CLIException("please use nvflare poc prepare to create workspace first")
 
@@ -249,12 +252,12 @@ def prepare_builders(project_dict: OrderedDict) -> List:
 
 
 def local_provision(
-    clients: List[str],
-    number_of_clients: int,
-    workspace: str,
-    docker_image: str,
-    use_he: bool = False,
-    project_conf_path: str = "",
+        clients: List[str],
+        number_of_clients: int,
+        workspace: str,
+        docker_image: str,
+        use_he: bool = False,
+        project_conf_path: str = "",
 ) -> Tuple:
     user_provided_project_config = False
     if project_conf_path:
@@ -375,9 +378,9 @@ def prepare_clients(clients, number_of_clients):
     return clients
 
 
-def save_startup_kit_dir_config(workspace):
+def save_startup_kit_dir_config(workspace, project_name):
     dst = get_hidden_nvflare_config_path()
-    prod_dir = get_prod_dir(workspace)
+    prod_dir = get_prod_dir(workspace, project_name)
     conf = f"""
     startup_kit {{
         path = {prod_dir}
@@ -408,13 +411,13 @@ def prepare_poc(cmd_args):
 
 
 def _prepare_poc(
-    clients: List[str],
-    number_of_clients: int,
-    workspace: str,
-    docker_image: str = None,
-    use_he: bool = False,
-    project_conf_path: str = "",
-    examples_dir: Optional[str] = None,
+        clients: List[str],
+        number_of_clients: int,
+        workspace: str,
+        docker_image: str = None,
+        use_he: bool = False,
+        project_conf_path: str = "",
+        examples_dir: Optional[str] = None,
 ) -> bool:
     if clients:
         number_of_clients = len(clients)
@@ -423,8 +426,8 @@ def _prepare_poc(
     else:
         print(f"prepare poc at {workspace} with {project_conf_path}")
 
-    save_startup_kit_dir_config(workspace)
-
+    project_config = None
+    result = False
     if os.path.exists(workspace):
         answer = input(
             f"This will delete poc folder in {workspace} directory and create a new one. Is it OK to proceed? (y/N) "
@@ -442,17 +445,21 @@ def _prepare_poc(
                 )
 
             shutil.rmtree(workspace, ignore_errors=True)
-            prepare_poc_provision(
+            project_config = prepare_poc_provision(
                 clients, number_of_clients, workspace, docker_image, use_he, project_conf_path, examples_dir
             )
-            return True
+            result = True
         else:
-            return False
+            result = False
     else:
-        prepare_poc_provision(
+        project_config = prepare_poc_provision(
             clients, number_of_clients, workspace, docker_image, use_he, project_conf_path, examples_dir
         )
-        return True
+        result = True
+
+    project_name = project_config.get("name") if project_config else None
+    save_startup_kit_dir_config(workspace, project_name)
+    return result
 
 
 def get_home_dir():
@@ -481,25 +488,29 @@ def get_hidden_nvflare_config_path() -> str:
 
 
 def prepare_poc_provision(
-    clients: List[str],
-    number_of_clients: int,
-    workspace: str,
-    docker_image: str,
-    use_he: bool = False,
-    project_conf_path: str = "",
-    examples_dir: Optional[str] = None,
-):
+        clients: List[str],
+        number_of_clients: int,
+        workspace: str,
+        docker_image: str,
+        use_he: bool = False,
+        project_conf_path: str = "",
+        examples_dir: Optional[str] = None,
+) -> Dict:
     os.makedirs(workspace, exist_ok=True)
     os.makedirs(os.path.join(workspace, "data"), exist_ok=True)
-    _, service_config = local_provision(clients, number_of_clients, workspace, docker_image, use_he, project_conf_path)
+    project_config, service_config = local_provision(clients, number_of_clients, workspace, docker_image, use_he,
+                                                     project_conf_path)
+    project_name = project_config.get("name")
     server_name = service_config[SC.FLARE_SERVER]
     # update storage
     if workspace != DEFAULT_WORKSPACE:
-        prod_dir = get_prod_dir(workspace)
+        prod_dir = get_prod_dir(workspace, project_name)
         update_storage_locations(local_dir=f"{prod_dir}/{server_name}/local", workspace=workspace)
     examples_dir = get_examples_dir(examples_dir)
     if examples_dir is not None:
         _prepare_examples(examples_dir, workspace, None)
+
+    return project_config
 
 
 def get_examples_dir(examples_dir):
@@ -539,16 +550,17 @@ def get_cmd_path(poc_workspace, service_name, cmd):
     return cmd_path
 
 
-def is_poc_ready(poc_workspace: str, service_config):
+def is_poc_ready(poc_workspace: str, service_config, project_config):
     # check server and admin directories exist
-    prod_dir = get_prod_dir(poc_workspace)
+    project_name = project_config.get("name") if project_config else DEFAULT_PROJECT_NAME
+    prod_dir = get_prod_dir(poc_workspace, project_name)
     console_dir = os.path.join(prod_dir, service_config[SC.FLARE_PROJ_ADMIN])
     server_dir = os.path.join(prod_dir, service_config[SC.FLARE_SERVER])
     return os.path.isdir(server_dir) and os.path.isdir(console_dir)
 
 
-def validate_poc_workspace(poc_workspace: str, service_config):
-    if not is_poc_ready(poc_workspace, service_config):
+def validate_poc_workspace(poc_workspace: str, service_config, project_config=None):
+    if not is_poc_ready(poc_workspace, service_config, project_config):
         raise CLIException(f"workspace {poc_workspace} is not ready, please use poc prepare to prepare poc workspace")
 
 
@@ -610,8 +622,9 @@ def _start_poc(poc_workspace: str, gpu_ids: List[int], excluded=None, services_l
         excluded = []
     print(f"start_poc at {poc_workspace}, gpu_ids={gpu_ids}, excluded = {excluded}, services_list={services_list}")
     validate_services(project_config, services_list, excluded)
-    validate_poc_workspace(poc_workspace, service_config)
-    _run_poc(SC.CMD_START, poc_workspace, gpu_ids, service_config, excluded=excluded, services_list=services_list)
+    validate_poc_workspace(poc_workspace, service_config, project_config)
+    _run_poc(SC.CMD_START, poc_workspace, gpu_ids, service_config, project_config, excluded=excluded,
+             services_list=services_list)
 
 
 def validate_services(project_config, services_list: List, excluded: List):
@@ -656,9 +669,10 @@ def _stop_poc(poc_workspace: str, excluded=None, services_list=None):
 
     validate_services(project_config, services_list, excluded)
 
-    validate_poc_workspace(poc_workspace, service_config)
+    validate_poc_workspace(poc_workspace, service_config, project_config)
     gpu_ids: List[int] = []
-    prod_dir = get_prod_dir(poc_workspace)
+    project_name = project_config.get("name")
+    prod_dir = get_prod_dir(poc_workspace, project_name)
 
     p_size = len(services_list)
     if p_size == 0 or service_config[SC.FLARE_SERVER] in services_list:
@@ -667,7 +681,8 @@ def _stop_poc(poc_workspace: str, excluded=None, services_list=None):
     else:
         print(f"start shutdown {services_list}")
 
-    _run_poc(SC.CMD_STOP, poc_workspace, gpu_ids, service_config, excluded=excluded, services_list=services_list)
+    _run_poc(SC.CMD_STOP, poc_workspace, gpu_ids, service_config, project_config, excluded=excluded,
+             services_list=services_list)
 
 
 def _get_clients(service_commands: list, service_config) -> List[str]:
@@ -675,12 +690,17 @@ def _get_clients(service_commands: list, service_config) -> List[str]:
         service_dir_name
         for service_dir_name, _ in service_commands
         if service_dir_name != service_config[SC.FLARE_PROJ_ADMIN]
-        and service_dir_name != service_config[SC.FLARE_SERVER]
+           and service_dir_name != service_config[SC.FLARE_SERVER]
     ]
     return clients
 
 
-def _build_commands(cmd_type: str, poc_workspace: str, service_config, excluded: list, services_list=None) -> list:
+def _build_commands(cmd_type: str,
+                    poc_workspace: str,
+                    service_config,
+                    project_config,
+                    excluded: list,
+                    services_list=None) -> list:
     """Builds commands.
 
     Args:
@@ -696,13 +716,14 @@ def _build_commands(cmd_type: str, poc_workspace: str, service_config, excluded:
 
     def is_fl_service_dir(p_dir_name: str) -> bool:
         fl_service = (
-            p_dir_name == service_config[SC.FLARE_PROJ_ADMIN]
-            or p_dir_name == service_config[SC.FLARE_SERVER]
-            or p_dir_name in service_config[SC.FLARE_CLIENTS]
+                p_dir_name == service_config[SC.FLARE_PROJ_ADMIN]
+                or p_dir_name == service_config[SC.FLARE_SERVER]
+                or p_dir_name in service_config[SC.FLARE_CLIENTS]
         )
         return fl_service
 
-    prod_dir = get_prod_dir(poc_workspace)
+    project_name = project_config.get("name")
+    prod_dir = get_prod_dir(poc_workspace, project_name)
 
     if services_list is None:
         services_list = []
@@ -753,11 +774,17 @@ def sync_process(service_name, cmd_path):
 
 
 def _run_poc(
-    cmd_type: str, poc_workspace: str, gpu_ids: List[int], service_config: Dict, excluded: list, services_list=None
+        cmd_type: str,
+        poc_workspace: str,
+        gpu_ids: List[int],
+        service_config: Dict,
+        project_config: Dict,
+        excluded: list,
+        services_list=None
 ):
     if services_list is None:
         services_list = []
-    service_commands = _build_commands(cmd_type, poc_workspace, service_config, excluded, services_list)
+    service_commands = _build_commands(cmd_type, poc_workspace, service_config, project_config, excluded, services_list)
     clients = _get_clients(service_commands, service_config)
     gpu_assignments: Dict[str, List[int]] = client_gpu_assignments(clients, gpu_ids)
     for service_name, cmd_path in service_commands:
@@ -783,7 +810,7 @@ def _clean_poc(poc_workspace: str):
     if os.path.isdir(poc_workspace):
         project_config, service_config = setup_service_config(poc_workspace)
         if project_config is not None:
-            if is_poc_ready(poc_workspace, service_config):
+            if is_poc_ready(poc_workspace, service_config, project_config):
                 shutil.rmtree(poc_workspace, ignore_errors=True)
                 print(f"{poc_workspace} is removed")
             else:
@@ -893,7 +920,7 @@ def define_prepare_parser(poc_parser, cmd: Optional[str] = None, help_str: Optio
         nargs="?",
         default="",
         help="project.yaml file path, If specified, "
-        + "'number_of_clients','clients' and 'docker' specific options will be ignored.",
+             + "'number_of_clients','clients' and 'docker' specific options will be ignored.",
     )
     prepare_parser.add_argument(
         "-d",
@@ -902,7 +929,7 @@ def define_prepare_parser(poc_parser, cmd: Optional[str] = None, help_str: Optio
         default=None,
         const="nvflare/nvflare",
         help="generate docker.sh based on the docker_image, used in '--prepare' command. and generate docker.sh "
-        + " 'start/stop' commands will start with docker.sh ",
+             + " 'start/stop' commands will start with docker.sh ",
     )
 
     prepare_parser.add_argument("-debug", "--debug", action="store_true", help="debug is on")
