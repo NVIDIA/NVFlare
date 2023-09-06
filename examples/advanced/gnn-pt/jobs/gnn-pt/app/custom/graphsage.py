8528045db34f6863892aad3ede2850a0156b74cf
xiaoyunw <xiaoyunw@nvidia.com>
2023-09-06 16:21:24 -0700

    update code

--- examples/advanced/gnn-pt/jobs/gnn-pt/app/custom/graphsage.py
+++ examples/advanced/gnn-pt/jobs/gnn-pt/app/custom/graphsage.py
@@ -1,4 +1,5 @@
 import os.path
+import time
 
 import torch
 import torch.nn.functional as F
@@ -15,11 +16,15 @@ from torch_geometric.nn import GraphSAGE
 # (0) import nvflare client API
 import nvflare.client as flare
 
-# Create PPI dataset for training.
+#Create PPI dataset for training.
 path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "data", "PPI")
 train_dataset = PPI(path, split="train")
+val_dataset = PPI(path, split="val")
+test_dataset = PPI(path, split="test")
+
+# Group all training graphs into a single graph to perform sampling:
 train_data = Batch.from_data_list(train_dataset)
-train_loader = LinkNeighborLoader(
+loader = LinkNeighborLoader(
     train_data,
     batch_size=2048,
     shuffle=True,
@@ -28,66 +33,49 @@ train_loader = LinkNeighborLoader(
     num_workers=6,
     persistent_workers=True,
 )
+print("finish setup train loader")
 
-test_dataset = PPI(path, split="test")
+# Evaluation loaders (one datapoint corresponds to a graph)
+train_loader = DataLoader(train_dataset, batch_size=2)
+val_loader = DataLoader(val_dataset, batch_size=2)
 test_loader = DataLoader(test_dataset, batch_size=2)
+print("finish setup eval loaders")
 
 n_iterations = len(train_loader)
 
-LR = 0.003
-EPOCHS = 60
-
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-
-# need to change 64 2 64 to json
-net = GraphSAGE(
+model = GraphSAGE(
     in_channels=train_dataset.num_features,
     hidden_channels=64,
     num_layers=2,
     out_channels=64,
 )
+optimizer = torch.optim.Adam(model.parameters(), lr=0.003)
+print("finish setup model")
 
 
 # (1) initializes NVFlare client API
 flare.init()
+print("finish init")
+
 # (2) gets FLModel from NVFlare
 input_model = flare.receive()
-
-# (3) loads model from NVFlare
-net.load_state_dict(input_model.params)
-net.to(device)
-
-# (4) test received model for model selection
-def encode(loader):
-    net.eval()
-    xs, ys = [], []
-    for data in loader:
-        data = data.to(device)
-        xs.append(net(data.x, data.edge_index).cpu())
-        ys.append(data.y.cpu())
-    return torch.cat(xs, dim=0), torch.cat(ys, dim=0)
+print("finish receive")
 
 
-with torch.no_grad():
-    x, y = encode(train_loader)
-    clf = MultiOutputClassifier(SGDClassifier(loss="log_loss", penalty="l2"))
-    clf.fit(x, y)
-    train_f1 = f1_score(y, clf.predict(x), average="micro")
+def train():
+    # (3) loads model from NVFlare
+    model.load_state_dict(input_model.params)
+    model.to(device)
+    print("finish load_state_dict")
+    model.train()
 
-    # Evaluate on test set:
-    x, y = encode(test_loader)
-    global_test_f1 = f1_score(y, clf.predict(x), average="micro")
-
-optimizer = torch.optim.Adam(net.parameters(), lr=LR)
-
-total_loss = 0
-total_examples = 0
-for epoch in range(EPOCHS):
-    for data in tqdm.tqdm(train_loader):
+    total_loss = total_examples = 0
+    for data in tqdm.tqdm(loader):
         data = data.to(device)
         optimizer.zero_grad()
-        h = net(data.x, data.edge_index)
+        h = model(data.x, data.edge_index)
 
         h_src = h[data.edge_label_index[0]]
         h_dst = h[data.edge_label_index[1]]
@@ -100,9 +88,58 @@ for epoch in range(EPOCHS):
         total_loss += float(loss) * link_pred.numel()
         total_examples += link_pred.numel()
 
+    return total_loss / total_examples
+
+
+@torch.no_grad()
+def encode(data_loader):
+    model.to(device)
+    model.eval()
+
+    xs, ys = [], []
+    for data in data_loader:
+        data = data.to(device)
+        xs.append(model(data.x, data.edge_index).cpu())
+        ys.append(data.y.cpu())
+    return torch.cat(xs, dim=0), torch.cat(ys, dim=0)
+
+
+@torch.no_grad()
+def test():
+    # Train classifier on training set:
+    x, y = encode(train_loader)
+
+    clf = MultiOutputClassifier(SGDClassifier(loss="log_loss", penalty="l2"))
+    clf.fit(x, y)
+
+    train_f1 = f1_score(y, clf.predict(x), average="micro")
+
+    # Evaluate on validation set:
+    x, y = encode(val_loader)
+    val_f1 = f1_score(y, clf.predict(x), average="micro")
+
+    # Evaluate on test set:
+    x, y = encode(test_loader)
+    test_f1 = f1_score(y, clf.predict(x), average="micro")
+
+    return train_f1, val_f1, test_f1
+
+
+times = []
+_, _, global_test_f1 = test()
+print(f"Global Test F1: {global_test_f1:.4f}")
+for epoch in range(1, 61):
+    start = time.time()
+    loss = train()
+    print(f"Epoch: {epoch:02d}, Loss: {loss:.4f}")
+    train_f1, val_f1, test_f1 = test()
+    print(f"Train F1: {train_f1:.4f}, Val F1: {val_f1:.4f}, " f"Test F1: {test_f1:.4f}")
+    times.append(time.time() - start)
+print(f"Median time per epoch: {torch.tensor(times).median():.4f}s")
+
 
 # (5) construct the FLModel to returned back
-output_model = flare.FLModel(params=net.cpu().state_dict(), params_type="FULL", metrics={"f1": global_test_f1})
+output_model = flare.FLModel(params=model.cpu().state_dict(), params_type="FULL", metrics={"test_f1": global_test_f1})
 
 # (6) send back model
 flare.send(output_model)
