5628384fd98730844415acc99fd98feae571e18c
YuanTingHsieh <yuantingh@nvidia.com>
2023-09-13 13:16:32 -0700

    Fix format

--- nvflare/app_opt/lightning/api.py
+++ nvflare/app_opt/lightning/api.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Dict, List
+from typing import Dict
 
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import Callback
