bc7be8b349d19e7d0b525e55ca81696bcf8170e0
Chester Chen <512707+chesterxgchen@users.noreply.github.com>
2023-09-20 15:44:32 -0700

    change app_config logic (#2014)
    * change app_config logics
    
    * 1, remove app_config argument
    2. fix POC command overwrite the hidden config.conf

--- tests/unit_test/tool/job/job_cli_test.py
+++ tests/unit_test/tool/job/job_cli_test.py
@@ -11,13 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from unittest.mock import patch
 
 import pytest
-from pyhocon import ConfigFactory as CF
 
-from nvflare.fuel_opt.utils.pyhocon_loader import PyhoconConfig
-from nvflare.tool.job.job_cli import _update_client_app_config_script, convert_args_list_to_dict
+from nvflare.tool.job.job_cli import convert_args_list_to_dict
 
 
 class TestJobCLI:
@@ -25,45 +22,3 @@ class TestJobCLI:
     def test_convert_args_list_to_dict(self, inputs, result):
         r = convert_args_list_to_dict(inputs)
         assert r == result
-
-    def test__update_client_app_config_script(self):
-        with patch("nvflare.tool.job.job_cli.has_client_config_file") as mock:
-            mock.return_value = True
-            with patch("nvflare.fuel.utils.config_factory.ConfigFactory.load_config") as mock2:
-                conf = CF.parse_string(
-                    """ 
-                        {
-                          format_version = 2
-                          app_script = "python custom/cifar10.py"
-                          app_config = ""
-                          executors = [
-                            {
-                              tasks = ["train"]
-                              executor {
-                                name = "PTFilePipeLauncherExecutor"
-                                args {
-                                  launcher_id = "launcher"
-                                  heartbeat_timeout = 60
-                                }
-                              }
-                            }
-                          ],
-                          task_result_filters= []
-                          task_data_filters =  []
-                          components =  [
-                            {
-                              id = "launcher"
-                              name =  "SubprocessLauncher"
-                              args.script = "{app_script}  {app_config} "
-                            }
-                          ]
-                        }
-                    """
-                )
-                mock2.return_value = PyhoconConfig(conf, "/tmp/my_job/app/config/config_fed_client.conf")
-
-                cli_app_configs = [["trainer.batch_size=1024", "eval_iters=100", "lr=0.1"]]
-                app_names = ["app"]
-                app_configs = _update_client_app_config_script("/tmp/my_job/", app_names, cli_app_configs)
-                config, config_path = app_configs.get("app")
-                assert config.get("app_config") == "--trainer.batch_size 1024 --eval_iters 100 --lr 0.1"
