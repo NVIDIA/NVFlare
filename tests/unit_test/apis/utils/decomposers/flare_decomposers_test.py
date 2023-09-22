01fe330983687e2bca7de6c54c07794ddfc42c3d
Yan Cheng <yanc@nvidia.com>
2023-09-21 20:16:00 -0400

    fix unit test

--- tests/unit_test/apis/utils/decomposers/flare_decomposers_test.py
+++ tests/unit_test/apis/utils/decomposers/flare_decomposers_test.py
@@ -11,6 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import os
+import tempfile
+import uuid
 from argparse import Namespace
 from typing import Any
 
@@ -47,6 +50,35 @@ class TestFlareDecomposers:
 
         new_shareable = new_command_shareable.get_header(ServerCommandKey.SHAREABLE)
         assert new_shareable[ReservedKey.TASK_ID] == TestFlareDecomposers.ID1
+
+    def test_handle_bytes(self):
+        flare_decomposers.register()
+        d = Shareable()
+        d["x"] = os.urandom(200)
+        d["y"] = b"123456789012345678901234567890123456789012345678901234567890"
+        d["z"] = {
+            "za": 12345,
+            "zb": b"123456789012345678901234567890123456789012345678",
+        }
+        ds = fobs.dumps(d, max_value_size=15)
+        dd = fobs.loads(ds)
+        assert d == dd
+
+    def test_handle_file(self):
+        flare_decomposers.register()
+        d = Shareable()
+        d["x"] = os.urandom(200)
+        d["y"] = b"123456789012345678901234567890123456789012345678901234567890"
+        d["z"] = {
+            "za": 12345,
+            "zb": b"123456789012345678901234567890123456789012345678",
+        }
+
+        with tempfile.TemporaryDirectory() as td:
+            file_path = os.path.join(td, str(uuid.uuid4()))
+            fobs.dumpf(d, file_path, max_value_size=15)
+            df = fobs.loadf(file_path)
+            assert df == d
 
     def test_fl_context(self):
 
@@ -121,5 +153,5 @@ class TestFlareDecomposers:
 
     @staticmethod
     def _run_fobs(data: Any) -> Any:
-        buf = fobs.dumps(data)
+        buf = fobs.dumps(data, max_value_size=15)
         return fobs.loads(buf)
