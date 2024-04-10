d72258c864312db5d8366239c5a0b0d2ba9da806
Yuhong Wen <yuhongw@nvidia.com>
2024-04-10 10:16:53 -0400

    Enable simulator to run HE (#2339)
    * Enable simulator to run HE.
    
    * fixed the unittest.
    
    * Created startup folder for simulator run if not exist.
    
    * Changed to use setup and teardown for pytest.
    
    * extract common codes init_security_content_service().
    
    * removed no use import.
    
    ---------
    
    Co-authored-by: Yuan-Ting Hsieh (謝沅廷) <yuantingh@nvidia.com>
    Co-authored-by: Chester Chen <512707+chesterxgchen@users.noreply.github.com>

--- nvflare/private/fed/app/simulator/simulator_runner.py
+++ nvflare/private/fed/app/simulator/simulator_runner.py
@@ -52,7 +52,7 @@ from nvflare.fuel.utils.network_utils import get_open_ports
 from nvflare.fuel.utils.zip_utils import split_path, unzip_all_from_bytes, zip_directory_to_bytes
 from nvflare.private.defs import AppFolderConstants
 from nvflare.private.fed.app.deployer.simulator_deployer import SimulatorDeployer
-from nvflare.private.fed.app.utils import kill_child_processes
+from nvflare.private.fed.app.utils import init_security_content_service, kill_child_processes
 from nvflare.private.fed.client.client_status import ClientStatus
 from nvflare.private.fed.server.job_meta_validator import JobMetaValidator
 from nvflare.private.fed.simulator.simulator_app_runner import SimulatorServerAppRunner
@@ -152,6 +152,8 @@ class SimulatorRunner(FLComponent):
         fobs_initialize()
         AuthorizationService.initialize(EmptyAuthorizer())
         AuditService.the_auditor = SimulatorAuditor()
+
+        init_security_content_service(self.args.workspace)
 
         self.simulator_root = os.path.join(self.args.workspace, SimulatorConstants.JOB_NAME)
         if os.path.exists(self.simulator_root):
