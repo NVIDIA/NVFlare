36cf887fe1c0e59f5e9cf0f8b3c0135496b37feb
Holger Roth <hroth@nvidia.com>
2024-04-26 18:19:20 -0400

    send received model to GPU

--- integration/monai/examples/mednist/code/monai_mednist_train.py
+++ integration/monai/examples/mednist/code/monai_mednist_train.py
@@ -32,6 +32,8 @@ from pathlib import Path
 import sys
 import tempfile
 import torch
+from collections.abc import Mapping
+from typing import cast
 
 from monai.apps import MedNISTDataset
 from monai.config import print_config
@@ -42,6 +44,7 @@ from monai.inferers import SimpleInferer
 from monai.networks import eval_mode
 from monai.networks.nets import densenet121
 from monai.transforms import LoadImageD, EnsureChannelFirstD, ScaleIntensityD, Compose
+from monai.networks.utils import copy_model_state, get_state_dict
 
 # (1) import nvflare client API
 import nvflare.client as flare
@@ -51,103 +54,112 @@ from nvflare.client.tracking import SummaryWriter
 
 print_config()
 
-# (2) initializes NVFlare client API
-flare.init()
+def main():
+    # (2) initializes NVFlare client API
+    flare.init()
 
-# Setup data directory
-directory = os.environ.get("MONAI_DATA_DIRECTORY")
-root_dir = tempfile.mkdtemp() if directory is None else directory
-print(root_dir)
-
-
-# Use MONAI transforms to preprocess data
-transform = Compose(
-    [
-        LoadImageD(keys="image", image_only=True),
-        EnsureChannelFirstD(keys="image"),
-        ScaleIntensityD(keys="image"),
-    ]
-)
+    # Setup data directory
+    directory = os.environ.get("MONAI_DATA_DIRECTORY")
+    root_dir = tempfile.mkdtemp() if directory is None else directory
+    print(root_dir)
 
 
-# Prepare datasets using MONAI Apps
-dataset = MedNISTDataset(root_dir=root_dir, transform=transform, section="training", download=True)
-
-
-# Define a network and a supervised trainer
-
-# If available, we use GPU to speed things up.
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-
-max_epochs = 5
-model = densenet121(spatial_dims=2, in_channels=1, out_channels=6).to(DEVICE)
-
-train_loader = DataLoader(dataset, batch_size=512, shuffle=True, num_workers=4)
-
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-trainer = SupervisedTrainer(
-    device=torch.device(DEVICE),
-    max_epochs=max_epochs,
-    train_data_loader=train_loader,
-    network=model,
-    optimizer=torch.optim.Adam(model.parameters(), lr=1e-5),
-    loss_function=torch.nn.CrossEntropyLoss(),
-    inferer=SimpleInferer(),
-    train_handlers=StatsHandler(),
-)
-
-# (optional) calculate total steps
-steps = max_epochs * len(train_loader)
-# Run the training
-
-summary_writer = SummaryWriter()
-while flare.is_running():
-    # (3) receives FLModel from NVFlare
-    input_model = flare.receive()
-    print(f"current_round={input_model.current_round}")
-
-    # (4) loads model from NVFlare
-    trainer.model.load_state_dict(input_model.params)
-
-    trainer.run()
-
-    # (5) wraps evaluation logic into a method to re-use for
-    #       evaluation on both trained and received model
-    def evaluate(input_weights):
-        model.load_state_dict(input_weights)
-
-        # Check the prediction on the test dataset
-        dataset_dir = Path(root_dir, "MedNIST")
-        class_names = sorted(f"{x.name}" for x in dataset_dir.iterdir() if x.is_dir())
-        testdata = MedNISTDataset(root_dir=root_dir, transform=transform, section="test", download=False,
-                                  runtime_cache=True)
-        correct = 0
-        total = 0
-        with eval_mode(model):
-            for item in DataLoader(testdata, batch_size=1, num_workers=0):
-                prob = np.array(model(item["image"].to(DEVICE)).detach().to("cpu"))[0]
-                pred = class_names[prob.argmax()]
-                gt = item["class_name"][0]
-                print(f"Class prediction is {pred}. Ground-truth: {gt}")
-
-                # the class with the highest energy is what we choose as prediction
-                total += gt.size(0)
-                correct += (pred == gt).sum().item()
-
-        print(f"Accuracy of the network on the {total} test images: {100 * correct // total} %")
-        return 100 * correct // total
-
-    # (6) evaluate on received model for model selection
-    accuracy = evaluate(input_model.params)
-    summary_writer.add_scalar(tag="global_model_accuracy", scalar=accuracy, global_step=input_model.current_round)
-
-    # (7) construct trained FL model
-    output_model = flare.FLModel(
-        params=model.cpu().state_dict(),
-        metrics={"accuracy": accuracy},
-        meta={"NUM_STEPS_CURRENT_ROUND": steps},
+    # Use MONAI transforms to preprocess data
+    transform = Compose(
+        [
+            LoadImageD(keys="image", image_only=True),
+            EnsureChannelFirstD(keys="image"),
+            ScaleIntensityD(keys="image"),
+        ]
     )
-    # (8) send model back to NVFlare
-    flare.send(output_model)
+
+    # Prepare datasets using MONAI Apps
+    dataset = MedNISTDataset(root_dir=root_dir, transform=transform, section="training", download=True)
+
+    # Define a network and a supervised trainer
+
+    # If available, we use GPU to speed things up.
+    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+    max_epochs = 1  # rather than 5 epochs, we run 5 FL rounds with 1 local epoch each.
+    model = densenet121(spatial_dims=2, in_channels=1, out_channels=6).to(DEVICE)
+
+    train_loader = DataLoader(dataset, batch_size=512, shuffle=True, num_workers=4)
+
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    trainer = SupervisedTrainer(
+        device=torch.device(DEVICE),
+        max_epochs=max_epochs,
+        train_data_loader=train_loader,
+        network=model,
+        optimizer=torch.optim.Adam(model.parameters(), lr=1e-5),
+        loss_function=torch.nn.CrossEntropyLoss(),
+        inferer=SimpleInferer(),
+        train_handlers=StatsHandler(),
+    )
+
+    # (optional) calculate total steps
+    steps = max_epochs * len(train_loader)
+    # Run the training
+
+    summary_writer = SummaryWriter()
+    while flare.is_running():
+        # (3) receives FLModel from NVFlare
+        input_model = flare.receive()
+        print(f"current_round={input_model.current_round}")
+
+        # (4) loads model from NVFlare and sends it to GPU
+        trainer.network.load_state_dict(input_model.params)
+        trainer.network.to(DEVICE)
+
+        trainer.run()
+
+        # (5) wraps evaluation logic into a method to re-use for
+        #       evaluation on both trained and received model
+        def evaluate(input_weights):
+            model.load_state_dict(input_weights)
+
+            # Check the prediction on the test dataset
+            dataset_dir = Path(root_dir, "MedNIST")
+            class_names = sorted(f"{x.name}" for x in dataset_dir.iterdir() if x.is_dir())
+            testdata = MedNISTDataset(root_dir=root_dir, transform=transform, section="test", download=False,
+                                      runtime_cache=True)
+            correct = 0
+            total = 0
+            max_items_to_print = 10
+            _print = 0
+            with eval_mode(model):
+                for item in DataLoader(testdata, batch_size=512, num_workers=0):  # changed to do batch processing
+                    prob = np.array(model(item["image"].to(DEVICE)).detach().to("cpu"))
+                    pred = [class_names[p] for p in prob.argmax(axis=1)]
+                    gt = item["class_name"]
+                    # changed the logic a bit from tutorial to compute accuracy on full test set
+                    # but only print for some.
+                    for _gt, _pred in zip(gt, pred):
+                        if _print < max_items_to_print:
+                            print(f"Class prediction is {_pred}. Ground-truth: {_gt}")
+                            _print += 1
+
+                        # compute accuracy
+                        total += 1
+                        correct += float(_pred == _gt)
+
+            print(f"Accuracy of the network on the {total} test images: {100 * correct // total} %")
+            return correct // total
+
+        # (6) evaluate on received model for model selection
+        accuracy = evaluate(input_model.params)
+        summary_writer.add_scalar(tag="global_model_accuracy", scalar=accuracy, global_step=input_model.current_round)
+
+        # (7) construct trained FL model
+        output_model = flare.FLModel(
+            params=trainer.network.cpu().state_dict(),
+            metrics={"accuracy": accuracy},
+            meta={"NUM_STEPS_CURRENT_ROUND": steps},
+        )
+        # (8) send model back to NVFlare
+        flare.send(output_model)
 
 
+if __name__ == "__main__":
+    main()
