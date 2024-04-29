c064acafce735401eec922aa195826857aa17b0d
chesterxgchen <chesterxgchen@yahoo.com>
2024-04-28 23:16:47 -0700

    1. rename ExeTaskFnWrapper class to TaskScriptRunner 2. Replace implementation of the inprocess function exection from calling a main() function to user runpy.run_path() which reduce the user requirements to have main() function 3. redirect print() to logger.info()

--- nvflare/app_common/executors/exec_task_fn_wrapper.py
+++ nvflare/app_common/executors/exec_task_fn_wrapper.py
@@ -11,69 +11,74 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import builtins
 import logging
+import os
 import sys
 import traceback
-from typing import Dict
 
-from nvflare.fuel.utils.function_utils import find_task_fn, require_arguments
+print_fn = builtins.print
 
 
-class ExecTaskFuncWrapper:
-    def __init__(self, task_fn_path: str, task_main_args: str = None, task_fn_args: Dict = None):
+class TaskScriptRunner:
+    logger = logging.getLogger(__name__)
+
+    def __init__(self, script_path: str, script_args: str = None):
         """Wrapper for function given function path and args
 
         Args:
-            task_fn_path (str): function path (ex: train.main, custom/train.main, custom.train.main).
-            task_fn_args (Dict, optional): function arguments to pass in.
+            script_path (str): script file name, such as train.py
+            script_args (str, Optional): script arguments to pass in.
         """
-        self.task_fn_path = task_fn_path
-        self.task_main_args = task_main_args
-        self.task_fn_args = task_fn_args
+        self.script_args = script_args
         self.client_api = None
         self.logger = logging.getLogger(self.__class__.__name__)
-        self.task_fn = find_task_fn(task_fn_path)
-        require_args, args_size, args_default_size = require_arguments(self.task_fn)
-        self.check_fn_inputs(task_fn_path, require_args, args_size, args_default_size)
-        self.task_fn_require_args = require_args
+        self.script_path = self.get_script_full_path(script_path)
 
     def run(self):
         """Call the task_fn with any required arguments."""
-        msg = f"\n start task run() with {self.task_fn_path}"
-        msg = msg if not self.task_fn_require_args else msg + f", {self.task_fn_args}"
-        self.logger.info(msg)
+        self.logger.info(f"\n start task run() with {self.script_path}")
         try:
-            if self.task_fn.__name__ == "main":
-                curr_argv = sys.argv
-                sys.argv = self.get_sys_argv()
-                self.task_fn()
-                sys.argv = curr_argv
-            elif self.task_fn_require_args:
-                self.task_fn(**self.task_fn_args)
-            else:
-                self.task_fn()
+            import runpy
+
+            curr_argv = sys.argv
+            builtins.print = log_print
+            sys.argv = self.get_sys_argv()
+            runpy.run_path(self.script_path, run_name="__main__")
+            sys.argv = curr_argv
+
         except Exception as e:
             msg = traceback.format_exc()
             self.logger.error(msg)
             if self.client_api:
                 self.client_api.exec_queue.ask_abort(msg)
             raise e
+        finally:
+            builtins.print = print_fn
 
     def get_sys_argv(self):
-        args_list = [] if not self.task_main_args else self.task_main_args.split()
-        return [self.task_fn_path.rsplit(".", 1)[0].replace(".", "/") + ".py"] + args_list
+        args_list = [] if not self.script_args else self.script_args.split()
+        return [self.script_path] + args_list
 
-    def check_fn_inputs(self, task_fn_path, require_args: bool, required_args_size: int, args_default_size: int):
-        """Check if the provided task_fn_args are compatible with the task_fn."""
-        if require_args:
-            if not self.task_fn_args:
-                raise ValueError(f"function '{task_fn_path}' requires arguments, but none provided")
-            elif len(self.task_fn_args) < required_args_size - args_default_size:
-                raise ValueError(
-                    f"function '{task_fn_path}' requires {required_args_size} "
-                    f"arguments, but {len(self.task_fn_args)} provided"
-                )
-        else:
-            if self.task_fn_args and self.task_fn.__name__ != "main":
-                msg = f"function '{task_fn_path}' does not require arguments, {self.task_fn_args} will be ignored"
-                self.logger.warning(msg)
+    def get_script_full_path(self, script_path) -> str:
+        target_files = None
+        for r, dirs, files in os.walk(os.getcwd()):
+            target_files = [os.path.join(r, f) for f in files if f == script_path]
+            if target_files:
+                break
+        if not target_files:
+            raise ValueError(f"{script_path} is not found")
+        return target_files[0]
+
+
+def log_print(*args, logger=TaskScriptRunner.logger, **kwargs):
+    # Create a message from print arguments
+    message = " ".join(str(arg) for arg in args)
+    logger.info(message)
+
+
+def tee_print(*args, logger=TaskScriptRunner.logger, **kwargs):
+    # Create a message from print arguments
+    message = " ".join(str(arg) for arg in args)
+    logger.info(message)
+    print_fn(*args, **kwargs)
