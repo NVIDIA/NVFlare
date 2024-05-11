fce5ebbddbe6d21c293329399984247dbdeea30f
Chester Chen <512707+chesterxgchen@users.noreply.github.com>
2024-05-10 21:42:02 -0700

    BugFix: InProcessClientAPIExecutor's TaskScriptRunner (#2558)
    * 1) find script full path to indicate which site script to avoid loading run script
    2) make sure the task script failed will cause the client to return failure status which will trigger job stop rather wait forever
    3) add different unit tests
    
    * sort key in unit test
    
    * add logic to improve error message
    
    * style format
    
    * add more tests and logics
    
    * code format
    
    * code format
    
    * fix steps error
    
    * fix global steps
    
    * rollback some changes and split it into another PR
    
    * rollback some changes and split it into another PR
    
    ---------
    
    Co-authored-by: Yuan-Ting Hsieh (謝沅廷) <yuantingh@nvidia.com>

---
+++ tests/unit_test/data/jobs/in_proc_job/custom/src/train.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+
+from model import Model
+
+
+class Code:
+    # code to test relative import
+    def run(self, dataset_path, batch_size):
+        model = Model()
+        model.train(dataset_path, batch_size)
+
+    def define_parser(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--dataset_path", type=str, default="/data", nargs="?")
+        parser.add_argument("--batch_size", type=int, default=4, nargs="?")
+        return parser.parse_args()
+
+    def main(self):
+        args = self.define_parser()
+        dataset_path = args.dataset_path
+        batch_size = args.batch_size
+        self.run(dataset_path, batch_size)
+
+
+print("__name__ src is ", __name__)
+
+if __name__ == "__main__":
+    code = Code()
+    code.main()
