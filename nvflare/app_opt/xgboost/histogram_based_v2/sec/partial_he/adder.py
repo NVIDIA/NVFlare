0a566341c1cef3bc54cbc2bbe578221d79f69594
Yuan-Ting Hsieh (謝沅廷) <yuantingh@nvidia.com>
2024-06-05 13:49:15 -0700

    Add intel phe (#2612)

--- nvflare/app_opt/xgboost/histogram_based_v2/sec/partial_he/adder.py
+++ nvflare/app_opt/xgboost/histogram_based_v2/sec/partial_he/adder.py
@@ -22,6 +22,7 @@ from .util import encode_encrypted_numbers_to_str
 class Adder:
     def __init__(self, max_workers=10):
         self.exe = concurrent.futures.ProcessPoolExecutor(max_workers=max_workers)
+        self.num_workers = max_workers
 
     def add(self, encrypted_numbers, features, sample_groups=None, encode_sum=True):
         """
@@ -50,7 +51,9 @@ class Adder:
                     gid, sample_id_list = g
                     items.append((encode_sum, fid, encrypted_numbers, mask, num_bins, gid, sample_id_list))
 
-        results = self.exe.map(_do_add, items)
+        chunk_size = int((len(items) - 1) / self.num_workers) + 1
+
+        results = self.exe.map(_do_add, items, chunksize=chunk_size)
         rl = []
         for r in results:
             rl.append(r)
