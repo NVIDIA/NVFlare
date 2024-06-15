0ab03a5b292fd2809f838da81897146852e6b12d
Ziyue Xu <71786575+ZiyueXu77@users.noreply.github.com>
2024-06-14 21:49:21 -0400

    remove standalone FL scripts to avoid confusion

--- examples/advanced/xgboost_secure/run_training_local.sh
+++ examples/advanced/xgboost_secure/run_training_local.sh
@@ -3,11 +3,3 @@ mkdir /tmp/nvflare/xgb_exp
 
 echo "Training baseline"
 python3 ./train_local/train_base.py 3
-echo "Training horizontal"
-python3 ./train_local/train_hori_base.py 3
-echo "Training secure horizontal"
-python3 ./train_local/train_hori_secure.py 3
-echo "Training vertical"
-python3 ./train_local/train_vert_base.py 3
-echo "Training secure vertical"
-python3 ./train_local/train_vert_secure.py 3
