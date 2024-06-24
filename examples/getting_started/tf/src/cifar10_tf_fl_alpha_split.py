42d095d4af023c394eb1bbe19031cec6033a41fb
Zhijin Li <zhijinl@nvidia.com>
2024-06-24 17:37:33 +0200

    Add preprocessing to match torch CIFAR10 result.

--- examples/getting_started/tf/src/cifar10_tf_fl_alpha_split.py
+++ examples/getting_started/tf/src/cifar10_tf_fl_alpha_split.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 import argparse
 import numpy as np
 
@@ -24,6 +25,7 @@ import nvflare.client as flare
 
 # (optional) metrics
 from nvflare.client.tracking import SummaryWriter
+
 
 PATH = "./tf_model.weights.h5"
 
@@ -69,6 +71,105 @@ class SetFedProxValues(callbacks.Callback):
         self.fedprox_loss.current_model = {layer.name: layer.get_weights() for layer in self.model.layers}
 
 
+def preprocess_dataset(dataset, is_training, batch_size=1):
+    """
+    Apply pre-processing transformations to CIFAR10 dataset.
+
+    Same pre-processings are used as in the Pytorch tutorial
+    on CIFAR10: https://github.com/NVIDIA/NVFlare/tree/main/examples/advanced/cifar10/cifar10-sim
+
+    Training time pre-processings are (in-order):
+    - Image padding with 4 pixels in "reflect" mode on each side
+    - RandomCrop of 32 x 32 images
+    - RandomHorizontalFlip
+    - Normalize to [0, 1]: dividing pixels values by given CIFAR10 data mean & std
+    - Random shuffle
+
+    Testing/Validation time pre-processings are:
+    - Normalize: dividing pixels values by 255
+
+    Args
+    ----------
+    dataset: tf.data.Datset
+    Tensorflow Dataset
+
+    is_training: bool
+    Boolean flag indicating if current phase is training phase.
+
+    batch_size: int
+    Batch size
+
+    Returns
+    ----------
+    tf.data.Dataset
+    Tensorflow Dataset with pre-processings applied.
+
+    """
+    # Values from: https://github.com/NVIDIA/NVFlare/blob/fc2bc47889b980c8de37de5528e3d07e6b1a942e/examples/advanced/cifar10/pt/learners/cifar10_model_learner.py#L147
+    mean_cifar10 = tf.constant([125.3, 123.0, 113.9], dtype=tf.float32)
+    std_cifar10 = tf.constant([63.0, 62.1, 66.7], dtype=tf.float32)
+
+    if is_training:
+
+
+        # Padding each dimension by 4 pixels each side
+        dataset = dataset.map(
+            lambda image, label: (
+                tf.stack(
+                    [
+                        tf.pad(tf.squeeze(t, [2]), [[4, 4], [4, 4]], mode='REFLECT')
+                        for t in tf.split(image, num_or_size_splits=3, axis=2)
+                    ],
+                    axis=2
+                ),
+                label
+            )
+        )
+
+        # Random crop of 32 x 32 x 3
+        dataset = dataset.map(
+            lambda image, label: (
+                tf.image.random_crop(
+                    image,
+                    size=(32, 32, 3)
+                ),
+                label
+            )
+        )
+
+        # Random horizontal flip
+        dataset = dataset.map(
+            lambda image, label: (
+                tf.image.random_flip_left_right(image),
+                label
+            )
+        )
+
+        # Normalize by dividing by given mean & std
+        dataset = dataset.map(
+            lambda image, label: (
+                (tf.cast(image, tf.float32) - mean_cifar10) / std_cifar10,
+                label
+            )
+        )
+
+        # Random shuffle
+        dataset = dataset.shuffle(len(dataset), reshuffle_each_iteration=True)
+
+        # Convert to batches.
+        return dataset.batch(batch_size)
+
+    else:
+
+        # For validation / test only do normalization.
+        return dataset.map(
+            lambda image, label: (
+                (tf.cast(image, tf.float32) - mean_cifar10) / std_cifar10,
+                label
+            )
+        ).batch(batch_size)
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -96,27 +197,36 @@ def main():
     # (2) initializes NVFlare client API
     flare.init()
 
-    # load train indices
-    print(f"Loading train indices from {args.train_idx_path}")
-    train_idx = np.load(args.train_idx_path)
-
     (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 
-    train_images = train_images[train_idx]
-    train_labels = train_labels[train_idx]
+    # Doing alpha splitting to simulate data heteogeniety,
+    # only if if train_idx_path is not None.
+    #
+    if args.train_idx_path != "None":
+
+        print(f"Loading train indices from {args.train_idx_path}")
+        train_idx = np.load(args.train_idx_path)
+        print(f"Loaded {len(train_idx)} training indices with label distribution:")
+
+        train_images = train_images[train_idx]
+        train_labels = train_labels[train_idx]
 
     unq, unq_cnt = np.unique(train_labels, return_counts=True)
-    print(f"Loaded {len(train_idx)} training indices with label distribution:")
     print("Unique labels:", unq)
     print("Unique Counts:", unq_cnt)
 
-    # Normalize pixel values to be between 0 and 1
-    train_images, test_images = train_images / 255.0, test_images / 255.0
+    # Convert training & testing data to datasets
+    train_ds = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
+    test_ds = tf.data.Dataset.from_tensor_slices((test_images, test_labels))
+
+    # Preprocessing
+    train_ds = preprocess_dataset(train_ds, is_training=True, batch_size=args.batch_size)
+    test_ds = preprocess_dataset(test_ds, is_training=False, batch_size=args.batch_size)
 
     model = ModerateTFNet()
     model.build(input_shape=(None, 32, 32, 3))
 
-    callbacks = [tf.keras.callbacks.TensorBoard(log_dir="./logs", write_graph=False)]
+    callbacks = [tf.keras.callbacks.TensorBoard(log_dir="./logs_keras", write_graph=False)]
 
     if args.fedprox_mu > 0:
         loss = SparseCategoricalCrossentropyWithFedProx(from_logits=True)
@@ -150,6 +260,7 @@ def main():
         # (5) evaluate aggregated/received model
         _, test_global_acc = model.evaluate(test_images, test_labels, verbose=2)
         summary_writer.add_scalar(tag="global_model_accuracy", scalar=test_global_acc, global_step=input_model.current_round)
+
         with tf_summary_writer.as_default():
             tf.summary.scalar("global_model_accuracy", test_global_acc, input_model.current_round)
         print(
@@ -158,16 +269,26 @@ def main():
 
         start_epoch = args.epochs*input_model.current_round
         end_epoch = start_epoch + args.epochs
+
         print(f"Train from epoch {start_epoch} to {end_epoch}")
-        model.fit(train_images, train_labels, epochs=end_epoch, validation_data=(test_images, test_labels), batch_size=args.batch_size, callbacks=callbacks,
-                  initial_epoch=start_epoch, validation_freq=args.epochs)
+        model.fit(
+            x=train_ds,
+            epochs=end_epoch,
+            validation_data=test_ds,
+            callbacks=callbacks,
+            initial_epoch=start_epoch,
+            validation_freq=1 #args.epochs
+        )
 
         print("Finished Training")
 
         model.save_weights(PATH)
 
-        _, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+        # _, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+        _, test_acc = model.evaluate(x=test_ds, verbose=2)
+
         summary_writer.add_scalar(tag="local_model_accuracy", scalar=test_acc, global_step=input_model.current_round)
+
         with tf_summary_writer.as_default():
             tf.summary.scalar("local_model_accuracy", test_acc, input_model.current_round)
         print(f"Accuracy of the model on the {len(test_images)} test images: {test_acc * 100} %")
