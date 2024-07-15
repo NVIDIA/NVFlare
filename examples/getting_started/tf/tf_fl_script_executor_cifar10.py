ddc687d8fb3e343c36f228dbff42d9a8bd097fad
LeoDuda <50424904+LeoDuda@users.noreply.github.com>
2024-07-15 13:18:32 +0200

    Changed path to scaffold workflow

--- examples/getting_started/tf/tf_fl_script_executor_cifar10.py
+++ examples/getting_started/tf/tf_fl_script_executor_cifar10.py
@@ -137,7 +137,7 @@ if __name__ == "__main__":
 
     elif args.algo == SCAFFOLD_ALGO:
         train_script = "src/cifar10_tf_fl_alpha_split_scaffold.py"
-        from nvflare.app_opt.tf.scaffold_workflow import Scaffold
+        from nvflare.app_common.workflows.scaffold import Scaffold
         controller = Scaffold(
             min_clients=args.n_clients,
             num_rounds=args.num_rounds,
