6c4d9c8659611639e0319fb87341d0b829608a02
Yuhong Wen <yuhongw@nvidia.com>
2024-07-24 14:18:33 -0400

    Changed to use job.as_id().

--- examples/getting_started/pt/swarm_script_executor_cifar10.py
+++ examples/getting_started/pt/swarm_script_executor_cifar10.py
@@ -47,19 +47,23 @@ if __name__ == "__main__":
         executor = ScriptExecutor(task_script_path=train_script)
         job.to(executor, f"site-{i}", gpu=0, tasks=["train", "validate", "submit_model"])
 
-        client_controller = SwarmClientController()
+        # In swarm learning, each client acts also as an aggregator
+        aggregator = InTimeAccumulateWeightedAggregator(expected_data_kind=DataKind.WEIGHTS)
+        job.to(aggregator, f"site-{i}")
+
+        # In swarm learning, each client uses a model persistor and shareable_generator
+        persistor = PTFileModelPersistor(model=Net())
+        job.to(persistor, f"site-{i}")
+        job.to(SimpleModelShareableGenerator(), f"site-{i}", id="shareable_generator")
+
+        client_controller = SwarmClientController(
+            aggregator_id=job.as_id(aggregator),
+            persistor_id=job.as_id(persistor)
+        )
         job.to(client_controller, f"site-{i}", tasks=["swarm_*"])
 
         client_controller = CrossSiteEvalClientController()
         job.to(client_controller, f"site-{i}", tasks=["cse_*"])
-
-        # In swarm learning, each client acts also as an aggregator
-        aggregator = InTimeAccumulateWeightedAggregator(expected_data_kind=DataKind.WEIGHTS)
-        job.to(aggregator, f"site-{i}", id="aggregator")
-
-        # In swarm learning, each client uses a model persistor and shareable_generator
-        job.to(PTFileModelPersistor(model=Net()), f"site-{i}", id="persistor")
-        job.to(SimpleModelShareableGenerator(), f"site-{i}", id="shareable_generator")
 
     # job.export_job("/tmp/nvflare/jobs/job_config")
     job.simulator_run("/tmp/nvflare/jobs/workdir")
