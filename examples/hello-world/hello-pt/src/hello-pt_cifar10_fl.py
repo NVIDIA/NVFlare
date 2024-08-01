a49a8d5686bcc4ca3666418dda09da3ddfc3b448
Kevin <kevlu@nvidia.com>
2024-07-31 21:41:34 -0400

    fix CI

--- examples/hello-world/hello-pt/src/hello-pt_cifar10_fl.py
+++ examples/hello-world/hello-pt/src/hello-pt_cifar10_fl.py
@@ -23,8 +23,6 @@ from torchvision.datasets import CIFAR10
 from torchvision.transforms import Compose, Normalize, ToTensor
 
 import nvflare.client as flare
-from nvflare.app_opt.pt.model_persistence_format_manager import PTModelPersistenceFormatManager
-from nvflare.client.tracking import SummaryWriter
 
 DATASET_PATH = "/tmp/nvflare/data"
 
