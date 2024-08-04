b94c2101e918e0fc329d7e7757ab4580d3941acc
chesterxgchen <chesterxgchen@yahoo.com>
2024-08-03 20:35:33 -0700

    license headers

--- nvflare/app_common/workflows/generic_task_controller.py
+++ nvflare/app_common/workflows/generic_task_controller.py
@@ -1,3 +1,18 @@
+# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 from nvflare.app_common.abstract.generic_task import GenericTask
 from nvflare.app_common.workflows.model_controller import ModelController
 
@@ -14,8 +29,6 @@ class GenericTaskController(ModelController):
         task = GenericTask()
         clients = self.sample_clients()
         results = self.send_task_and_wait(task_name=self.task_name, targets=clients, data=task)
-
-        print(f"\n\n =================== controller result = {results} ")
 
         self.info("Finished etl.")
 
