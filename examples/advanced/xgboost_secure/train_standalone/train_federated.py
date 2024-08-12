57bb3dc192724fc4dfaa8576bd6df136e75c2339
Ziyue Xu <ziyue.xu@gmail.com>
2024-08-12 10:55:29 -0400

    format update

--- examples/advanced/xgboost_secure/train_standalone/train_federated.py
+++ examples/advanced/xgboost_secure/train_standalone/train_federated.py
@@ -12,10 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import multiprocessing
 import argparse
+import multiprocessing
 import os
-import sys
 import time
 
 import matplotlib.pyplot as plt
