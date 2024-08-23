045f046732ed1d8b31fe136eb39d52c692c2c348
YuanTingHsieh <yuantingh@nvidia.com>
2024-08-22 17:57:34 -0700

    Undo unnecessary change

--- examples/advanced/finance/run_testing.sh
+++ examples/advanced/finance/run_testing.sh
@@ -2,7 +2,7 @@
 echo "Testing baseline_xgboost"
 python test_xgboost.py --model_path ./workspaces/xgboost_workspace_centralized/model_centralized.json
 echo "Testing xgboost_vertical"
-python test_xgboost.py --model_path ./workspaces/xgboost_workspace_vertical/site-1/simulate_job/model.json
+python test_xgboost.py --model_path ./workspaces/xgboost_workspace_vertical/site-1/simulate_job/test.model.json
 echo "Testing xgboost_horizontal_histogram"
 python test_xgboost.py --model_path ./workspaces/xgboost_workspace_2_histogram/site-1/simulate_job/test.model.json
 echo "Testing xgboost_horizontal_cyclic"
