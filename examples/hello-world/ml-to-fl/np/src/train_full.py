a6e110098397a0cf12b51f900353ced8927d56cf
Sean Yang <seany314@gmail.com>
2024-08-23 18:30:53 -0700

    Update ml-to-fl examples with new APIs (#2836)
    * update ml-to-fl examples with new apis
    
    * address comments
    
    * add export config option
    
    * rename to launch_process
    
    ---------
    
    Co-authored-by: Chester Chen <512707+chesterxgchen@users.noreply.github.com>

--- examples/hello-world/ml-to-fl/np/src/train_full.py
+++ examples/hello-world/ml-to-fl/np/src/train_full.py
@@ -32,28 +32,36 @@ def main():
     # initializes NVFlare interface
     flare.init()
 
-    # get model from NVFlare
-    input_model = flare.receive()
-    print(f"received weights is: {input_model.params}")
-
     # get system information
     sys_info = flare.system_info()
     print(f"system info is: {sys_info}")
 
-    input_numpy_array = input_model.params["numpy_key"]
+    while flare.is_running():
 
-    # training
-    output_numpy_array = train(input_numpy_array)
+        # get model from NVFlare
+        input_model = flare.receive()
+        print(f"received weights is: {input_model.params}", flush=True)
 
-    # evaluation
-    metrics = evaluate(input_numpy_array)
+        input_numpy_array = input_model.params["numpy_key"]
 
-    # calculate difference here
-    diff = output_numpy_array - input_numpy_array
+        # training
+        output_numpy_array = train(input_numpy_array)
 
-    # send back the model difference
-    print(f"send back: {diff}")
-    flare.send(flare.FLModel(params={"numpy_key": diff}, params_type="DIFF", metrics={"accuracy": metrics}))
+        # evaluation
+        metrics = evaluate(input_numpy_array)
+
+        print(f"finish round: {input_model.current_round}", flush=True)
+
+        # send back the model
+        print(f"send back: {output_numpy_array}", flush=True)
+        flare.send(
+            flare.FLModel(
+                params={"numpy_key": output_numpy_array},
+                params_type="FULL",
+                metrics={"accuracy": metrics},
+                current_round=input_model.current_round,
+            )
+        )
 
 
 if __name__ == "__main__":
