b7c39fd54e168de27546aa2a83e9105e45865bea
Yuhong Wen <yuhongw@nvidia.com>
2024-08-25 00:47:32 -0400

    hello-pt-mlflow job api example (#2839)
    * WIP
    
    * hello-pt-mlflow job api example.
    
    * Extracted the BaseFedJob for FedAvgJob and SAGMLFlowJob.
    
    * refactoried.
    
    * reformat
    
    ---------
    
    Co-authored-by: Chester Chen <512707+chesterxgchen@users.noreply.github.com>

--- nvflare/app_opt/pt/job_config/fed_sag_mlflow.py
+++ nvflare/app_opt/pt/job_config/fed_sag_mlflow.py
@@ -15,11 +15,16 @@ from typing import List, Optional
 
 import torch.nn as nn
 
-from nvflare.app_common.workflows.fedavg import FedAvg
+from nvflare.apis.dxo import DataKind
+from nvflare.app_common.aggregators import InTimeAccumulateWeightedAggregator
+from nvflare.app_common.shareablegenerators import FullModelShareableGenerator
+from nvflare.app_common.workflows.scatter_and_gather import ScatterAndGather
 from nvflare.app_opt.pt.job_config.base_fed_job import BaseFedJob
+from nvflare.app_opt.tracking.mlflow.mlflow_receiver import MLflowReceiver
+from nvflare.app_opt.tracking.mlflow.mlflow_writer import MLflowWriter
 
 
-class FedAvgJob(BaseFedJob):
+class SAGMLFlowJob(BaseFedJob):
     def __init__(
         self,
         initial_model: nn.Module,
@@ -29,10 +34,13 @@ class FedAvgJob(BaseFedJob):
         min_clients: int = 1,
         mandatory_clients: Optional[List[str]] = None,
         key_metric: str = "accuracy",
+        tracking_uri=None,
+        kwargs=None,
+        artifact_location=None,
     ):
-        """PyTorch FedAvg Job.
+        """PyTorch ScatterAndGather with MLFlow Job.
 
-        Configures server side FedAvg controller, persistor with initial model, and widgets.
+        Configures server side ScatterAndGather controller, persistor with initial model, and widgets.
 
         User must add executors.
 
@@ -46,12 +54,31 @@ class FedAvgJob(BaseFedJob):
             key_metric (str, optional): Metric used to determine if the model is globally best.
                 if metrics are a `dict`, `key_metric` can select the metric used for global model selection.
                 Defaults to "accuracy".
+            kwargs: kwargs dict
         """
         super().__init__(initial_model, name, min_clients, mandatory_clients, key_metric)
 
-        controller = FedAvg(
-            num_clients=n_clients,
+        shareable_generator = FullModelShareableGenerator()
+        shareable_generator_id = self.to_server(shareable_generator, id="shareable_generator")
+        aggregator_id = self.to_server(
+            InTimeAccumulateWeightedAggregator(expected_data_kind=DataKind.WEIGHTS), id="aggregator"
+        )
+
+        component = MLflowReceiver(tracking_uri=tracking_uri, kw_args=kwargs, artifact_location=artifact_location)
+        self.to_server(id="mlflow_receiver_with_tracking_uri", obj=component)
+
+        controller = ScatterAndGather(
+            min_clients=n_clients,
             num_rounds=num_rounds,
+            wait_time_after_min_received=10,
+            aggregator_id=aggregator_id,
             persistor_id=self.comp_ids["persistor_id"],
+            shareable_generator_id=shareable_generator_id,
         )
         self.to_server(controller)
+
+    def set_up_client(self, target: str):
+        super().set_up_client(target)
+
+        ml_flow_writer = MLflowWriter(event_type="event_type")
+        self.to(id="log_writer", obj=ml_flow_writer, target=target)
