01e1f27ba0c453b3d7b62d94e5db23aa1e885302
chesterxgchen <chesterxgchen@yahoo.com>
2024-08-26 11:18:23 -0700

    rename executor to runner

--- examples/hello-world/step-by-step/higgs/sklearn-linear/sklearn_linear_job.py
+++ examples/hello-world/step-by-step/higgs/sklearn-linear/sklearn_linear_job.py
@@ -55,8 +55,8 @@ if __name__ == "__main__":
 
     # Add clients
     for i in range(n_clients):
-        executor = ScriptRunner(script=train_script, script_args=script_args, framework=FrameworkType.RAW)
-        job.to(executor, f"site-{i + 1}")
+        runner = ScriptRunner(script=train_script, script_args=script_args, framework=FrameworkType.RAW)
+        job.to(runner, f"site-{i + 1}")
 
     job.export_job("/tmp/nvflare/jobs")
     job.simulator_run("/tmp/nvflare/sklearn_sgd", gpu="0")
