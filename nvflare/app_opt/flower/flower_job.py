9737f53af0cf72d9aff74f8af8570a56614ccdcb
Yuan-Ting Hsieh (謝沅廷) <yuantingh@nvidia.com>
2024-08-27 17:41:44 -0700

    Add flower metrics streaming example (#2764)
    * Add flower metrics streaing example
    
    * Fix format
    
    * Use context and RecordSet
    
    * Undo stuff
    
    * Update to new style
    
    * Update hello-flwr-pt_tb_streaming
    
    * Remove debug msgs
    
    * Update readme
    
    * Use flower job
    
    * Add missing code
    
    * Make client api type an arg

--- nvflare/app_opt/flower/flower_job.py
+++ nvflare/app_opt/flower/flower_job.py
@@ -11,10 +11,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import os.path
 from typing import List, Optional
 
 from nvflare.app_common.tie.defs import Constant
+from nvflare.app_common.widgets.external_configurator import ExternalConfigurator
+from nvflare.app_common.widgets.metric_relay import MetricRelay
+from nvflare.app_common.widgets.streaming import AnalyticsReceiver
+from nvflare.app_opt.tracking.tb.tb_receiver import TBAnalyticsReceiver
+from nvflare.fuel.utils.pipe.cell_pipe import CellPipe
+from nvflare.fuel.utils.validation_utils import check_object_type
 from nvflare.job_config.api import FedJob
 
 from .controller import FlowerController
@@ -24,7 +31,7 @@ from .executor import FlowerExecutor
 class FlowerJob(FedJob):
     def __init__(
         self,
-        job_name: str,
+        name: str,
         flower_content: str,
         min_clients: int = 1,
         mandatory_clients: Optional[List[str]] = None,
@@ -38,12 +45,15 @@ class FlowerJob(FedJob):
         per_msg_timeout=10.0,
         tx_timeout=100.0,
         client_shutdown_timeout=5.0,
+        stream_metrics=False,
+        analytics_receiver=None,
+        client_api_type: str = "EX_PROCESS_API",
     ):
         """
         Flower Job.
 
         Args:
-            job_name (str): Name of the job.
+            name (str): Name of the job.
             flower_content (str): Content for the flower job.
             min_clients (int, optional): The minimum number of clients for the job. Defaults to 1.
             mandatory_clients (List[str], optional): List of mandatory clients for the job. Defaults to None.
@@ -57,11 +67,18 @@ class FlowerJob(FedJob):
             per_msg_timeout (float, optional): Timeout for receiving individual messages. Defaults to 10.0 seconds.
             tx_timeout (float, optional): Timeout for transmitting data. Defaults to 100.0 seconds.
             client_shutdown_timeout (float, optional): Timeout for client shutdown. Defaults to 5.0 seconds.
+            stream_metrics (bool, optional): Whether to stream metrics from Flower client to Flare
+            analytics_receiver (AnalyticsReceiver, optional): the AnalyticsReceiver to use to process received metrics.
+            client_api_type (str, optional): Client API type, can choose from EX_PROCESS_API and IN_PROCESS_API
         """
         if not os.path.isdir(flower_content):
             raise ValueError(f"{flower_content} is not a valid directory")
 
-        super().__init__(name=job_name, min_clients=min_clients, mandatory_clients=mandatory_clients)
+        super().__init__(name=name, min_clients=min_clients, mandatory_clients=mandatory_clients)
+
+        if client_api_type not in ["EX_PROCESS_API", "IN_PROCESS_API"]:
+            raise ValueError("Invalid client api type.")
+        os.environ["CLIENT_API_TYPE"] = client_api_type
 
         controller = FlowerController(
             database=database,
@@ -82,3 +99,39 @@ class FlowerJob(FedJob):
         )
         self.to_clients(executor)
         self.to_clients(obj=flower_content)
+
+        if not stream_metrics:
+            return
+
+        # add required components for metrics streaming
+        # server side - need analytics_receiver
+        if analytics_receiver:
+            check_object_type("analytics_receiver", analytics_receiver, AnalyticsReceiver)
+        else:
+            analytics_receiver = TBAnalyticsReceiver(events=["fed.analytix_log_stats"])
+
+        self.to_server(analytics_receiver, "analytics_receiver")
+
+        # client side
+        # cell pipe
+        cell_pipe = CellPipe(
+            mode="PASSIVE",
+            site_name="{SITE_NAME}",
+            token="{JOB_ID}",
+            root_url="{ROOT_URL}",
+            secure_mode="{SECURE_MODE}",
+            workspace_dir="{WORKSPACE}",
+        )
+        pipe_id = self.to_clients(cell_pipe, "metrics_pipe")
+
+        metric_relay = MetricRelay(
+            pipe_id=pipe_id,
+            event_type="fed.analytix_log_stats",
+            read_interval=0.1,
+            heartbeat_timeout=0,
+            fed_event=True,
+        )
+
+        relay_id = self.to_clients(metric_relay, "metric_relay")
+        conf = ExternalConfigurator(component_ids=[relay_id])
+        self.to_clients(conf, "client_api_config_preparer")
