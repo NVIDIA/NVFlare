42e5fce2acd4f70f5a571951b2c73770d661b791
chesterxgchen <chesterxgchen@yahoo.com>
2024-09-06 11:13:35 -0700

    remove redundant move

--- examples/advanced/federated-statistics/hierarchical_stats/utils/prepare_data.py
+++ examples/advanced/federated-statistics/hierarchical_stats/utils/prepare_data.py
@@ -42,7 +42,12 @@ def prepare_data():
 
     # Generate the entries for 7 different universities and copy it to each client data directory
     for n in range(1, num_universities + 1):
-        output_file = f"university-{n}.csv"
+        client_path = os.path.join(dataset_path, f"university-{n}")
+        if not os.path.exists(client_path):
+            os.makedirs(client_path)
+        file_name = f"university-{n}.csv"
+        output_file = os.path.join(client_path, file_name)
+
         with open(output_file, "w", newline="") as csvfile:
             csvwriter = csv.writer(csvfile)
             # Create and write the header to the CSV file
@@ -58,13 +63,8 @@ def prepare_data():
                     fail = 1
                     percentage = round(random.uniform(20.00, 49.99), 2)
                 csvwriter.writerow([pass_, fail, percentage])
-
-        client_path = os.path.join(dataset_path, f"university-{n}")
-        if not os.path.exists(client_path):
-            os.makedirs(client_path)
-        shutil.move(output_file, os.path.join(client_path, output_file))
         print(
-            f"CSV file `{output_file}` is generated with {num_entries} entries for client `university-{n}` at {client_path}."
+            f"CSV file `{file_name}` is generated with {num_entries} entries for client `university-{n}` at {client_path}."
         )
 
     print("\nDone preparing data.")
