8947b9aee95cd4370d7298241356f2e1d36aae4f
Ziyue Xu <ziyue.xu@gmail.com>
2024-09-20 13:42:57 -0400

    update eval bash

--- examples/advanced/rag/embedding/eval_all.sh
+++ examples/advanced/rag/embedding/eval_all.sh
@@ -1,7 +1,8 @@
 for dataset_name in nli squad quora all
 do
-    echo "Evaluation on ${dataset_name} with model"
+    echo "Evaluation on model ${dataset_name}"
     python utils/eval_model.py --model_path /tmp/embed/cen/models_single/mpnet-base-${dataset_name}/final
 done
 
+echo "Evaluation on model federated"
 python utils/eval_model.py --model_path /tmp/embed/nvflare/workspace/site-1/simulate_job/app_site-1/models/mpnet-base-nli/global
