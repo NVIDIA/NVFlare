4972fb47a359fb197e3523fca9111650bcf4005e
Kevin <kevlu@nvidia.com>
2024-11-26 11:40:03 -0500

    more cleanup and use thread for sending

--- nvflare/app_common/widgets/log_streaming.py
+++ nvflare/app_common/widgets/log_streaming.py
@@ -12,16 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from http import client
 import os
-from threading import Lock
-import time
-from typing import List, Optional
+import threading
 from builtins import dict as StreamContext
 
-from flask.cli import F
-
-from nvflare.apis.dxo import DXO, DataKind
 from nvflare.apis.event_type import EventType
 from nvflare.apis.fl_constant import FLContextKey, ReturnCode, SystemComponents
 from nvflare.apis.fl_context import FLContext
@@ -40,44 +34,43 @@ class LogConst(object):
 
 class LogSender(Widget):
     def __init__(self, event_type=EventType.JOB_COMPLETED, should_report_error_log: bool = True):
-        """Sender for analytics data.
-
-        Args:
-            event_type (str): event type to fire (defaults to "stream_log").
-        """
+        """Sender for analytics data."""
         super().__init__()
         self.event_type = event_type
         self.should_report_error_log = should_report_error_log
 
+    def _stream_error_log_file(self, fl_ctx: FLContext):
+        error_log_contents = None
+        workspace_root = fl_ctx.get_prop(FLContextKey.WORKSPACE_ROOT)
+        client_name = fl_ctx.get_prop(FLContextKey.CLIENT_NAME)
+        job_id = fl_ctx.get_prop(FLContextKey.CURRENT_JOB_ID)
+        workspace_object = Workspace(root_dir=workspace_root, site_name=client_name)
+        error_log_path = workspace_object.get_app_error_log_file_path(job_id=job_id)
+        if os.path.exists(error_log_path):
+            with open(error_log_path, "r") as f:
+                error_log_contents = f.read()
+        if error_log_contents:
+            FileStreamer.stream_file(
+                channel="error_logs",
+                topic=LOG_STREAM_EVENT_TYPE,
+                stream_ctx={LogConst.CLIENT_NAME: client_name, LogConst.JOB_ID: job_id},
+                targets=["server"],
+                file_name=error_log_path,
+                fl_ctx=fl_ctx,
+            )
+
     def handle_event(self, event_type: str, fl_ctx: FLContext):
         if event_type == self.event_type:
             if self.should_report_error_log:
-                error_log_contents = None
-                workspace_root = fl_ctx.get_prop(FLContextKey.WORKSPACE_ROOT)
+                t = threading.Thread(target=self._stream_error_log_file, args=(fl_ctx,), daemon=True)
+                t.start()
                 client_name = fl_ctx.get_prop(FLContextKey.CLIENT_NAME)
                 job_id = fl_ctx.get_prop(FLContextKey.CURRENT_JOB_ID)
-                workspace_object = Workspace(root_dir=workspace_root, site_name=client_name)
-                error_log_path = workspace_object.get_app_error_log_file_path(job_id=job_id)
-                if os.path.exists(error_log_path):
-                    with open(error_log_path, "r") as f:
-                        error_log_contents = f.read()
-                if error_log_contents:
-                    FileStreamer.stream_file(
-                        channel="error_logs",
-                        topic=LOG_STREAM_EVENT_TYPE,
-                        stream_ctx={LogConst.CLIENT_NAME: client_name, LogConst.JOB_ID: job_id},
-                        targets=["server"],
-                        file_name=error_log_path,
-                        fl_ctx=fl_ctx,
-                    )
+                self.log_info(fl_ctx, f"Started streaming error log file for {client_name} for job {job_id}")
 
 class LogReceiver(Widget):
     def __init__(self):
-        """Receives log data.
-
-        Args:
-            events (optional, List[str]): A list of event that this receiver will handle.
-        """
+        """Receives log data."""
         super().__init__()
 
     def process_log(self, stream_ctx: StreamContext, fl_ctx: FLContext):
