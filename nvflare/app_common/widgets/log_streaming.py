94057828ce832e13a4fd36d451bf046e4dc832db
Kevin <kevlu@nvidia.com>
2024-12-05 14:23:20 -0500

    add check

--- nvflare/app_common/widgets/log_streaming.py
+++ nvflare/app_common/widgets/log_streaming.py
@@ -17,7 +17,7 @@ import threading
 from builtins import dict as StreamContext
 
 from nvflare.apis.event_type import EventType
-from nvflare.apis.fl_constant import FLContextKey, ReturnCode, SystemComponents
+from nvflare.apis.fl_constant import FLContextKey, ProcessType, ReturnCode, SystemComponents
 from nvflare.apis.fl_context import FLContext
 from nvflare.apis.workspace import Workspace
 from nvflare.app_common.streamers.file_streamer import FileStreamer
@@ -59,23 +59,24 @@ class LogSender(Widget):
 
     def handle_event(self, event_type: str, fl_ctx: FLContext):
         if event_type == self.event_type:
-            if self.should_report_error_log:
-                workspace_root = fl_ctx.get_prop(FLContextKey.WORKSPACE_ROOT)
-                client_name = fl_ctx.get_prop(FLContextKey.CLIENT_NAME)
-                job_id = fl_ctx.get_prop(FLContextKey.CURRENT_JOB_ID)
-                workspace_object = Workspace(root_dir=workspace_root, site_name=client_name)
-                error_log_path = workspace_object.get_app_error_log_file_path(job_id=job_id)
+            if fl_ctx.get_process_type() == ProcessType.CLIENT_PARENT:
+                if self.should_report_error_log:
+                    workspace_root = fl_ctx.get_prop(FLContextKey.WORKSPACE_ROOT)
+                    client_name = fl_ctx.get_prop(FLContextKey.CLIENT_NAME)
+                    job_id = fl_ctx.get_prop(FLContextKey.CURRENT_JOB_ID)
+                    workspace_object = Workspace(root_dir=workspace_root, site_name=client_name)
+                    error_log_path = workspace_object.get_app_error_log_file_path(job_id=job_id)
 
-                if os.path.exists(error_log_path):
-                    t = threading.Thread(
-                        target=self._stream_log_file,
-                        args=(fl_ctx, error_log_path, LogChannels.ERROR_LOGS_CHANNEL),
-                        daemon=True,
-                    )
-                    t.start()
-                    self.log_info(fl_ctx, f"Started streaming error log file for {client_name} for job {job_id}")
-                else:
-                    self.log_info(fl_ctx, f"No error log file found for {client_name} for job {job_id}")
+                    if os.path.exists(error_log_path):
+                        t = threading.Thread(
+                            target=self._stream_log_file,
+                            args=(fl_ctx, error_log_path, LogChannels.ERROR_LOGS_CHANNEL),
+                            daemon=True,
+                        )
+                        t.start()
+                        self.log_info(fl_ctx, f"Started streaming error log file for {client_name} for job {job_id}")
+                    else:
+                        self.log_info(fl_ctx, f"No error log file found for {client_name} for job {job_id}")
 
 
 class LogReceiver(Widget):
