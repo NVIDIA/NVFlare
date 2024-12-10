91a0bea52ca00ae65a640ba043a6dd67cfadb685
Sean Yang <seany314@gmail.com>
2024-12-10 10:22:09 -0800

    add additional BaseFedJob layer

--- nvflare/app_opt/pt/job_config/fed_sag_mlflow.py
+++ nvflare/app_opt/pt/job_config/fed_sag_mlflow.py
@@ -19,12 +19,12 @@ from nvflare.apis.dxo import DataKind
 from nvflare.app_common.aggregators import InTimeAccumulateWeightedAggregator
 from nvflare.app_common.shareablegenerators import FullModelShareableGenerator
 from nvflare.app_common.workflows.scatter_and_gather import ScatterAndGather
-from nvflare.app_opt.pt.job_config.base_fed_job import BaseFedJob
+from nvflare.app_opt.pt.job_config.base_fed_job import PTBaseFedJob
 from nvflare.app_opt.tracking.mlflow.mlflow_receiver import MLflowReceiver
 from nvflare.app_opt.tracking.mlflow.mlflow_writer import MLflowWriter
 
 
-class SAGMLFlowJob(BaseFedJob):
+class SAGMLFlowJob(PTBaseFedJob):
     def __init__(
         self,
         initial_model: nn.Module,
