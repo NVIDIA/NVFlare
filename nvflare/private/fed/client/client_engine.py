32769607ee772c4129a28375836f153fc995292e
Yuhong Wen <yuhongw@nvidia.com>
2024-12-13 19:34:25 -0500

    Removed the extra client app custom path. (#3102)

--- nvflare/private/fed/client/client_engine.py
+++ nvflare/private/fed/client/client_engine.py
@@ -152,12 +152,6 @@ class ClientEngine(ClientEngineInternalSpec):
             return f"{ERROR_MSG_PREFIX}: Client app does not exist. Please deploy it before starting client."
 
         app_custom_folder = os.path.join(app_root, "custom")
-        if os.path.isdir(app_custom_folder):
-            try:
-                sys.path.index(app_custom_folder)
-            except ValueError:
-                _remove_custom_path()
-                sys.path.append(app_custom_folder)
 
         self.logger.info("Starting client app. rank: {}".format(self.rank))
 
