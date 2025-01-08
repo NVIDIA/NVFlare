dc23c7c37c8bd6484bed862d6d92e613f0e7c658
chesterxgchen <chesterxgchen@yahoo.com>
2025-01-08 12:31:26 -0800

    refactoring code

--- nvflare/metrics/job_metrics_collector.py
+++ nvflare/metrics/job_metrics_collector.py
@@ -13,17 +13,13 @@
 # limitations under the License.
 
 
-import time
 
 from nvflare.apis.event_type import EventType
-from nvflare.apis.fl_component import FLComponent
 from nvflare.apis.fl_context import FLContext
-from nvflare.fuel.data_event.data_bus import DataBus
-from nvflare.metrics.metrics_keys import MetricKeys, MetricTypes
-from nvflare.metrics.metrics_publisher import collect_metrics
+from nvflare.metrics.metrics_collector import MetricsCollector
 
 
-class JobMetricsCollector(FLComponent):
+class JobMetricsCollector(MetricsCollector):
 
     def __init__(self, tags: dict, streaming_to_server: bool = False):
         """
@@ -31,181 +27,212 @@ class JobMetricsCollector(FLComponent):
             tags: comma separated static tags. used to specify server, client, production, test etc.
             streaming_to_server: boolean to specify if metrics should be streamed to server
         """
-        super().__init__()
-        self.tags: dict = tags
-        self.data_bus = DataBus()
-        self.streaming_to_server = streaming_to_server
+        super().__init__(tags = tags, streaming_to_server = streaming_to_server)
+        
+        self.single_events = [
+            EventType.SUBMIT_JOB,
+            EventType.DEPLOY_JOB_TO_SERVER,
+            EventType.DEPLOY_JOB_TO_CLIENT,
+            EventType.BEFORE_CHECK_RESOURCE_MANAGER,
+            EventType.BEFORE_SEND_ADMIN_COMMAND
+        ]
+    
+        self.pair_events = {
+            EventType.START_WORKFLOW: "_workflow",
+            EventType.END_WORKFLOW: "_workflow",
 
-        # job events
-        self.job_start_workflow = 0
-        self.job_started = 0
+            EventType.JOB_STARTED: "_job",
+            EventType.JOB_COMPLETED: "_job",
+            EventType.JOB_ABORTED: "_job",
+            EventType.JOB_CANCELLED: "_job",
 
-        # job task events
-        self.before_pull_task = 0
-        self.before_process_task_request = 0
-        self.before_process_submission = 0
-        self.before_task_data_filter = 0
-        self.before_task_result_filter = 0
-        self.before_task_execution = 0
-        self.before_send_task_result = 0
-        self.before_process_result_of_unknown_task = 0
+            EventType.BEFORE_PULL_TASK: "_pull_task",
+            EventType.AFTER_PULL_TASK: "_pull_task",
+            EventType.BEFORE_PROCESS_TASK_REQUEST: "_process_task",
+
+            EventType.BEFORE_PROCESS_SUBMISSION: "_process_submission",
+            EventType.AFTER_PROCESS_SUBMISSION: "_process_submission",
+
+            EventType.BEFORE_TASK_DATA_FILTER: "_data_filter",
+            EventType.AFTER_TASK_DATA_FILTER: "_data_filter",
+
+            EventType.BEFORE_TASK_RESULT_FILTER: "_result_filter",
+            EventType.AFTER_TASK_RESULT_FILTER: "_result_filter",
+
+            EventType.BEFORE_TASK_EXECUTION: "_task_execution",
+            EventType.AFTER_TASK_EXECUTION: "_task_execution",
+            EventType.ABORT_TASK: "_task_execution",
+
+            EventType.BEFORE_SEND_TASK_RESULT: "_send_task_result",
+            EventType.AFTER_SEND_TASK_RESULT: "_send_task_result",
+
+            EventType.BEFORE_PROCESS_RESULT_OF_UNKNOWN_TASK: "_process_result_of_unknown_task",
+            EventType.AFTER_PROCESS_RESULT_OF_UNKNOWN_TASK: "_process_result_of_unknown_task"
+        }
+
 
     def handle_event(self, event: str, fl_ctx: FLContext):
-
-        current_time = time.time()
+        print("job event = ", event)
         job_id = fl_ctx.get_job_id()
-        metric_name = event
-
         tags: dict = self.tags
         tags["job_id"] = job_id
+        super().collect_event_metrics(event=event, tags = tags, fl_ctx=fl_ctx)
 
-        metrics = {MetricKeys.count: 1, MetricKeys.type: MetricTypes.COUNTER}
-        duration_metrics = {MetricKeys.time_taken: 0, MetricKeys.type: MetricTypes.GAUGE}
 
-        if event == EventType.START_WORKFLOW:
-            self.publish_metrics(metrics, metric_name, tags, fl_ctx)
-            self.job_start_workflow = current_time
+        # metrics = {MetricKeys.count: 1, MetricKeys.type: MetricTypes.COUNTER}
+        # duration_metrics = {MetricKeys.time_taken: 0, MetricKeys.type: MetricTypes.GAUGE}
 
-        elif event == EventType.END_WORKFLOW:
-            self.publish_metrics(metrics, metric_name, tags, fl_ctx)
+        # if event == EventType.START_WORKFLOW:
+        #     self.publish_metrics(metrics, metric_name, tags, fl_ctx)
+        #     self.job_start_workflow = current_time
 
-            time_taken = current_time - self.job_start_workflow
-            duration_metrics[MetricKeys.time_taken] = time_taken
-            metric_name = "_workflow"
-            self.publish_metrics(duration_metrics, metric_name, tags, fl_ctx)
+        # elif event == EventType.END_WORKFLOW:
+        #     self.publish_metrics(metrics, metric_name, tags, fl_ctx)
 
-        elif event == EventType.JOB_STARTED:
-            self.publish_metrics(metrics, metric_name, tags, fl_ctx)
-            self.job_started = current_time
+        #     time_taken = current_time - self.job_start_workflow
+        #     duration_metrics[MetricKeys.time_taken] = time_taken
+        #     metric_name = "_workflow"
+        #     self.publish_metrics(duration_metrics, metric_name, tags, fl_ctx)
 
-        elif event == EventType.JOB_COMPLETED or event == EventType.JOB_ABORTED or event == EventType.JOB_CANCELLED:
-            self.publish_metrics(metrics, metric_name, tags, fl_ctx)
+        # elif event == EventType.JOB_STARTED:
+        #     self.publish_metrics(metrics, metric_name, tags, fl_ctx)
+        #     self.job_started = current_time
 
-            time_taken = current_time - self.job_started
-            duration_metrics[MetricKeys.time_taken] = time_taken
-            metric_name = "_job"
-            self.publish_metrics(duration_metrics, metric_name, tags, fl_ctx)
+        # elif event == EventType.JOB_COMPLETED or event == EventType.JOB_ABORTED or event == EventType.JOB_CANCELLED:
+        #     self.publish_metrics(metrics, metric_name, tags, fl_ctx)
 
-        elif event == EventType.BEFORE_PULL_TASK:
-            self.publish_metrics(metrics, metric_name, tags, fl_ctx)
-            self.before_pull_task = current_time
+        #     time_taken = current_time - self.job_started
+        #     duration_metrics[MetricKeys.time_taken] = time_taken
+        #     metric_name = "_job"
+        #     self.publish_metrics(duration_metrics, metric_name, tags, fl_ctx)
 
-        elif event == EventType.AFTER_PULL_TASK:
-            self.publish_metrics(metrics, metric_name, tags, fl_ctx)
+        # elif event == EventType.BEFORE_PULL_TASK:
+        #     self.publish_metrics(metrics, metric_name, tags, fl_ctx)
+        #     self.before_pull_task = current_time
 
-            time_taken = current_time - self.before_pull_task
-            duration_metrics[MetricKeys.time_taken] = time_taken
-            metric_name = "_pull_task"
-            self.publish_metrics(duration_metrics, metric_name, tags, fl_ctx)
+        # elif event == EventType.AFTER_PULL_TASK:
+        #     self.publish_metrics(metrics, metric_name, tags, fl_ctx)
 
-        elif event == EventType.BEFORE_PROCESS_TASK_REQUEST:
-            self.publish_metrics(metrics, metric_name, tags, fl_ctx)
-            self.before_process_task_request = current_time
+        #     time_taken = current_time - self.before_pull_task
+        #     duration_metrics[MetricKeys.time_taken] = time_taken
+        #     metric_name = "_pull_task"
+        #     self.publish_metrics(duration_metrics, metric_name, tags, fl_ctx)
 
-        elif event == EventType.AFTER_PROCESS_TASK_REQUEST:
-            self.publish_metrics(metrics, metric_name, tags, fl_ctx)
+        # elif event == EventType.BEFORE_PROCESS_TASK_REQUEST:
+        #     self.publish_metrics(metrics, metric_name, tags, fl_ctx)
+        #     self.before_process_task_request = current_time
 
-            time_taken = current_time - self.before_process_task_request
-            duration_metrics[MetricKeys.time_taken] = time_taken
-            metric_name = "_process_task"
-            self.publish_metrics(duration_metrics, metric_name, tags, fl_ctx)
+        # elif event == EventType.AFTER_PROCESS_TASK_REQUEST:
+        #     self.publish_metrics(metrics, metric_name, tags, fl_ctx)
 
-        elif event == EventType.BEFORE_PROCESS_SUBMISSION:
-            self.publish_metrics(metrics, metric_name, tags, fl_ctx)
-            self.before_process_submission = current_time
+        #     time_taken = current_time - self.before_process_task_request
+        #     duration_metrics[MetricKeys.time_taken] = time_taken
+        #     metric_name = "_process_task"
+        #     self.publish_metrics(duration_metrics, metric_name, tags, fl_ctx)
 
-        elif event == EventType.AFTER_PROCESS_SUBMISSION:
-            self.publish_metrics(metrics, metric_name, tags, fl_ctx)
+        # elif event == EventType.BEFORE_PROCESS_SUBMISSION:
+        #     self.publish_metrics(metrics, metric_name, tags, fl_ctx)
+        #     self.before_process_submission = current_time
 
-            time_taken = current_time - self.before_process_submission
-            duration_metrics[MetricKeys.time_taken] = time_taken
-            metric_name = "_process_submission"
-            self.publish_metrics(duration_metrics, metric_name, tags, fl_ctx)
+        # elif event == EventType.AFTER_PROCESS_SUBMISSION:
+        #     self.publish_metrics(metrics, metric_name, tags, fl_ctx)
 
-        elif event == EventType.BEFORE_TASK_DATA_FILTER:
-            self.publish_metrics(metrics, metric_name, tags, fl_ctx)
-            self.before_task_data_filter = current_time
+        #     time_taken = current_time - self.before_process_submission
+        #     duration_metrics[MetricKeys.time_taken] = time_taken
+        #     metric_name = "_process_submission"
+        #     self.publish_metrics(duration_metrics, metric_name, tags, fl_ctx)
 
-        elif event == EventType.AFTER_TASK_DATA_FILTER:
-            self.publish_metrics(metrics, metric_name, tags, fl_ctx)
+        # elif event == EventType.BEFORE_TASK_DATA_FILTER:
+        #     self.publish_metrics(metrics, metric_name, tags, fl_ctx)
+        #     self.before_task_data_filter = current_time
 
-            time_taken = current_time - self.before_task_data_filter
-            duration_metrics[MetricKeys.time_taken] = time_taken
-            metric_name = "_data_filter"
-            self.publish_metrics(duration_metrics, metric_name, tags, fl_ctx)
+        # elif event == EventType.AFTER_TASK_DATA_FILTER:
+        #     self.publish_metrics(metrics, metric_name, tags, fl_ctx)
 
-        elif event == EventType.BEFORE_TASK_RESULT_FILTER:
-            self.publish_metrics(metrics, metric_name, tags, fl_ctx)
-            self.before_task_result_filter = current_time
+        #     time_taken = current_time - self.before_task_data_filter
+        #     duration_metrics[MetricKeys.time_taken] = time_taken
+        #     metric_name = "_data_filter"
+        #     self.publish_metrics(duration_metrics, metric_name, tags, fl_ctx)
 
-        elif event == EventType.AFTER_TASK_RESULT_FILTER:
-            self.publish_metrics(metrics, metric_name, tags, fl_ctx)
+        # elif event == EventType.BEFORE_TASK_RESULT_FILTER:
+        #     self.publish_metrics(metrics, metric_name, tags, fl_ctx)
+        #     self.before_task_result_filter = current_time
 
-            time_taken = current_time - self.before_task_result_filter
-            duration_metrics[MetricKeys.time_taken] = time_taken
-            metric_name = "_result_filter"
-            self.publish_metrics(duration_metrics, metric_name, tags, fl_ctx)
+        # elif event == EventType.AFTER_TASK_RESULT_FILTER:
+        #     self.publish_metrics(metrics, metric_name, tags, fl_ctx)
 
-        elif event == EventType.BEFORE_TASK_EXECUTION:
-            self.publish_metrics(metrics, metric_name, tags, fl_ctx)
-            self.before_task_execution = current_time
+        #     time_taken = current_time - self.before_task_result_filter
+        #     duration_metrics[MetricKeys.time_taken] = time_taken
+        #     metric_name = "_result_filter"
+        #     self.publish_metrics(duration_metrics, metric_name, tags, fl_ctx)
 
-        elif event == EventType.AFTER_TASK_EXECUTION:
+        # elif event == EventType.BEFORE_TASK_EXECUTION:
+        #     self.publish_metrics(metrics, metric_name, tags, fl_ctx)
+        #     self.before_task_execution = current_time
 
-            time_taken = current_time - self.before_task_execution
-            duration_metrics[MetricKeys.time_taken] = time_taken
-            metric_name = "_task_execution"
-            self.publish_metrics(duration_metrics, metric_name, tags, fl_ctx)
+        # elif event == EventType.AFTER_TASK_EXECUTION:
 
-        elif event == EventType.ABORT_TASK:
-            self.publish_metrics(metrics, metric_name, tags, fl_ctx)
+        #     time_taken = current_time - self.before_task_execution
+        #     duration_metrics[MetricKeys.time_taken] = time_taken
+        #     metric_name = "_task_execution"
+        #     self.publish_metrics(duration_metrics, metric_name, tags, fl_ctx)
 
-            time_taken = current_time - self.before_task_execution
-            duration_metrics[MetricKeys.time_taken] = time_taken
-            metric_name = "_before_abort"
-            self.publish_metrics(duration_metrics, metric_name, tags, fl_ctx)
+        # elif event == EventType.ABORT_TASK:
+        #     self.publish_metrics(metrics, metric_name, tags, fl_ctx)
 
-        elif event == EventType.BEFORE_SEND_TASK_RESULT:
-            self.publish_metrics(metrics, metric_name, tags, fl_ctx)
-            self.before_send_task_result = current_time
+        #     time_taken = current_time - self.before_task_execution
+        #     duration_metrics[MetricKeys.time_taken] = time_taken
+        #     metric_name = "_before_abort"
+        #     self.publish_metrics(duration_metrics, metric_name, tags, fl_ctx)
 
-        elif event == EventType.AFTER_SEND_TASK_RESULT:
-            self.publish_metrics(metrics, metric_name, tags, fl_ctx)
+        # elif event == EventType.BEFORE_SEND_TASK_RESULT:
+        #     self.publish_metrics(metrics, metric_name, tags, fl_ctx)
+        #     self.before_send_task_result = current_time
 
-            time_taken = current_time - self.before_send_task_result
-            duration_metrics[MetricKeys.time_taken] = time_taken
-            metric_name = "_send_task_result"
-            self.publish_metrics(duration_metrics, metric_name, tags, fl_ctx)
+        # elif event == EventType.AFTER_SEND_TASK_RESULT:
+        #     self.publish_metrics(metrics, metric_name, tags, fl_ctx)
 
-        elif event == EventType.BEFORE_PROCESS_RESULT_OF_UNKNOWN_TASK:
-            self.publish_metrics(metrics, metric_name, tags, fl_ctx)
-            self.before_process_result_of_unknown_task = current_time
+        #     time_taken = current_time - self.before_send_task_result
+        #     duration_metrics[MetricKeys.time_taken] = time_taken
+        #     metric_name = "_send_task_result"
+        #     self.publish_metrics(duration_metrics, metric_name, tags, fl_ctx)
 
-        elif event == EventType.AFTER_PROCESS_RESULT_OF_UNKNOWN_TASK:
-            self.publish_metrics(metrics, metric_name, tags, fl_ctx)
+        # elif event == EventType.BEFORE_PROCESS_RESULT_OF_UNKNOWN_TASK:
+        #     self.publish_metrics(metrics, metric_name, tags, fl_ctx)
+        #     self.before_process_result_of_unknown_task = current_time
 
-            time_taken = current_time - self.before_process_result_of_unknown_task
-            duration_metrics[MetricKeys.time_taken] = time_taken
-            metric_name = "_process_result_of_unknown_task"
-            self.publish_metrics(duration_metrics, metric_name, tags, fl_ctx)
+        # elif event == EventType.AFTER_PROCESS_RESULT_OF_UNKNOWN_TASK:
+        #     self.publish_metrics(metrics, metric_name, tags, fl_ctx)
 
-        elif event == EventType.SUBMIT_JOB:
-            self.publish_metrics(metrics, metric_name, tags, fl_ctx)
+        #     time_taken = current_time - self.before_process_result_of_unknown_task
+        #     duration_metrics[MetricKeys.time_taken] = time_taken
+        #     metric_name = "_process_result_of_unknown_task"
+        #     self.publish_metrics(duration_metrics, metric_name, tags, fl_ctx)
 
-        elif event == EventType.DEPLOY_JOB_TO_SERVER:
-            self.publish_metrics(metrics, metric_name, tags, fl_ctx)
+        # elif event == EventType.SUBMIT_JOB:
+        #     self.publish_metrics(metrics, metric_name, tags, fl_ctx)
 
-        elif event == EventType.DEPLOY_JOB_TO_CLIENT:
-            self.publish_metrics(metrics, metric_name, tags, fl_ctx)
+        # elif event == EventType.DEPLOY_JOB_TO_SERVER:
+        #     self.publish_metrics(metrics, metric_name, tags, fl_ctx)
 
-        elif event == EventType.BEFORE_CHECK_RESOURCE_MANAGER:
-            self.publish_metrics(metrics, metric_name, tags, fl_ctx)
+        # elif event == EventType.DEPLOY_JOB_TO_CLIENT:
+        #     self.publish_metrics(metrics, metric_name, tags, fl_ctx)
 
-        elif event == EventType.BEFORE_SEND_ADMIN_COMMAND:
-            self.publish_metrics(metrics, metric_name, tags, fl_ctx)
+        # elif event == EventType.BEFORE_CHECK_RESOURCE_MANAGER:
+        #     self.publish_metrics(metrics, metric_name, tags, fl_ctx)
 
-        else:
-            pass
+        # elif event == EventType.BEFORE_SEND_ADMIN_COMMAND:
+        #     self.publish_metrics(metrics, metric_name, tags, fl_ctx)
 
-    def publish_metrics(self, metrics: dict, metric_name: str, tags: dict, fl_ctx: FLContext):
-        collect_metrics(self, self.streaming_to_server, metrics, metric_name, tags, self.data_bus, fl_ctx)
+        # else:
+        #     pass
+
+    # def publish_metrics(self, metrics: dict, metric_name: str, tags: dict, fl_ctx: FLContext):
+    #     collect_metrics(self, self.streaming_to_server, metrics, metric_name, tags, self.data_bus, fl_ctx)
+
+    def get_single_events(self):
+        return self.single_events
+
+    def get_pair_events(self):
+        return self.pair_events
+ 
--- nvflare/metrics/system_metrics_collector.py
+++ nvflare/metrics/system_metrics_collector.py
@@ -13,136 +13,61 @@
 # limitations under the License.
 
 
-import time
 
 from nvflare.apis.event_type import EventType
-from nvflare.apis.fl_component import FLComponent
 from nvflare.apis.fl_context import FLContext
-from nvflare.fuel.data_event.data_bus import DataBus
-from nvflare.metrics.metrics_keys import MetricKeys, MetricTypes
-from nvflare.metrics.metrics_publisher import collect_metrics
+from nvflare.metrics.metrics_collector import MetricsCollector
 
 
-class SysMetricsCollector(FLComponent):
+class SysMetricsCollector(MetricsCollector):
 
     def __init__(self, tags: dict, streaming_to_server: bool = False):
         """
         Args:
             tags: comma separated static tags. used to specify server, client, production, test etc.
         """
-        super().__init__()
+        super().__init__(tags=tags, streaming_to_server= streaming_to_server)
+        
 
-        self.tags = tags
-        self.data_bus = DataBus()
-        self.streaming_to_server = streaming_to_server
+        self.pair_events = {
+            EventType.SYSTEM_START: "_system",
+            EventType.SYSTEM_END: "_system",
+            
+            EventType.START_RUN: "_run",
+            EventType.END_RUN: "_run",
+            
+            EventType.BEFORE_CHECK_CLIENT_RESOURCES: "_check_client_resources",
+            EventType.AFTER_CHECK_CLIENT_RESOURCES: "_check_client_resources",
+            
+            EventType.BEFORE_CLIENT_REGISTER: "_client_register",
+            EventType.AFTER_CLIENT_REGISTER: "_client_register",
 
-        # system events
-        self.system_start = 0
-        self.system_end = 0
-        self.start_run = 0
-        self.end_run = 0
+            EventType.BEFORE_CLIENT_HEARTBEAT: "_client_heartbeat",
+            EventType.AFTER_CLIENT_HEARTBEAT: "_client_heartbeat",
+        }
 
-        # server-site only events
-        self.before_client_heartbeat = 0
-        self.before_client_register = 0
-        self.client_disconnected = 0
-        self.client_reconnected = 0
-        self.before_check_client_resources = 0
-        self.after_check_client_resources = 0
-        self.start = True
-
+   
+        self.single_events = [
+                EventType.CLIENT_DISCONNECTED,
+                EventType.CLIENT_RECONNECTED,
+                EventType.BEFORE_CHECK_RESOURCE_MANAGER, 
+                EventType.BEFORE_SEND_ADMIN_COMMAND,
+                EventType.CLIENT_REGISTER_RECEIVED, 
+                EventType.CLIENT_REGISTER_PROCESSED,
+                EventType.CLIENT_QUIT, 
+                EventType.SYSTEM_BOOTSTRAP,
+                EventType.CLIENT_HEARTBEAT_RECEIVED,
+                EventType.CLIENT_HEARTBEAT_PROCESSED,
+                EventType.BEFORE_JOB_LAUNCH
+        ]
+ 
     def handle_event(self, event: str, fl_ctx: FLContext):
+        super().collect_event_metrics(event=event, tags=self.tags, fl_ctx=fl_ctx)
 
-        current_time = time.time()
-        metric_name = event
 
-        metrics = {MetricKeys.count: 1, MetricKeys.type: MetricTypes.COUNTER}
-        duration_metrics = {MetricKeys.time_taken: 0, MetricKeys.type: MetricTypes.GAUGE}
+    def get_single_events(self):
+        return self.single_events
 
-        if event == EventType.SYSTEM_START:
-            self.publish_metrics(metrics, metric_name, self.tags, fl_ctx)
-            self.system_start = current_time
-
-        elif event == EventType.SYSTEM_END:
-            self.publish_metrics(metrics, metric_name, self.tags, fl_ctx)
-
-            time_taken = current_time - self.system_start
-            duration_metrics[MetricKeys.time_taken] = time_taken
-            metric_name = "_system"
-            self.publish_metrics(duration_metrics, metric_name, self.tags, fl_ctx)
-
-        elif event == EventType.START_RUN:
-            self.publish_metrics(metrics, metric_name, self.tags, fl_ctx)
-            self.start_run = current_time
-
-        elif event == EventType.END_RUN:
-            self.publish_metrics(metrics, metric_name, self.tags, fl_ctx)
-
-            time_taken = current_time - self.start_run
-            duration_metrics[MetricKeys.time_taken] = time_taken
-            metric_name = "_run"
-            self.publish_metrics(duration_metrics, metric_name, self.tags, fl_ctx)
-
-        elif event == EventType.BEFORE_CHECK_CLIENT_RESOURCES:
-            self.publish_metrics(metrics, metric_name, self.tags, fl_ctx)
-            self.before_check_client_resources = current_time
-
-        elif event == EventType.AFTER_CHECK_CLIENT_RESOURCES:
-            self.publish_metrics(metrics, metric_name, self.tags, fl_ctx)
-
-            time_taken = current_time - self.before_check_client_resources
-            duration_metrics[MetricKeys.time_taken] = time_taken
-            metric_name = "_check_client_resources"
-            self.publish_metrics(duration_metrics, metric_name, self.tags, fl_ctx)
-        elif event == EventType.CLIENT_DISCONNECTED:
-            self.publish_metrics(metrics, metric_name, self.tags, fl_ctx)
-        elif event == EventType.CLIENT_RECONNECTED:
-            self.publish_metrics(metrics, metric_name, self.tags, fl_ctx)
-        elif event == EventType.BEFORE_CHECK_RESOURCE_MANAGER:
-            self.publish_metrics(metrics, metric_name, self.tags, fl_ctx)
-        elif event == EventType.BEFORE_SEND_ADMIN_COMMAND:
-            self.publish_metrics(metrics, metric_name, self.tags, fl_ctx)
-        elif event == EventType.BEFORE_CLIENT_REGISTER:
-            self.publish_metrics(metrics, metric_name, self.tags, fl_ctx)
-            self.before_client_register = current_time
-        elif event == EventType.AFTER_CLIENT_REGISTER:
-            self.publish_metrics(metrics, metric_name, self.tags, fl_ctx)
-
-            time_taken = current_time - self.before_client_register
-            duration_metrics[MetricKeys.time_taken] = time_taken
-            metric_name = "_client_register"
-            self.publish_metrics(duration_metrics, metric_name, self.tags, fl_ctx)
-
-        elif event == EventType.CLIENT_REGISTER_RECEIVED:
-            self.publish_metrics(metrics, metric_name, self.tags, fl_ctx)
-
-        elif event == EventType.CLIENT_REGISTER_PROCESSED:
-            self.publish_metrics(metrics, metric_name, self.tags, fl_ctx)
-
-        elif event == EventType.CLIENT_QUIT:
-
-            self.publish_metrics(metrics, metric_name, self.tags, fl_ctx)
-        elif event == EventType.SYSTEM_BOOTSTRAP:
-            self.publish_metrics(metrics, metric_name, self.tags, fl_ctx)
-        elif event == EventType.BEFORE_CLIENT_HEARTBEAT:
-            self.publish_metrics(metrics, metric_name, self.tags, fl_ctx)
-            self.before_client_heartbeat = current_time
-        elif event == EventType.AFTER_CLIENT_HEARTBEAT:
-            self.publish_metrics(metrics, metric_name, self.tags, fl_ctx)
-
-            time_taken = current_time - self.before_client_heartbeat
-            duration_metrics[MetricKeys.time_taken] = time_taken
-            metric_name = "_client_heartbeat"
-            self.publish_metrics(duration_metrics, metric_name, self.tags, fl_ctx)
-        elif event == EventType.CLIENT_HEARTBEAT_RECEIVED:
-            self.publish_metrics(metrics, metric_name, self.tags, fl_ctx)
-        elif event == EventType.CLIENT_HEARTBEAT_PROCESSED:
-            self.publish_metrics(metrics, metric_name, self.tags, fl_ctx)
-        elif event == EventType.BEFORE_JOB_LAUNCH:
-            self.publish_metrics(metrics, metric_name, self.tags, fl_ctx)
-        else:
-            pass
-
-    def publish_metrics(self, metrics: dict, metric_name: str, tags: dict, fl_ctx: FLContext):
-
-        collect_metrics(self, self.streaming_to_server, metrics, metric_name, tags, self.data_bus, fl_ctx)
+    def get_pair_events(self):
+        return self.pair_events
+ 
