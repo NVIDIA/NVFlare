00ec1c78e1c223c17d522ea8fde3a5d99203b53a
Francesco Farina <farina.francesco1991@gmail.com>
2025-02-07 13:21:26 +0000

    add license and format

--- examples/tutorials/self-paced-training/part-4_advanced_federated_learning/chapter-9_flare_low_level_apis/09.2_tasks_and_data_share/modules.py
+++ examples/tutorials/self-paced-training/part-4_advanced_federated_learning/chapter-9_flare_low_level_apis/09.2_tasks_and_data_share/modules.py
@@ -1,26 +1,40 @@
-from nvflare.apis.fl_context import FLContext
-from nvflare.apis.impl.controller import Controller
-from nvflare.apis.signal import Signal
+# Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 from nvflare.apis.controller_spec import Task
 from nvflare.apis.dxo import DXO, DataKind, from_shareable
 from nvflare.apis.executor import Executor
-from nvflare.apis.shareable import Shareable, make_reply
 from nvflare.apis.fl_constant import ReturnCode
+from nvflare.apis.fl_context import FLContext
+from nvflare.apis.impl.controller import Controller
+from nvflare.apis.shareable import Shareable, make_reply
+from nvflare.apis.signal import Signal
+
 
 class HelloController(Controller):
 
-    def control_flow(self, abort_signal: Signal, fl_ctx: FLContext):        
+    def control_flow(self, abort_signal: Signal, fl_ctx: FLContext):
         # Create the task with name "hello"
         task = Task(name="hello", data=Shareable())
 
         # Broadcast the task to all clients and wait for all to respond
         self.broadcast_and_wait(
             task=task,
-            targets=None, # meaning all clients, determined dynamically
-            min_responses=0, # meaning all clients
+            targets=None,  # meaning all clients, determined dynamically
+            min_responses=0,  # meaning all clients
             fl_ctx=fl_ctx,
         )
-    
+
     def start_controller(self, fl_ctx: FLContext):
         self.log_info(fl_ctx, "Starting the controller...")
 
@@ -40,10 +54,11 @@ class HelloExecutor(Executor):
         if task_name == "hello":
             self.log_info(fl_ctx, f"Received task with name {task_name} and data {shareable}")
             return make_reply(ReturnCode.OK)
-        
+
+
 class HelloDataController(Controller):
 
-    def control_flow(self, abort_signal: Signal, fl_ctx: FLContext):        
+    def control_flow(self, abort_signal: Signal, fl_ctx: FLContext):
         # Prepare any extra parameters to send to the clients
         data = DXO(
             data_kind=DataKind.APP_DEFINED,
@@ -56,17 +71,17 @@ class HelloDataController(Controller):
         # Broadcast the task to all clients and wait for all to respond
         self.broadcast_and_wait(
             task=task,
-            targets=None, # meaning all clients
+            targets=None,  # meaning all clients
             min_responses=0,
             fl_ctx=fl_ctx,
         )
-    
+
     def start_controller(self, fl_ctx: FLContext):
         self.log_info(fl_ctx, "Starting the controller...")
 
     def stop_controller(self, fl_ctx: FLContext):
         self.log_info(fl_ctx, "Stopping the controller...")
-        
+
 
 class HelloDataExecutor(Executor):
 
@@ -83,9 +98,10 @@ class HelloDataExecutor(Executor):
             self.log_info(fl_ctx, f"Received message from server: {message}")
             return make_reply(ReturnCode.OK)
 
+
 class HelloResponseController(Controller):
 
-    def control_flow(self, abort_signal: Signal, fl_ctx: FLContext):        
+    def control_flow(self, abort_signal: Signal, fl_ctx: FLContext):
         # Prepare any extra parameters to send to the clients
         dxo = DXO(
             data_kind=DataKind.APP_DEFINED,
@@ -99,11 +115,11 @@ class HelloResponseController(Controller):
         # Broadcast the task to all clients and wait for all to respond
         self.broadcast_and_wait(
             task=task,
-            targets=None, # meaning all clients
+            targets=None,  # meaning all clients
             min_responses=0,
             fl_ctx=fl_ctx,
         )
-    
+
     def start_controller(self, fl_ctx: FLContext):
         self.log_info(fl_ctx, "Starting the controller...")
 
@@ -117,6 +133,7 @@ class HelloResponseController(Controller):
         received_msg = from_shareable(response).data["message"]
 
         self.log_info(fl_ctx, f"Received message {received_msg} from client {client.name} for task {task.name}")
+
 
 class HelloResponseExecutor(Executor):
 
--- examples/tutorials/self-paced-training/part-4_advanced_federated_learning/chapter-9_flare_low_level_apis/09.3_p2p_communication/modules.py
+++ examples/tutorials/self-paced-training/part-4_advanced_federated_learning/chapter-9_flare_low_level_apis/09.3_p2p_communication/modules.py
@@ -1,30 +1,43 @@
+# Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 from nvflare.apis.controller_spec import Task
 from nvflare.apis.dxo import DXO, DataKind, from_shareable
+from nvflare.apis.event_type import EventType
 from nvflare.apis.executor import Executor
 from nvflare.apis.fl_constant import ReturnCode
 from nvflare.apis.fl_context import FLContext
 from nvflare.apis.impl.controller import Controller
 from nvflare.apis.shareable import Shareable, make_reply
 from nvflare.apis.signal import Signal
-from nvflare.apis.event_type import EventType
 
 
 class BasicController(Controller):
 
-    def control_flow(self, abort_signal: Signal, fl_ctx: FLContext):        
+    def control_flow(self, abort_signal: Signal, fl_ctx: FLContext):
         self.broadcast_and_wait(
             task=Task(name="talk", data=Shareable()),
             targets=None,
             min_responses=0,
             fl_ctx=fl_ctx,
         )
-    
+
     def start_controller(self, fl_ctx: FLContext):
         self.log_info(fl_ctx, "Starting the controller...")
 
     def stop_controller(self, fl_ctx: FLContext):
         self.log_info(fl_ctx, "Stopping the controller...")
-    
+
 
 class P2PExecutor(Executor):
 
@@ -61,7 +74,7 @@ class P2PExecutor(Executor):
             engine.register_aux_message_handler(topic="hello", message_handle_func=self._handle_aux_request)
 
     def _handle_aux_request(self, topic: str, request: Shareable, fl_ctx: FLContext) -> Shareable:
-        sender = request.get_peer_props()["__identity_name__"] # extract sender name
+        sender = request.get_peer_props()["__identity_name__"]  # extract sender name
         received_message = from_shareable(request).data["message"]
 
         # log received message
