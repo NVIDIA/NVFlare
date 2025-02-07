d092db7b6bc24e7c5977e6fa80b58ef99100611a
Francesco Farina <farina.francesco1991@gmail.com>
2025-02-07 10:29:14 +0000

    p2p communication notebook

---
+++ examples/tutorials/self-paced-training/part-4_advanced_federated_learning/chapter-9_flare_low_level_apis/09.3_p2p_communication/modules.py
@@ -0,0 +1,70 @@
+from nvflare.apis.controller_spec import Task
+from nvflare.apis.dxo import DXO, DataKind, from_shareable
+from nvflare.apis.executor import Executor
+from nvflare.apis.fl_constant import ReturnCode
+from nvflare.apis.fl_context import FLContext
+from nvflare.apis.impl.controller import Controller
+from nvflare.apis.shareable import Shareable, make_reply
+from nvflare.apis.signal import Signal
+from nvflare.apis.event_type import EventType
+
+
+class BasicController(Controller):
+
+    def control_flow(self, abort_signal: Signal, fl_ctx: FLContext):        
+        self.broadcast_and_wait(
+            task=Task(name="talk", data=Shareable()),
+            targets=None,
+            min_responses=0,
+            fl_ctx=fl_ctx,
+        )
+    
+    def start_controller(self, fl_ctx: FLContext):
+        self.log_info(fl_ctx, "Starting the controller...")
+
+    def stop_controller(self, fl_ctx: FLContext):
+        self.log_info(fl_ctx, "Stopping the controller...")
+    
+
+class P2PExecutor(Executor):
+
+    def execute(
+        self,
+        task_name: str,
+        shareable: Shareable,
+        fl_ctx: FLContext,
+        abort_signal: Signal,
+    ):
+        if task_name == "talk":
+            engine = fl_ctx.get_engine()
+            identity_name = fl_ctx.get_identity_name()
+
+            engine.send_aux_request(
+                targets=[f"site-{i}" for i in range(3) if f"site-{i}" != identity_name],
+                topic="hello",
+                request=DXO(
+                    data_kind=DataKind.APP_DEFINED,
+                    data={
+                        "message": f"Hello from {identity_name}",
+                    },
+                ).to_shareable(),
+                timeout=0,
+                fl_ctx=fl_ctx,
+            )
+            return make_reply(ReturnCode.OK)
+
+    def handle_event(self, event_type: str, fl_ctx: FLContext):
+        if event_type == EventType.START_RUN:
+            engine = fl_ctx.get_engine()
+
+            # Register the aux message handler
+            engine.register_aux_message_handler(topic="hello", message_handle_func=self._handle_aux_request)
+
+    def _handle_aux_request(self, topic: str, request: Shareable, fl_ctx: FLContext) -> Shareable:
+        sender = request.get_peer_props()["__identity_name__"] # extract sender name
+        received_message = from_shareable(request).data["message"]
+
+        # log received message
+        self.log_info(fl_ctx, f"Received message from {sender}: {received_message}")
+
+        return make_reply(ReturnCode.OK)
