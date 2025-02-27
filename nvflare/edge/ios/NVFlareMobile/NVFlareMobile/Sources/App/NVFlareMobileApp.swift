a3f779cec126dc26c64f4bd524807c9da6bac3ac
Yuan-Ting Hsieh <yuantingh@yuantingh-mlt.client.nvidia.com>
2025-02-27 15:24:04 -0800

    Add demo ios app

---
+++ nvflare/edge/ios/NVFlareMobile/NVFlareMobile/Sources/App/NVFlareMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  NVFlareMobileApp.swift
+//  NVFlareMobile
+//
+//  Created by Yuan-Ting Hsieh on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct NVFlareMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
