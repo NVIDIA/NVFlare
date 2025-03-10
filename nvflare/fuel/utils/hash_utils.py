99959b500baa4f486b1c8a3e885a195cb72d3695
Zhihong Zhang <zhihongz@nvidia.com>
2025-03-10 11:46:37 -0400

    Added docstring

--- nvflare/fuel/utils/hash_utils.py
+++ nvflare/fuel/utils/hash_utils.py
@@ -22,31 +22,59 @@ MAX_NUM_BUCKETS = 64 * 1024
 
 class UniformHash:
     """A hash algorithm with uniform distribution. It achieves this with following steps,
-    1. Get a hash value using SHA256
-    2. Map the hash value to a virtual hash table
-    3. Map the virtual bucket to real bucket
+    1. Get a hash value of the key using first 8 bytes of SHA256
+    2. Map the hash value to a virtual hash table using modulo operation
+    3. Map the virtual bucket to real bucket by even distribution
+
+    The virtual hash is needed because real hash table size may not be a prime number and
+    there are known issues of modulo operation.
 
     """
 
     def __init__(self, num_buckets: int):
+        """
+        Initialize the hash function
+        Args:
+            num_buckets: Number of buckets, e.g. Number of servers to distribute the load
+        """
         check_number_range("num_buckets", num_buckets, 1, MAX_NUM_BUCKETS)
         self.num_buckets = num_buckets
         self.virtual_hashes_per_bucket = PRIME // num_buckets
 
     def get_num_buckets(self) -> int:
+        """
+        Get the number of buckets
+        Returns:
+             Number of buckets
+        """
+
         return self.num_buckets
 
     def hash(self, key: str) -> int:
+        """
+        Hash the key to a bucket index
+        Args:
+            key: A string key to be hashed
+        Returns:
+            The bucket index between 0 and num_buckets-1
+        """
         check_str("key", key)
-        # The hash() function changes value every run so SHA256 is used
+
+        # Step 1, calculate hash value using first 8 bytes of SHA256
         sha_bytes = hashlib.sha256(key.encode()).digest()
         sha = int.from_bytes(sha_bytes[:8], "big")
+
+        # Step 2, map the hash value to a virtual hash table whose size is PRIME using modulo operation
         virtual_hash = sha % PRIME
 
+        # Step 3, evenly distribute the virtual hash to real buckets
+        # n is a float number representing the bucket index
         n = virtual_hash / self.virtual_hashes_per_bucket
         if n < self.num_buckets:
             index = int(n)
         else:
+            # The last bucket may have more virtual hashes than others
+            # Evenly spread the extra to the first few buckets
             index = virtual_hash % self.num_buckets
 
         return index
