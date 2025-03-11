f7503b5177db5b3d79dd5767aa89fa8fba7601e6
YuanTingHsieh <yuantingh@nvidia.com>
2025-03-10 21:00:28 -0700

    Update Readme

---
+++ examples/advanced/edge/setup_nvflare.sh
@@ -0,0 +1,17 @@
+PROJECT_NAME=edge_example
+
+# Check if project directory exists and remove it
+if [ -d "$PROJECT_NAME" ]; then
+    rm -rf "$PROJECT_NAME"
+fi
+
+python ../tree_prov.py --root_dir ./ -p $PROJECT_NAME -d 1 -w 2
+
+# Define leaf clients array
+leaf_clients=(C11 C12 C21 C22)
+
+# Loop through leaf clients and copy resources
+for client in "${leaf_clients[@]}"; do
+    cp edge__p_resources.json ./$PROJECT_NAME/prod_00/$client/local/
+done
+
