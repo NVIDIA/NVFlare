0019c6c46691f3479fd3ad657610f1fd71dc4e98
Zhihong Zhang <zhihongz@nvidia.com>
2025-03-26 14:55:39 -0400

    Added file_streaming example job

---
+++ examples/advanced/streaming/jobs/file_streaming/app/custom/file_streaming.py
@@ -0,0 +1,101 @@
+# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import os
+import time
+from threading import Thread
+
+from nvflare.apis.event_type import EventType
+from nvflare.apis.fl_component import FLComponent
+from nvflare.apis.fl_context import FLContext
+from nvflare.app_common.streamers.file_streamer import FileStreamer
+
+CHANNEL = "_test_channel"
+TOPIC = "_test_topic"
+TIMESTAMP = "_timestamp"
+
+
+class FileSender(FLComponent):
+
+    def __init__(self, file_name: str):
+        super().__init__()
+        self.seq = 0
+        self.aborted = False
+        self.file_name = file_name
+
+    def handle_event(self, event_type: str, fl_ctx: FLContext):
+        if event_type == EventType.START_RUN:
+            self.log_info(fl_ctx, "FileSender is started")
+            Thread(target=self._sending_file, args=(fl_ctx,), daemon=True).start()
+        elif event_type == EventType.ABORT_TASK:
+            self.log_info(fl_ctx, "Sender is aborted")
+            self.aborted = True
+
+    def _sending_file(self, fl_ctx):
+        try:
+            self.log_info(fl_ctx, "Sending file {self.file_name")
+            rc, result = FileStreamer.stream_file(
+                targets=["server"],
+                stream_ctx={TIMESTAMP: time.time()},
+                channel=CHANNEL,
+                topic=TOPIC,
+                file_name=self.file_name,
+                fl_ctx=fl_ctx,
+                optional=False,
+                secure=False,
+            )
+
+            self.log_info(fl_ctx, f"Sending finished with RC: {rc}")
+        except Exception as e:
+            self.log_error(fl_ctx, f"Error sending file: {e}")
+
+
+class FileReceiver(FLComponent):
+
+    def __init__(self, output_folder: str):
+        super().__init__()
+        self.output_folder = output_folder
+        self.done = False
+
+    def is_done(self):
+        return self.done
+
+    def handle_event(self, event_type: str, fl_ctx: FLContext):
+        if event_type == EventType.START_RUN:
+            self._receive_file(fl_ctx)
+            self.log_info(fl_ctx, "FileReceiver is started")
+
+    def _receive_file(self, fl_ctx):
+        FileStreamer.register_stream_processing(
+            fl_ctx=fl_ctx,
+            channel=CHANNEL,
+            topic=TOPIC,
+            dest_dir=self.output_folder,
+            stream_done_cb=self._done_cb,
+        )
+
+    def _done_cb(self, stream_ctx: dict, fl_ctx: FLContext):
+        self.log_info(fl_ctx, "File streaming is done")
+        self.done = True
+
+        file_name = FileStreamer.get_file_location(stream_ctx)
+        file_size = FileStreamer.get_file_size(stream_ctx)
+        size = os.path.getsize(file_name)
+
+        if size != file_size:
+            self.log_error(fl_ctx, f"File {file_name} sizes mismatch {size} <> {file_size} bytes")
+            return
+
+        start_time = stream_ctx.get(TIMESTAMP)
+        duration = time.time() - start_time
+        self.log_info(fl_ctx, f"File {file_name} with {size} bytes received in {duration:.3f} seconds")
