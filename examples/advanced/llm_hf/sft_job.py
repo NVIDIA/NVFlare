4c8aa2765e04521c4f3cf05c7e6858d41a98be19
Ziyue Xu <ziyuex@nvidia.com>
2025-04-29 16:46:46 -0400

    script fixes

--- examples/advanced/llm_hf/sft_job.py
+++ examples/advanced/llm_hf/sft_job.py
@@ -74,7 +74,10 @@ def main():
 
     # Define the model persistor and send to server
     # First send the model to the server
-    job.to("src/hf_sft_model.py", "server")
+    if train_mode.lower() == "sft":
+        job.to("src/hf_sft_model.py", "server")
+    elif train_mode.lower() == "peft":
+        job.to("src/hf_peft_model.py", "server")
     # Then send the model persistor to the server
     model_args = {"path": "src.hf_sft_model.CausalLMModel", "args": {"model_name_or_path": model_name_or_path}}
     job.to(PTFileModelPersistor(model=model_args, allow_numpy_conversion=False), "server", id="persistor")
