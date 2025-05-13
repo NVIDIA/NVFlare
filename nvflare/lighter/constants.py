59d696563c32375753011637f3f8bc1303cd28a1
YuanTingHsieh <yuantingh@nvidia.com>
2025-05-12 20:11:11 -0700

    Add CC and OnPrem Builder

--- nvflare/lighter/constants.py
+++ nvflare/lighter/constants.py
@@ -64,6 +64,7 @@ class PropKey:
     CAPACITY = "capacity"
     NUM_GPUS = "num_of_gpus"
     GPU_MEM = "mem_per_gpu_in_GiB"
+    CC_CONFIG = "cc_config_dict"
 
     # the following are internal properties added by builders during provision
     PARENT = "parent"
