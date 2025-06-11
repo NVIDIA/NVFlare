03e5488ebc3e37cb9c724514b0d971676cc2f141
Zhihong Zhang <zhihongz@nvidia.com>
2025-06-11 19:24:53 +0000

    Changed builder to use new file layouts

--- nvflare/lighter/cc/image_builder/base_images/launch_vm.sh
+++ nvflare/lighter/cc/image_builder/base_images/launch_vm.sh
@@ -1,66 +1,21 @@
-if [ $# -ne 2 ]; then
-  echo "Usage: $0 <cvm_image> <applog_image>"
-  exit 1
-fi
-
-VDD_IMAGE=$1
-APPLOG_IMAGE=$2
-
-UEFI_BIOS=/shared/OVMF.fd
-
-#Hardware Settings
-MEM=64 #in GBs
-FWDPORT=2222
-
-doecho=false
-docc=true
-
-while getopts "exp:" flag
-do
-        case ${flag} in
-                e) doecho=true;;
-                x) docc=false;;
-                p) FWDPORT=${OPTARG};;
-        esac
-done
-
-NVIDIA_GPU=$(lspci -d 10de: | awk '/NVIDIA/{print $1}')
-NVIDIA_PASSTHROUGH=$(lspci -n -s $NVIDIA_GPU | awk -F: '{print $4}' | awk '{print $1}')
-
-if [ "$doecho" = true ]; then
-         echo 10de $NVIDIA_PASSTHROUGH > /sys/bus/pci/drivers/vfio-pci/new_id
-fi
-
-if [ "$docc" = true ]; then
-        USE_HCC=true
-fi
-
-# SEV-ES:
-#             -machine confidential-guest-support=sev0,vmport=off \
-#             -object sev-guest,id=sev0,cbitpos=51,reduced-phys-bits=1,policy=0x5 \
-#
-# SEV-SNP:
-#             -machine confidential-guest-support=sev0,vmport=off \
-#             -object sev-snp-guest,id=sev0,cbitpos=51,reduced-phys-bits=1,policy=0x30000 \
-
-#strace -f -o rob-sev-working-strace.txt qemu-system-x86_64 \
-
-qemu-system-x86_64 \
-             -machine confidential-guest-support=sev0,vmport=off \
-             -object sev-snp-guest,id=sev0,cbitpos=51,reduced-phys-bits=1,policy=0x30000,kernel-hashes=off \
-                -vga none \
-                -monitor telnet:127.0.0.1:5555,server,nowait \
-                -enable-kvm -nographic -no-reboot \
-                -cpu EPYC-v4 \
-                -machine q35 -smp 12,maxcpus=31 -m ${MEM}G,slots=2,maxmem=512G \
-                -bios $UEFI_BIOS \
-		-kernel /localhome/local-zhihongz/boot/vmlinuz \
-                -initrd /localhome/local-zhihongz/boot/initrd.img \
-                -drive file=$VDD_IMAGE,if=none,id=disk0,format=qcow2 \
-                -drive file=$APPLOG_IMAGE,if=virtio,id=disk1,format=qcow2 \
-                -device virtio-scsi-pci,id=scsi0,disable-legacy=on,iommu_platform=true,romfile= \
-                -device scsi-hd,drive=disk0 \
-                -netdev user,id=vmnic,hostfwd=tcp::$FWDPORT-:22 \
-                -device virtio-net-pci,disable-legacy=on,iommu_platform=true,netdev=vmnic,romfile= \
-                -device pcie-root-port,id=pci.1,bus=pcie.0 \
-#                 -device vfio-pci,host=${NVIDIA_GPU},bus=pci.1,romfile= 
+sudo qemu-system-x86_64 \
+    -bios OVMF_AMD.fd \
+    -kernel bzImage \
+    -initrd initramfs.cpio.gz\
+    -append "console=ttyS0 root=/dev/vda1 rw net.ifnames=0 biosdevname=0 amd_iommu=on swiotlb=2097152,force vm_id=d5287b0be9cb" \
+    -nographic \
+    -machine q35,confidential-guest-support=sev0,vmport=off \
+    -object sev-snp-guest,id=sev0,cbitpos=51,reduced-phys-bits=1,policy=0x30000,kernel-hashes=on \
+    -vga none \
+    -enable-kvm -no-reboot \
+    -cpu EPYC-v4 \
+    -machine q35 -smp cores=8,threads=2,sockets=2 -m 30G,slots=2,maxmem=512G \
+    -drive file=crypt_root.qcow2,if=none,id=disk0,format=qcow2\
+    -device virtio-blk-pci,drive=disk0,serial=rootfs \
+    -drive file=applog_base.qcow2,if=none,id=disk1,format=qcow2\
+    -device virtio-blk-pci,drive=disk1,serial=encrypted\
+    -device virtio-scsi-pci,id=scsi0,disable-legacy=on,iommu_platform=true,romfile= \
+    -netdev user,id=vmnic,hostfwd=tcp::2222-:22,hostfwd=tcp::8002-:8002,hostfwd=tcp::8003-:8003 \
+    -device virtio-net-pci,disable-legacy=on,iommu_platform=true,netdev=vmnic,romfile=  \
+    -device pcie-root-port,id=pci.1,bus=pcie.0 \
+    -device vfio-pci,host=81:00.0,bus=pci.1,romfile=
--- nvflare/lighter/cc/image_builder/cvm_build.sh
+++ nvflare/lighter/cc/image_builder/cvm_build.sh
@@ -16,20 +16,18 @@ SITE_NAME=$2
 STARTUP_FOLDER=$3
 
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-BASE_IMAGE="${BASE_DIR}/base_images/nvidia_cc_base.qcow2"
-TARGET_IMAGE="${BASE_DIR}/target/${CVM_ID}_${SITE_NAME}_cvm.qcow2"
-APPLOG_BASE="${BASE_DIR}/base_images/applog_base.qcow2"
-APPLOG_IMAGE="${BASE_DIR}/target/${CVM_ID}_${SITE_NAME}_applog.qcow2"
+BASE_IMAGE_FOLDER="${BASE_DIR}/base_images"
+TARGET_FOLDER="${BASE_DIR}/target/cvm_${CVM_ID}"
 
-echo "Copy images ..."
+echo "Copy base images ..."
 
-mkdir -p "${BASE_DIR}/target"
-cp $BASE_IMAGE $TARGET_IMAGE
-cp $APPLOG_BASE $APPLOG_IMAGE
+mkdir -p "${TARGET_FOLDER}"
+cp -r $BASE_IMAGE_FOLDER/* $TARGET_FOLDER
 
 echo "Start VM ..."
+sleep 4
 
-nohup sudo base_images/launch_vm.sh $TARGET_IMAGE $APPLOG_IMAGE > cvm.log 2>&1 &
+# nohup sudo base_images/launch_vm.sh $TARGET_IMAGE $APPLOG_IMAGE > cvm.log 2>&1 &
 
 # Wait for SSH to be ready
 echo "Waiting for CVM to become reachable..."
@@ -44,8 +42,8 @@ echo "Starting playbook ..."
 
 echo "Playbook: $BASE_DIR/playbooks/cvm_build.yml"
 # Run ansible playbook to configure the VM
-ansible-playbook -i $BASE_DIR/playbooks/inventory.ini $BASE_DIR/playbooks/cvm_build.yml \
-    -e "yaml_config=$YAML_CONFIG site_name=$SITE_NAME startup_folder=$STARTUP_FOLDER 
-        cvm_id=$CVM_ID target_image=$TARGET_IMAGE applog_image=$APPLOG_IMAGE"
+#ansible-playbook -i $BASE_DIR/playbooks/inventory.ini $BASE_DIR/playbooks/cvm_build.yml \
+#    -e "yaml_config=$YAML_CONFIG site_name=$SITE_NAME startup_folder=$STARTUP_FOLDER 
+#         cvm_id=$CVM_ID target_image=$TARGET_IMAGE applog_image=$APPLOG_IMAGE"
 
 echo "CVM ${TARGET_IMAGE} build completed"
