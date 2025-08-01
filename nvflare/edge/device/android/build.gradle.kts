99b51c2b5b03cefb44ad8fc88015eba3daf9751f
Kevin <kevlu@nvidia.com>
2025-08-01 17:54:55 -0400

    add missing Android project files

--- nvflare/edge/device/android/build.gradle.kts
+++ nvflare/edge/device/android/build.gradle.kts
@@ -1,13 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     dependencies {
-        classpath("com.android.tools.build:gradle:8.9.2")
+        classpath("com.android.tools.build:gradle:8.12.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     }
 }
 
 plugins {
-    id("com.android.application") version "8.9.2" apply false
+    id("com.android.application") version "8.12.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
 
