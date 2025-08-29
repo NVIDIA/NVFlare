9e81bea041ebfa9d91bfe69ce2e837a8790f7aa0
Yuan-Ting Hsieh (謝沅廷) <yuantingh@nvidia.com>
2025-08-29 14:24:40 -0700

    Add hello cyclic recipe (#3645)
    Add cyclic recipe and update example 
    
    ### Description
    
    Add cyclic recipe and update example
    
    ### Types of changes
    <!--- Put an `x` in all the boxes that apply, and remove the not
    applicable items -->
    - [x] Non-breaking change (fix or new feature that would not break
    existing functionality).
    - [ ] Breaking change (fix or new feature that would cause existing
    functionality to change).
    - [ ] New tests added to cover the changes.
    - [ ] Quick tests passed locally by running `./runtest.sh`.
    - [ ] In-line docstrings updated.
    - [ ] Documentation updated.

---
+++ examples/hello-world/hello-cyclic/job.py
@@ -0,0 +1,39 @@
+# Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+This script demonstrates how to run the cyclic script runner for federated learning.
+"""
+
+from model import Net
+
+from nvflare.app_opt.tf.job_config.model import TFModel
+from nvflare.job_config.script_runner import FrameworkType
+from nvflare.recipe.cyclic_recipe import CyclicRecipe
+from nvflare.recipe.sim_env import SimEnv
+
+if __name__ == "__main__":
+    n_clients = 2
+    num_rounds = 3
+    train_script = "client.py"
+
+    recipe = CyclicRecipe(
+        framework=FrameworkType.TENSORFLOW,
+        num_rounds=num_rounds,
+        initial_model=TFModel(Net()),
+        train_script=train_script,
+        train_args="",
+    )
+
+    env = SimEnv(num_clients=n_clients)
+    recipe.execute(env=env)
