84e528a6340787cf447b98f560eb6aa8909ab822
Kevin <kevlu@nvidia.com>
2025-11-24 07:54:55 -0500

    fix imports

--- nvflare/app_opt/sklearn/__init__.py
+++ nvflare/app_opt/sklearn/__init__.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from nvflare.app_opt.sklearn.recipes import SklearnFedAvgRecipe
-from nvflare.app_opt.sklearn.recipes import SVMFedAvgRecipe
+from nvflare.app_opt.sklearn.recipes import SklearnFedAvgRecipe, SVMFedAvgRecipe
 
 __all__ = ["SklearnFedAvgRecipe", "SVMFedAvgRecipe"]
