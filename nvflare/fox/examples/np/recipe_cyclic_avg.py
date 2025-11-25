3b30b41639704a3590eeaed169f9be1068a8f66d
Yan Cheng <yanc@nvidia.com>
2025-11-25 14:48:51 -0500

    add cyclic-avg recipe

--- nvflare/fox/examples/np/recipe_cyclic_avg.py
+++ nvflare/fox/examples/np/recipe_cyclic_avg.py
@@ -11,16 +11,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import logging
+import os
 
 from nvflare.fox import fox
-from nvflare.fox.api.app import ClientApp, ServerApp
-from nvflare.fox.api.utils import simple_logging
 from nvflare.fox.examples.np.algos.client import NPTrainer
 from nvflare.fox.examples.np.algos.strategies.avg_para import NPFedAvgParallel
 from nvflare.fox.examples.np.algos.strategies.cyclic import NPCyclic
-from nvflare.fox.sim.simulator import Simulator
+from nvflare.fox.examples.np.algos.utils import save_np_model
+from nvflare.fox.sys.recipe import FoxRecipe
 from nvflare.fuel.utils.log_utils import get_obj_logger
+
+JOB_ROOT_DIR = "/Users/yanc/NVFlare/sandbox/v27/prod_00/admin@nvidia.com/transfer"
 
 
 class Controller:
@@ -41,31 +42,34 @@ class Controller:
         self.logger.info("running cyclic ...")
         ctl = NPCyclic(self.initial_model, num_rounds=self.cyclic_rounds)
         result = ctl.execute()
-        self.logger.info(f"final cyclic model: {result}")
+
+        file_name = os.path.join(fox.workspace.get_work_dir(), "cyclic_model.npy")
+        save_np_model(result, file_name)
+        self.logger.info(f"[{fox.call_info}]: saved cyclic model {result} to {file_name}")
 
         self.logger.info("running fed-avg ...")
         ctl = NPFedAvgParallel(initial_model=result, num_rounds=self.avg_rounds)
-        result = ctl.execute()
-        self.logger.info(f"final model: {result}")
-        return result
+        return ctl.execute()
+
+    @fox.final
+    def save_result(self):
+        final_result = fox.get_input()
+        file_name = os.path.join(fox.workspace.get_work_dir(), "final_model.npy")
+        save_np_model(final_result, file_name)
+        self.logger.info(f"[{fox.call_info}]: saved final model {final_result} to {file_name}")
 
 
 def main():
-    simple_logging(logging.DEBUG)
-    exp_name = "cyclic_avg"
-
-    server_app = ServerApp(Controller(initial_model=[[1, 2, 3], [4, 5, 6], [7, 8, 9]], cyclic_rounds=2, avg_rounds=3))
-
-    simulator = Simulator(
-        root_dir="/tmp/fox",
-        experiment_name=exp_name,
-        server_app=server_app,
-        client_app=ClientApp(NPTrainer(delta=1.0)),
-        num_clients=3,
+    recipe = FoxRecipe(
+        job_name="fox_cyclic_avg",
+        server=Controller(
+            initial_model=[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            cyclic_rounds=2,
+            avg_rounds=3,
+        ),
+        client=NPTrainer(delta=1.0),
     )
-
-    final_result = simulator.run()
-    print(f"final model: {final_result}")
+    recipe.export(JOB_ROOT_DIR)
 
 
 if __name__ == "__main__":
