1c450464ea979447dc1e508a39b0116618340c93
Holger Roth <hroth@nvidia.com>
2025-12-22 15:12:07 -0500

    fix benchmark setting

--- examples/advanced/cifar10/pt/src/model.py
+++ examples/advanced/cifar10/pt/src/model.py
@@ -51,7 +51,6 @@ def set_seed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
 
 
 class SimpleCNN(nn.Module):
