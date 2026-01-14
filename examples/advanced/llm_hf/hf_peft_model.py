08aae05525c3850b8816131d329665743b847526
Ziyue Xu <ziyuex@nvidia.com>
2026-01-14 14:18:57 -0500

    Convert job to recipe for LLM_HF example (#3888)
    Fixes # .
    
    ### Description
    
    From job api to recipe
    
    ### Types of changes
    <!--- Put an `x` in all the boxes that apply, and remove the not
    applicable items -->
    - [x] Non-breaking change (fix or new feature that would not break
    existing functionality).
    - [ ] Breaking change (fix or new feature that would cause existing
    functionality to change).
    - [ ] New tests added to cover the changes.
    - [ ] Quick tests passed locally by running `./runtest.sh`.
    - [ ] In-line docstrings updated.
    - [ ] Documentation updated.

--- examples/advanced/llm_hf/hf_peft_model.py
+++ examples/advanced/llm_hf/hf_peft_model.py
@@ -20,6 +20,7 @@ from transformers import AutoModelForCausalLM
 class CausalLMPEFTModel(torch.nn.Module):
     def __init__(self, model_name_or_path):
         super(CausalLMPEFTModel, self).__init__()
+        self.model_name_or_path = model_name_or_path
         # PEFT configs
         peft_config = LoraConfig(
             lora_alpha=16,
