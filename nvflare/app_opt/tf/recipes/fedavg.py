de1e5d1f987d95667eb3e8fee781ef3b6d9df5af
Holger Roth <hroth@nvidia.com>
2026-01-21 18:24:45 -0500

    formatting

--- nvflare/app_opt/tf/recipes/fedavg.py
+++ nvflare/app_opt/tf/recipes/fedavg.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, Dict, Optional
+from typing import Any, Optional
 
 from nvflare.apis.dxo import DataKind
 from nvflare.app_common.abstract.aggregator import Aggregator
