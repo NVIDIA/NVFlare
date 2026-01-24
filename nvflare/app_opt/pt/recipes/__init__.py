a5b45de2cdeb16b238bea16e87e2565ccda9ce7a
YuanTingHsieh <yuantingh@nvidia.com>
2026-01-23 18:30:39 -0800

    Fix format

--- nvflare/app_opt/pt/recipes/__init__.py
+++ nvflare/app_opt/pt/recipes/__init__.py
@@ -15,9 +15,8 @@
 from .cyclic import CyclicRecipe
 from .fedavg import FedAvgRecipe
 from .fedavg_he import FedAvgRecipeWithHE
+from .fedeval import FedEvalRecipe
 from .fedopt import FedOptRecipe
 from .scaffold import ScaffoldRecipe
-from .fedeval import FedEvalRecipe
 
 __all__ = ["FedAvgRecipe", "CyclicRecipe", "FedOptRecipe", "ScaffoldRecipe", "FedAvgRecipeWithHE", "FedEvalRecipe"]
-
