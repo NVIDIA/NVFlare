ce07d625cb12670ef15f0081151d95c9bb198b29
Peter Cnudde <pcnudde@nvidia.com>
2026-01-26 09:44:29 -0800

    fix higgs data downloader to doanload even if there is already a corrupted download. (overwrite in the wget)

--- examples/advanced/sklearn-linear/prepare_data.sh
+++ examples/advanced/sklearn-linear/prepare_data.sh
@@ -6,10 +6,12 @@ DATASET_PATH="${DATASET_DIR}/HIGGS.csv"
 if [ -f "$DATASET_PATH" ]; then
     echo "${DATASET_PATH} exists."
 else
+    mkdir -p "${DATASET_DIR}"
     # Please note that the UCI's website may experience occasional downtime.
-    wget --directory-prefix ${DATASET_DIR} https://archive.ics.uci.edu/static/public/280/higgs.zip
-    unzip "${DATASET_DIR}/higgs.zip" -d "${DATASET_DIR}"
-    gzip -d "${DATASET_DIR}/HIGGS.csv.gz"
+    wget -O "${DATASET_DIR}/higgs.zip" https://archive.ics.uci.edu/static/public/280/higgs.zip
+    unzip -o "${DATASET_DIR}/higgs.zip" -d "${DATASET_DIR}"
+    gzip -d -f "${DATASET_DIR}/HIGGS.csv.gz"
+    rm -f "${DATASET_DIR}/higgs.zip"
 
     echo "Data downloaded and saved in ${DATASET_PATH}"
 fi
