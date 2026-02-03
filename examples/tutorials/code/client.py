01b00d40efba044f65a91bed11844d20ebf0ed02
Holger Roth <6304754+holgerroth@users.noreply.github.com>
2026-02-02 20:02:58 -0500

    [2.7] Job CLI Tutorial Fixes (#4096)
    Fixes # .
    
    Fix broken Job CLI tutorial.
    Adds missing model.py for Job Recipe tutorial.
    
    <!--- Put an `x` in all the boxes that apply, and remove the not
    applicable items -->
    - [x] Non-breaking change (fix or new feature that would not break
    existing functionality).
    - [ ] Breaking change (fix or new feature that would cause existing
    functionality to change).
    - [ ] New tests added to cover the changes.
    - [ ] Quick tests passed locally by running `./runtest.sh`.
    - [ ] In-line docstrings updated.
    - [ ] Documentation updated.

--- examples/tutorials/code/client.py
+++ examples/tutorials/code/client.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-client side training scripts
+    client side training scripts
 """
 
 import argparse
