34500b4e983bf2f8b9cce6edb3ecc8e22fcddcb7
Ziyue Xu <ziyuex@nvidia.com>
2026-02-04 13:29:25 -0500

    cross site val

--- examples/advanced/job_api/pt/fedavg_script_runner_cifar10.py
+++ examples/advanced/job_api/pt/fedavg_script_runner_cifar10.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
 
     job.to(IntimeModelSelector(key_metric="accuracy"), "server")
 
-    # Add TensorBoard analytics receiver on each client for local events
+    # Add TensorBoard analytics receiver to each client for local events
 
     # Note: We can optionally replace the above code with the FedAvgJob, which is a pattern to simplify FedAvg job creations
     # job = FedAvgJob(name="cifar10_fedavg", num_rounds=num_rounds, n_clients=n_clients, initial_model=Net())
