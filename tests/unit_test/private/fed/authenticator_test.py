8ccef5765c1df132c16d5042a6f9874d92e233be
Peter Cnudde <pcnudde@nvidia.com>
2026-05-15 14:30:31 -0700

    Allow CellPipe stream aliases for auth binding
    (cherry picked from commit 18a73507fcc0ea7fec59b5994a91f154a8bc0737)

--- tests/unit_test/private/fed/authenticator_test.py
+++ tests/unit_test/private/fed/authenticator_test.py
@@ -19,6 +19,7 @@ import pytest
 from nvflare.apis.fl_constant import ServerCommandNames
 from nvflare.fuel.f3.cellnet.defs import CellChannel, MessageHeaderKey, ReturnCode
 from nvflare.fuel.f3.message import Message
+from nvflare.fuel.f3.streaming.stream_const import STREAM_CHANNEL, STREAM_DATA_TOPIC
 from nvflare.private.defs import CellMessageHeaderKeys
 from nvflare.private.fed.authenticator import MISSING_CLIENT_FQCN, validate_auth_headers
 
@@ -28,11 +29,11 @@ class _TokenVerifier:
         return True
 
 
-def _make_message(origin):
+def _make_message(origin, channel=CellChannel.SERVER_COMMAND, topic=ServerCommandNames.GET_TASK):
     return Message(
         headers={
-            MessageHeaderKey.CHANNEL: CellChannel.SERVER_COMMAND,
-            MessageHeaderKey.TOPIC: ServerCommandNames.GET_TASK,
+            MessageHeaderKey.CHANNEL: channel,
+            MessageHeaderKey.TOPIC: topic,
             MessageHeaderKey.ORIGIN: origin,
             CellMessageHeaderKeys.CLIENT_NAME: "site-a",
             CellMessageHeaderKeys.TOKEN: "token-a",
@@ -41,9 +42,9 @@ def _make_message(origin):
     )
 
 
-def _validate(origin, client_fqcn_resolver):
+def _validate(origin, client_fqcn_resolver, channel=CellChannel.SERVER_COMMAND, topic=ServerCommandNames.GET_TASK):
     return validate_auth_headers(
-        message=_make_message(origin),
+        message=_make_message(origin, channel=channel, topic=topic),
         token_verifier=_TokenVerifier(),
         logger=logging.getLogger(__name__),
         client_fqcn_resolver=client_fqcn_resolver,
@@ -56,8 +57,80 @@ def test_validate_auth_headers_accepts_token_from_registered_origin(origin):
     assert _validate(origin, lambda _client_name, _token: "site-a") is None
 
 
+@pytest.mark.parametrize(
+    "origin",
+    [
+        "site-a_8065f1c4-fd35-47ef-b945-800f4d0d5176_active",
+        "site-a_8065f1c4-fd35-47ef-b945-800f4d0d5176_passive",
+    ],
+)
+def test_validate_auth_headers_accepts_direct_cell_pipe_stream_alias(origin):
+    assert (
+        _validate(
+            origin,
+            lambda _client_name, _token: "site-a",
+            channel=STREAM_CHANNEL,
+            topic=STREAM_DATA_TOPIC,
+        )
+        is None
+    )
+
+
 def test_validate_auth_headers_rejects_token_from_different_origin():
     reply = _validate("site-b", lambda _client_name, _token: "site-a")
+
+    assert reply.get_header(MessageHeaderKey.RETURN_CODE) == ReturnCode.UNAUTHENTICATED
+
+
+def test_validate_auth_headers_rejects_cell_pipe_alias_for_different_client():
+    reply = _validate(
+        "site-b_8065f1c4-fd35-47ef-b945-800f4d0d5176_passive",
+        lambda _client_name, _token: "site-a",
+        channel=STREAM_CHANNEL,
+        topic=STREAM_DATA_TOPIC,
+    )
+
+    assert reply.get_header(MessageHeaderKey.RETURN_CODE) == ReturnCode.UNAUTHENTICATED
+
+
+def test_validate_auth_headers_rejects_cell_pipe_alias_on_non_stream_channel():
+    reply = _validate(
+        "site-a_8065f1c4-fd35-47ef-b945-800f4d0d5176_passive",
+        lambda _client_name, _token: "site-a",
+    )
+
+    assert reply.get_header(MessageHeaderKey.RETURN_CODE) == ReturnCode.UNAUTHENTICATED
+
+
+def test_validate_auth_headers_rejects_cell_pipe_alias_with_hierarchical_runtime_id():
+    reply = _validate(
+        "site-a_8065f1c4-fd35-47ef-b945-800f4d0d5176.child_passive",
+        lambda _client_name, _token: "site-a",
+        channel=STREAM_CHANNEL,
+        topic=STREAM_DATA_TOPIC,
+    )
+
+    assert reply.get_header(MessageHeaderKey.RETURN_CODE) == ReturnCode.UNAUTHENTICATED
+
+
+def test_validate_auth_headers_rejects_cell_pipe_alias_with_underscore_runtime_id():
+    reply = _validate(
+        "site-a_simulate_job_passive",
+        lambda _client_name, _token: "site-a",
+        channel=STREAM_CHANNEL,
+        topic=STREAM_DATA_TOPIC,
+    )
+
+    assert reply.get_header(MessageHeaderKey.RETURN_CODE) == ReturnCode.UNAUTHENTICATED
+
+
+def test_validate_auth_headers_rejects_underscore_prefixed_client_ambiguity():
+    reply = _validate(
+        "site-a_x_8065f1c4-fd35-47ef-b945-800f4d0d5176_passive",
+        lambda _client_name, _token: "site-a",
+        channel=STREAM_CHANNEL,
+        topic=STREAM_DATA_TOPIC,
+    )
 
     assert reply.get_header(MessageHeaderKey.RETURN_CODE) == ReturnCode.UNAUTHENTICATED
 
