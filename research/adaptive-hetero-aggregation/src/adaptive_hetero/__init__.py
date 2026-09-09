4efb55173b1e878c2bb3efe3810a04fa14cfe8cb
SAURAV SINGLA <sauravsingla08@gmail.com>
2026-09-09 08:50:05 +0530

    Export adaptive ModelAggregator adapter

--- research/adaptive-hetero-aggregation/src/adaptive_hetero/__init__.py
+++ research/adaptive-hetero-aggregation/src/adaptive_hetero/__init__.py
@@ -12,6 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from .model_aggregator import AdaptiveHeterogeneityModelAggregator
+from .nvflare_aggregator import AdaptiveHeterogeneityAggregator, AdaptiveMetaKey
 from .policy import AdaptiveHeterogeneityPolicy, AdaptiveWeightingConfig, WeightingResult
 
-__all__ = ["AdaptiveHeterogeneityPolicy", "AdaptiveWeightingConfig", "WeightingResult"]
+__all__ = [
+    "AdaptiveHeterogeneityAggregator",
+    "AdaptiveHeterogeneityModelAggregator",
+    "AdaptiveHeterogeneityPolicy",
+    "AdaptiveMetaKey",
+    "AdaptiveWeightingConfig",
+    "WeightingResult",
+]
